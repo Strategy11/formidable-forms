3f44f6b0afef7055b93849541870257826c0353e
jwahlin <jamie@strategy11.com>
2015-03-02 11:02:01 -0700

    Update version number for 2.0RC3

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.0rc2
+Version: 2.0rc3
 Plugin URI: http://formidablepro.com/
 Author URI: http://strategy11.com
 Author: Strategy11
