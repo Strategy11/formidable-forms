7d0e4f56e160ad53986260897656210ef52dd4ab
Steph <steph@strategy11.com>
2015-03-11 11:29:01 -0600

    Fix php 5.2 issue in unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,15 +12,14 @@ $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'formidable/formidable.php' ),
 );
 
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
-}
-else {
+} else {
 	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
 
-if ( file_exists( __DIR__  .'/../vendor/autoload_52.php' ) ) {
-    include( __DIR__  .'/../vendor/autoload_52.php' );
+if ( file_exists( dirname( __FILE__ )  . '/../vendor/autoload_52.php' ) ) {
+	include( dirname( __FILE__ )  . '/../vendor/autoload_52.php' );
 }
 
-@include __DIR__ . '/../vendor/autoload.php';
+include( dirname( __FILE__ ) . '/../vendor/autoload.php' );
