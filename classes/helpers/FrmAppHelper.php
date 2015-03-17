497c22f612f2855cd21369626ab522cfb4dd4f53
Steph <steph@strategy11.com>
2015-03-17 10:46:34 -0600

    Next version number

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -10,7 +10,7 @@ class FrmAppHelper {
     /**
      * @since 2.0
      */
-    public static $plug_version = '2.0rc2';
+    public static $plug_version = '2.0rc5';
 
     /**
      * @since 1.07.02
