6a7ce35c2a942e67775ef0e58ca3c653414be759
Steph <steph@strategy11.com>
2015-03-31 15:12:56 -0600

    Update version number

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -10,7 +10,7 @@ class FrmAppHelper {
     /**
      * @since 2.0
      */
-    public static $plug_version = '2.0rc7';
+    public static $plug_version = '2.0rc8';
 
     /**
      * @since 1.07.02
--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.0rc7
+Version: 2.0rc8
 Plugin URI: http://formidablepro.com/
 Author URI: http://strategy11.com
 Author: Strategy11
