10052b9cd12ee484ad5ac4ca80cd3dbabbfcdc13
Steph <steph@strategy11.com>
2015-05-15 15:55:17 -0600

    Add recaptcha placeholder on build page https://github.com/Strategy11/formidable/issues/739

--- classes/views/frm-fields/show-build.php
+++ classes/views/frm-fields/show-build.php
@@ -53,10 +53,11 @@
     </div>
 <?php
 } else if ( $field['type'] == 'captcha' ) {
-?>
-    <?php if ( empty($frm_settings->pubkey) ) { ?>
+	if ( empty($frm_settings->pubkey) ) { ?>
     <div class="howto frm_no_captcha_text"><?php printf(__( 'Your captcha will not appear on your form until you %1$sset up%2$s the Site and Private Keys', 'formidable' ), '<a href="?page=formidable-settings">', '</a>') ?></div>
-    <?php } ?>
+    <?php
+    } ?>
+	<img src="<?php echo esc_url( FrmAppHelper::plugin_url() . '/images/recaptcha.png' ) ?>" class="recaptcha_placeholder" alt="reCaptcha"/>
     <input type="hidden" name="<?php echo esc_attr( $field_name ) ?>" value="1" />
 <?php
 } else {
--- classes/views/frm-fields/show.php
+++
@@ -1,4 +0,0 @@
-<?php
-_deprecated_file( basename(__FILE__), '1.07.11', 'FrmFieldsHelper::replace_shortcodes' );
-
-echo FrmFieldsHelper::replace_shortcodes($field['custom_html'], $field, $errors, $form);
