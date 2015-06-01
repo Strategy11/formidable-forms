14dc89c9ace425b2e140b81fa1ec1dbde70d9b2d
jwahlin <jamie@strategy11.com>
2015-06-01 14:12:48 -0600

    More for formresults unit testing

--- tests/test_FrmProEntriesController.php
+++ tests/test_FrmProEntriesController.php
@@ -61,47 +61,114 @@ SCRIPT;
 		}
 	}
 
+	/**
+	 * @covers FrmProEntriesController::get_form_results
+	 */
 	public function _test_single_form_formresults( $array_key, $form_key ) {
 		$form_id = $this->factory->form->get_id_by_key( $form_key );
 
+		// Get number of entries in form
 		$expected_entry_count = 3;
-
-		// Get number of entries
 		$entry_count = FrmEntry::getRecordCount( $form_id );
 		$this->assertTrue( $entry_count == $expected_entry_count, 'Entries are not being retrieved correctly. Retrieved ' . $entry_count . ' but should have ' . $expected_entry_count . ' for form ' . $this->all_fields_form_key );
 
-		// Get number of fields
+		// Get number of fields in form (exlcluding no_save_fields)
+		$field_count = self::_get_field_count( $form_id );
+
+		// This will always test parameters in the same order. What about different combinations of parameters?
+		$params_to_test = array( 'none' => '', 'fields' => '493ito,p3eiuk,uc580i,4t3qo4,54tffk', 'cols' => 3, 'google' => 1, 'pagesize' => 2);
+		$params = '';
+		foreach ( $params_to_test as $pname => $pval ) {
+			if ( $pname != 'none' ) {
+				$params .= ' ' . $pname . '="' . $pval . '"';
+			}
+
+			// Set the expected number of columns
+			if ( $pname == 'fields' ) {
+				$field_count = count( explode( ',', $pval ) );
+			} else if ( $pname == 'cols' ) {
+				$field_count = $pval;
+			} else if ( $pname == 'edit_link' || $pname == 'delete_link' ) {
+				$field_count++;
+			}
+
+			// Set the expected number of rows
+			if ( $pname == 'pagesize' ) {
+				$entry_count = $pval;
+			}
+
+			self::_check_formresults_values( $form_id, $entry_count, $field_count, $params );
+			unset( $pname, $pval);
+		}
+		// Check w/ and w/o posts, custom fields, Dynamic fields, etc.
+	}
+
+	function _get_field_count( $form_id ) {
+		// Get all fields in form
 		$expected_field_count = 36;//Includes 3 fields from repeating section form
-		// Probably exclude specific field types (embed form, divider, end divider, html, ...);
-		$field_count = count( FrmField::get_all_for_form( $form_id ) );
+		$all_fields = FrmField::get_all_for_form( $form_id );
+		$field_count = count( $all_fields );
 		$this->assertTrue( $field_count == $expected_field_count, 'Fields are not being retrieved correctly. Retrieved ' . $field_count . ', but should have ' . $expected_field_count );
 
-		// Regular form (no post or custom fields)
-		// This will always test parameters in the same order. What about different combinations of parameters?
-		$params_to_test = array( '', ' google=1', ' fields=x');
-		$params = '';
-		foreach ( $params_to_test as $param ) {
-			$params .= $param;
-			self::_check_formresults_values( $form_id, $entry_count, $field_count, $params );
+		// Remove excluded field types from field count
+		$exclude_fields = array( 'divider', 'end_divider', 'captcha', 'break', 'html' );
+		foreach ( $all_fields as $f ) {
+			if ( in_array( $f->type, $exclude_fields ) ) {
+				$field_count--;
+			}
 		}
-
-		// Check for the correct number of rows and columns
-		// Check w/ and w/o posts, custom fields, Dynamic fields, etc.
+		return $field_count;
 	}
 
 	function _check_formresults_values( $form_id, $expected_row_num, $expected_col_num, $params='' ) {
 		$param_text = $params ? $params : ' no';
-		$formresults = do_shortcode('[formresults id="' . $form_id . '" ' . $params . ']');
+		$formresults = do_shortcode('[formresults id="' . $form_id . '"' . $params . ']');
+		$this->assertNotEquals( '', $formresults, 'Formresults (with' . $param_text . ' parameters) is not showing up at all.');
 
-		// Correct number of rows
-		$actual_row_num = substr_count( $formresults, '<tr');
-		$this->assertEquals( $actual_row_num, $expected_row_num, 'Formresults (with' . $params . ' parameters) is not showing the correct number of rows/entries' );
-
-		// Correct number of columns
-		$actual_col_num = substr_count( $formresults, '<th')/2;
-		$this->assertEquals( $actual_col_num, $expected_col_num, 'Formresults (with' . $params . ' parameters) is not showing the correct number of columns' );
-
-		// Check if edit link is showing up
+		self::_check_row_num( $formresults, $expected_row_num, $param_text );
+		self::_check_col_num( $formresults, $expected_col_num, $param_text );
+		self::_check_for_google_table( $formresults, $params, $param_text, $form_id );
+		self::_check_for_edit_link( $formresults, $expected_row_num, $params, $param_text );
+		self::_check_for_delete_link( $formresults, $expected_row_num, $params, $param_text );
 		// Check pagination
+	}
+
+	function _check_row_num( $formresults, $expected_row_num, $param_text ) {
+		$actual_row_num = substr_count( $formresults, '<tr') - 2;
+		$this->assertEquals( $expected_row_num, $actual_row_num, 'Formresults (with' . $param_text . ' parameters) is not showing the correct number of rows/entries' );
+	}
+
+	function _check_col_num( $formresults, $expected_col_num, $param_text ) {
+		$actual_col_num = substr_count( $formresults, '<th>')/2;
+		$this->assertEquals( $expected_col_num, $actual_col_num, 'Formresults (with' . $param_text . ' parameters) is not showing the correct number of columns' );
+	}
+
+	function _check_for_google_table( $formresults, $params, $param_text, $form_id ) {
+		if ( strpos( $params, 'google=' ) !== false ) {
+			$this->assertTrue( strpos( $formresults, 'frm_google_table_' . $form_id ), 'Formresults (with' . $param_text . ' parameters) is not showing google table when it should be' );
+		}
+	}
+
+	function _check_for_edit_link( $formresults, $expected_row_num, $params, $param_text ) {
+		if ( strpos( $params, 'edit_link' ) !== false ) {
+			// TODO: Check if edit link shows up for different users
+			$params_array = explode( '"', $params );
+			$edit_link_key = array_search( ' edit_link=', $params_array );
+			$edit_link_text = $params_array[ $edit_link_key + 1 ];
+			$edit_link_count = substr_count( $formresults, $edit_link_text );
+			$this->assertEquals( $edit_link_count, $expected_row_num, 'Formresults (with' . $param_text . ' parameters) is not showing the correct number of edit links' );
+		}
+	}
+
+	function _check_for_delete_link( $formresults, $expected_row_num, $params, $param_text ) {
+		if ( strpos( $params, 'delete_link' ) !== false ) {
+			$params_array = explode( '"', $params );
+			$del_link_key = array_search( ' delete_link=', $params_array );
+			$del_link_text = $params_array[ $del_link_key + 1 ];
+
+			// TODO: Check if delete link shows up for different users
+			$delete_link_count = substr_count( $formresults, $del_link_text );
+			$this->assertEquals( $delete_link_count, $expected_row_num, 'Formresults (with' . $param_text . ' parameters) is not showing the correct number of delete links' );
+		}
 	}
 }
