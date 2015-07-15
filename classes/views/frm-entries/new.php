0e4ea6c9122c5cb6507ec13621fbe9373b871f70
Steph <steph@strategy11.com>
2015-07-15 13:32:19 -0600

    Don't require $include_form_tag to be set for multi-paged forms https://github.com/Strategy11/formidable/issues/931

--- classes/views/frm-entries/new.php
+++ classes/views/frm-entries/new.php
@@ -1,6 +1,6 @@
 <div class="frm_forms <?php echo FrmFormsHelper::get_form_style_class($values); ?>" id="frm_form_<?php echo esc_attr( $form->id ) ?>_container">
 <?php
-if ( $include_form_tag ) {
+if ( ! isset( $include_form_tag ) || $include_form_tag ) {
 ?>
 <form enctype="<?php echo esc_attr( apply_filters( 'frm_form_enctype', 'multipart/form-data', $form ) ) ?>" method="post" class="frm-show-form <?php do_action('frm_form_classes', $form) ?>" id="form_<?php echo esc_attr( $form->form_key ) ?>" <?php echo $frm_settings->use_html ? '' : 'action=""'; ?>>
 <?php
@@ -13,7 +13,7 @@ include(FrmAppHelper::plugin_path() .'/classes/views/frm-entries/errors.php');
 $form_action = 'create';
 require(FrmAppHelper::plugin_path() .'/classes/views/frm-entries/form.php');
 
-if ( $include_form_tag ) {
+if ( ! isset( $include_form_tag ) || $include_form_tag ) {
 ?>
 </form>
 <?php
