9a8f6eadfe6aae430bbb4a146daccdd3862cdb86
jwahlin <jamie@strategy11.com>
2015-08-07 15:44:21 -0700

    Add more testing for duplicating forms

--- tests/test_FrmForm.php
+++ tests/test_FrmForm.php
@@ -18,7 +18,7 @@ class WP_Test_FrmForm extends FrmUnitTest {
 	 * @covers FrmForm::duplicate
 	 */
 	function test_duplicate(){
-		$form = $this->factory->form->get_object_by_id( 'contact' );
+		$form = $this->factory->form->get_object_by_id( $this->all_fields_form_key );
 
 		$id = FrmForm::duplicate( $form->id );
 		$this->assertTrue( is_numeric( $id ) );
@@ -28,6 +28,59 @@ class WP_Test_FrmForm extends FrmUnitTest {
 		$original_actions = FrmFormAction::get_action_for_form( $form->id );
 		$new_actions = FrmFormAction::get_action_for_form( $id );
 		$this->assertEquals( count( $original_actions ), count( $new_actions ) );
+
+		// For repeating sections
+		self::_check_if_child_forms_duplicate( $form->id, $id );
+		self::_check_if_form_select_updates( $form->id, $id );
+	}
+
+	function _check_if_child_forms_duplicate( $old_form_id, $new_form_id ) {
+		// Check if there are any child forms in the original form
+		$old_child_forms = FrmForm::getAll( array( 'parent_form_id' => $old_form_id ) );
+		if ( ! $old_child_forms ) {
+			return;
+		}
+
+		// Check if there are any child forms in the new form
+		$new_child_forms = FrmForm::getAll( array( 'parent_form_id' => $new_form_id ) );
+
+		// Check if there are the same number of child forms in the duplicated form
+		$this->assertEquals( count( $old_child_forms ), count( $new_child_forms ), 'When a form is duplicated, the child forms are not duplicated correctly.' );
+
+		self::_check_if_child_fields_duplicate( $old_child_forms, $new_child_forms );
+	}
+
+	function _check_if_child_fields_duplicate( $old_child_forms, $new_child_forms ) {
+		// Just check the first form
+		$old_child_form = reset( $old_child_forms );
+		$new_child_form = reset( $new_child_forms );
+
+		// Get all fields in each form
+		$old_child_form_fields = FrmField::get_all_for_form( $old_child_form->id );
+		$new_child_form_fields = FrmField::get_all_for_form( $new_child_form->id );
+
+		// Check if there are the same number of child form fields in the duplicated child form
+		$this->assertEquals( count( $old_child_form_fields ), count( $new_child_form_fields ), 'When a form is duplicated, the fields in the repeating section are not duplicated correctly.' );
+	}
+
+	function _check_if_form_select_updates( $old_form_id, $new_form_id ) {
+		// Get all repeating sections in both forms
+		$old_repeating_sections = FrmField::get_all_types_in_form( $old_form_id, 'divider' );
+		$new_repeating_sections = FrmField::get_all_types_in_form( $new_form_id, 'divider' );
+		if ( ! $old_repeating_sections ) {
+			return;
+		}
+
+		foreach ( $old_repeating_sections as $key => $section ) {
+			if ( ! FrmField::is_repeating_field( $section ) ) {
+				continue;
+			}
+
+			$old_form_select = $section->field_options['form_select'];
+			$new_form_select = $new_repeating_sections[ $key ]->field_options['form_select'];
+
+			$this->assertNotEquals( $old_form_select, $new_form_select, 'A form was duplicated, but the form_select was not updated for the repeating section :/');
+		}
 	}
 
 	/**
