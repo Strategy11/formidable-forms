9a0151e6082534a1af3baebe51f026d1aa6de8d7
Christoph Herr <christophherr@users.noreply.github.com>
2015-10-09 23:45:50 -0400

    Fixes Typo #L8 FrmAddonsController.php

--- classes/controllers/FrmAddonsController.php
+++ classes/controllers/FrmAddonsController.php
@@ -5,7 +5,7 @@ class FrmAddonsController {
 	public static function show_addons() {
 		$plugins = apply_filters( 'frm_installed_addons', array() );
 		if ( empty( $plugins ) ) {
-			_e( 'There are not any plugins on your site the require a license', 'formidable' );
+			_e( 'There are no plugins on your site that require a license', 'formidable' );
 			return;
 		}
 
