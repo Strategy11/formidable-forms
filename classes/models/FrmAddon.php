f2c5e0f8fa5080a9a337b15a9827650b29ec48b2
Steph <steph@strategy11.com>
2016-01-05 15:03:24 -0700

    Use the WordPress changelog popup for Formidable

--- classes/models/FrmAddon.php
+++ classes/models/FrmAddon.php
@@ -69,7 +69,10 @@ class FrmAddon {
 				$api_data['item_id'] = $this->download_id;
 			}
 
-			new FrmEDD_SL_Plugin_Updater( $this->store_url, $this->plugin_file, $api_data );
+			$edd = new FrmEDD_SL_Plugin_Updater( $this->store_url, $this->plugin_file, $api_data );
+			if ( $this->plugin_folder == 'formidable/formidable.php' ) {
+				remove_filter( 'plugins_api', array( $edd, 'plugins_api_filter' ), 10, 3 );
+			}
 
 			add_filter( 'site_transient_update_plugins', array( &$this, 'clear_expired_download' ) );
 		}
