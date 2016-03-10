e9cf346cc7090b790904a23ca644321171823c6f
Steph <steph@strategy11.com>
2016-03-10 11:15:33 -0700

    Trigger a style save after update

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -4,7 +4,7 @@ if ( ! defined('ABSPATH') ) {
 }
 
 class FrmAppHelper {
-	public static $db_version = 28; //version of the database we are moving to
+	public static $db_version = 29; //version of the database we are moving to
 	public static $pro_db_version = 32;
 
 	/**
