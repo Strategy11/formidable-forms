08969c503f9f56f409abbda01d590c48961729d4
Steph <steph@strategy11.com>
2016-05-18 13:46:51 -0600

    Check more safely for redis

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -495,7 +495,7 @@ class FrmAppHelper {
 
 		if ( is_callable( array( $wp_object_cache, '__get' ) ) ) {
 			$group_cache = $wp_object_cache->__get('cache');
-		} elseif ( $wp_object_cache->redis_status() ) {
+		} elseif ( is_callable( array( $wp_object_cache, 'redis_status' ) ) && $wp_object_cache->redis_status() ) {
 			// check if the object cache is overridden by Redis
 			$wp_object_cache->flush();
 			$group_cache = array();
