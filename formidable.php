733cf458be825286dabd1f1d4a996d4612e872e4
jwahlin <jamie@strategy11.com>
2016-05-23 13:40:16 -0700

    Update to 2.01.02 Update english translation file

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.01.02b2
+Version: 2.01.02
 Plugin URI: http://formidablepro.com/
 Author URI: http://strategy11.com
 Author: Strategy11
