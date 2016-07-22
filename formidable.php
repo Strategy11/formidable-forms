61cecf762ab0f5aa5525febed50b9734209b82fc
jwahlin <jamie@strategy11.com>
2016-07-22 16:26:27 -0700

    Update to 2.02.05b1 Temporarily remove commit for https://github.com/Strategy11/formidable/issues/1104

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.02.04
+Version: 2.02.05b1
 Plugin URI: http://formidablepro.com/
 Author URI: http://strategy11.com
 Author: Strategy11
