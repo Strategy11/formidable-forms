d91ae9d7d9fd48da3426ddd5cbfba811a529b449
jwahlin <jamie@strategy11.com>
2016-10-14 11:34:14 -0700

    Update version number to 2.02.09 Update readme

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -10,7 +10,7 @@ class FrmAppHelper {
 	/**
 	 * @since 2.0
 	 */
-	public static $plug_version = '2.02.08';
+	public static $plug_version = '2.02.09';
 
     /**
      * @since 1.07.02
--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.02.08
+Version: 2.02.09
 Plugin URI: http://formidablepro.com/
 Author URI: http://strategy11.com
 Author: Strategy11
