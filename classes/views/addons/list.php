bf9b8899db2b5f84cdf5046a1e481a74dc37b437
Steph <steph@strategy11.com>
2016-12-07 16:05:03 -0700

    Work on https://github.com/Strategy11/formidable/issues/1402 Add doc links on the Formidable -> Addons page Use static content instead of API

--- classes/views/addons/list.php
+++ classes/views/addons/list.php
@@ -2,45 +2,32 @@
 	<h1><?php _e( 'Formidable AddOns', 'formidable' ) ?></h1>
 
 	<div id="the-list" class="frm-addons">
-		<?php foreach ( $addons as $addon ) {
-			if ( empty( $addon['info']['excerpt'] ) || empty( $addon['licensing']['enabled'] ) ) {
-				continue;
-			}
-
-			if ( isset( $plugin_names[ $addon['info']['slug'] ] ) ) {
-				$installed = is_dir( WP_PLUGIN_DIR . '/' . $plugin_names[ $addon['info']['slug'] ] );
-			} else {
-				$installed = isset( $installed_addons[ $addon['info']['slug'] ] ) || is_dir( WP_PLUGIN_DIR . '/formidable-' . $addon['info']['slug'] );
-			}
-			$has_thumbnail = ! empty( $addon['info']['thumbnail'] );
-			if ( $addon['info']['slug'] == 'formidable-pro' ) {
-				$addon['info']['link'] = $pro_link;
-			}
-			$addon['info']['link'] = FrmAppHelper::make_affiliate_url( $addon['info']['link'] );
-
-		?>
-			<div class="plugin-card plugin-card-<?php echo esc_attr( $addon['info']['slug'] ) ?> <?php echo esc_attr( $has_thumbnail ? '' : 'frm-no-thumb' ) ?>">
+		<?php foreach ( $addons as $slug => $addon ) { ?>
+			<div class="plugin-card plugin-card-<?php echo esc_attr( $slug ) ?> frm-no-thumb">
 				<div class="plugin-card-top">
 					<div class="name column-name">
 						<h3>
-							<a href="<?php echo esc_url( $addon['info']['link'] ) ?>">
-								<?php echo esc_html( $addon['info']['title'] ) ?>
-								<?php if ( $has_thumbnail ) { ?>
-								<img src="<?php echo esc_url( $addon['info']['thumbnail'] ) ?>" class="plugin-icon" alt="" />
-								<?php } ?>
+							<a href="<?php echo esc_url( $site_url . $addon['link'] ) ?>">
+								<?php echo esc_html( $addon['title'] ) ?>
 							</a>
 						</h3>
 					</div>
 					<div class="action-links">
 						<ul class="plugin-action-buttons">
-							<?php if ( $installed ) { ?>
-								<li><span class="button button-disabled" title="<?php esc_attr_e( 'This plugin is already installed', 'formidable' ) ?>"><?php _e( 'Installed', 'formidable' ) ?></span></li>
+							<?php if ( $addon['installed'] ) { ?>
+								<?php if ( empty( $addon['activate_url'] ) ) { ?>
+									<li><span class="button button-disabled" title="<?php esc_attr_e( 'This plugin is already installed', 'formidable' ) ?>"><?php _e( 'Installed', 'formidable' ) ?></span></li>
+								<?php } else { ?>
+								<li><a href="<?php echo esc_url( $addon['activate_url'] ) ?>" class="button activate-now"><?php _e( 'Activate', 'formidable' ); ?></a></li>
+								<?php } ?>
+							<?php } else { ?>
+								<li><a class="install-now button" href="<?php echo esc_url( $site_url . $addon['link'] ) ?>" target="_blank" aria-label="<?php esc_attr_e( 'Get Started', 'formidable' ) ?>"><?php _e( 'Get Started', 'formidable' ) ?></a></li>
 							<?php } ?>
-							<li><a href="<?php echo esc_url( $addon['info']['link'] ) ?>" target="_blank" aria-label="<?php esc_attr_e( 'More Details', 'formidable' ) ?>"><?php _e( 'More Details', 'formidable' ) ?></a></li>
+							<li><a href="<?php echo esc_url( $site_url . 'knowledgebase/' . $addon['docs'] ) ?>" target="_blank" aria-label="<?php esc_attr_e( 'View Docs', 'formidable' ) ?>"><?php _e( 'View Docs', 'formidable' ) ?></a></li>
 						</ul>
 					</div>
 					<div class="desc column-description">
-						<p><?php echo wp_kses_post( $addon['info']['excerpt'] ) ?></p>
+						<p><?php echo wp_kses_post( $addon['excerpt'] ) ?></p>
 					</div>
 				</div>
 			</div>
