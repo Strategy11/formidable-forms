573a52caaf607a711c00a948bce3e04dde337482
jwahlin <jamie@strategy11.com>
2016-12-13 12:32:42 -0800

    Fix PHP 7.1 errors https://github.com/Strategy11/formidable/issues/1419

---
+++ classes/helpers/FrmShortcodeHelper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @since 2.02.12
+ */
+
+class FrmShortcodeHelper{
+
+	/**
+	 * Get the shortcode attributes in key/value pairs from a string
+	 *
+	 * @since 2.02.12
+	 * @param string $text
+	 *
+	 * @return array
+	 */
+	public static function get_shortcode_attribute_array( $text ) {
+		$atts = array();
+		if ( $text !== '' ) {
+			$atts = shortcode_parse_atts( $text );
+		}
+
+		if ( ! is_array( $atts ) ) {
+			$atts = array();
+		}
+
+		return $atts;
+	}
+}
