b3b06af2523c3df01b26f34fb3471c13457784a5
jwahlin <jamie@strategy11.com>
2017-01-11 17:59:33 -0800

    Update version number to 2.02.14b1

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable Forms
 Description: Quickly and easily create drag-and-drop forms
-Version: 2.02.13
+Version: 2.02.14b1
 Plugin URI: https://formidableforms.com/
 Author URI: http://strategy11.com
 Author: Strategy11
