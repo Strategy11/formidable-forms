07b3f640f4373d500eb7c5f906e5b743bec07337
Steph <steph@strategy11.com>
2017-01-25 12:27:21 -0700

    adjust https://github.com/Strategy11/formidable/issues/1374

--- classes/controllers/FrmEntriesController.php
+++ classes/controllers/FrmEntriesController.php
@@ -121,7 +121,9 @@ class FrmEntriesController {
 					$col_id .= '-_-form' . $form_col->form_id;
 				}
 
-				if ( isset($form_col->field_options['separate_value']) && $form_col->field_options['separate_value'] && empty($form_col->field_options['post_field']) ) {
+				$has_separate_value = ! FrmField::is_option_empty( $form_col, 'separate_value' );
+				$is_not_post_status = FrmField::is_option_empty( $form_col, 'post_field' );
+				if ( $has_separate_value && $is_not_post_status ) {
 					$columns[ $form_id . '_frmsep_' . $col_id ] = FrmAppHelper::truncate( $form_col->name, 35 );
 				}
 				$columns[ $form_id . '_' . $col_id ] = FrmAppHelper::truncate( $form_col->name, 35 );
