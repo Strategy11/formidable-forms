c6223f83594e165e15dba465072191c20b182a7a
Steph <steph@strategy11.com>
2017-01-25 15:42:51 -0700

    add the js back to the head https://github.com/Strategy11/formidable/issues/1491

--- js/formidable.js
+++ js/formidable.js
@@ -4341,8 +4341,6 @@ jQuery(document).ready(function($){
 	frmFrontForm.init();
 });
 
-document.documentElement.className += ' js';
-
 function frmRecaptcha() {
 	var captchas = jQuery('.frm-g-recaptcha');
 	for ( var c = 0, cl = captchas.length; c < cl; c++ ) {
