3fe5749e695ca31ad9976626cf40d95b462e16fd
jwahlin <jamie@strategy11.com>
2017-03-16 11:58:27 -0700

    Update version numbers for deprecated messages Remove FrmField::getIds() deprecated function Fix a couple of return errors

--- classes/models/FrmDbDeprecated.php
+++ classes/models/FrmDbDeprecated.php
@@ -3,7 +3,7 @@
 /**
  * Class FrmDbDeprecated
  *
- * @since 2.03.04
+ * @since 2.03.05
  */
 class FrmDbDeprecated {
 
@@ -12,12 +12,7 @@ class FrmDbDeprecated {
 	var $entries;
 	var $entry_metas;
 
-	/**
-	 * @deprecated 2.03.04
-	 */
 	public function __construct() {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'new FrmDb()' );
-
 		if ( ! defined( 'ABSPATH' ) ) {
 			die( 'You are not allowed to call this page directly.' );
 		}
@@ -30,110 +25,110 @@ class FrmDbDeprecated {
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public function upgrade( $old_db_version = false ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::upgrade( $old_db_version )' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::upgrade( $old_db_version )' );
 
 		$db = new FrmDb();
 		$db->upgrade( $old_db_version );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public function collation() {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::collation()' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::collation()' );
 
 		$db = new FrmDb();
 		$db->collation();
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public function uninstall() {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::uninstall()' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::uninstall()' );
 
 		$db = new FrmDb();
 		$db->uninstall();
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_where_clause_and_values( &$args, $starts_with = ' WHERE ' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_where_clause_and_values( $args, $starts_with )' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_where_clause_and_values( $args, $starts_with )' );
 		FrmDb::get_where_clause_and_values( $args, $starts_with );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function parse_where_from_array( $args, $base_where, &$where, &$values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::parse_where_from_array( $args, $base_where, $where, $values )' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::parse_where_from_array( $args, $base_where, $where, $values )' );
 		FrmDb::parse_where_from_array( $args, $base_where, $where, $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_count( $table, $where = array(), $args = array() ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_count' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_count' );
 
 		return FrmDb::get_count( $table, $where, $args );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_var( $table, $where = array(), $field = 'id', $args = array(), $limit = '', $type = 'var' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_var' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_var' );
 
 		return FrmDb::get_var( $table, $where, $field, $args, $limit, $type );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_col( $table, $where = array(), $field = 'id', $args = array(), $limit = '' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_col' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_col' );
 
 		return FrmDb::get_col( $table, $where, $field, $args, $limit );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_row( $table, $where = array(), $fields = '*', $args = array() ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_row' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_row' );
 
 		return FrmDb::get_row( $table, $where, $fields, $args );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_results( $table, $where = array(), $fields = '*', $args = array() ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_results' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_results' );
 
 		return FrmDb::get_results( $table, $where, $fields, $args );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function append_where_is( $where_is ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::append_where_is' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::append_where_is' );
 
 		return FrmDb::append_where_is( $where_is );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_associative_array_results( $columns, $table, $where ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmDb::get_associative_array_results' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmDb::get_associative_array_results' );
 
 		return FrmDb::get_associative_array_results( $columns, $table, $where );
 	}
--- classes/models/FrmEntryDeprecated.php
+++ classes/models/FrmEntryDeprecated.php
@@ -1,177 +1,177 @@
 <?php
 
 /**
- * @since 2.03.04
+ * @since 2.03.05
  */
 class FrmEntryDeprecated {
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function create( $values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::create' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::create' );
 
 		return FrmEntry::create( $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function is_duplicate( $new_values, $values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::is_duplicate' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::is_duplicate' );
 
 		return FrmEntry::is_duplicate( $new_values, $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function duplicate( $id ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::duplicate' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::duplicate' );
 
 		return FrmEntry::duplicate( $id );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function update( $id, $values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::update' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::update' );
 
 		return FrmEntry::update( $id, $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function destroy( $id ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::destroy' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::destroy' );
 
 		return FrmEntry::destroy( $id );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function update_form( $id, $value, $form_id ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::update_form' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::update_form' );
 
 		return FrmEntry::update_form( $id, $value, $form_id );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function clear_cache() {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::clear_cache' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::clear_cache' );
 
 		FrmEntry::clear_cache();
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_new_entry_name( $values, $default = '' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::get_new_entry_name' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::get_new_entry_name' );
 
 		return FrmEntry::get_new_entry_name( $values, $default );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function maybe_get_entry( &$entry ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::maybe_get_entry' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::maybe_get_entry' );
 
 		FrmEntry::maybe_get_entry( $entry );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function getOne( $id, $meta = false ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::getOne' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::getOne' );
 
 		return FrmEntry::getOne( $id, $meta );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_meta( $entry ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::get_meta' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::get_meta' );
 
 		return FrmEntry::get_meta( $entry );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function exists( $id ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::exists' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::exists' );
 
 		return FrmEntry::exists( $id );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function getAll( $where, $order_by = '', $limit = '', $meta = false, $inc_form = true ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::getAll' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::getAll' );
 
 		return FrmEntry::getAll( $where, $order_by, $limit, $meta, $inc_form );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function getRecordCount( $where = '' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::getRecordCount' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::getRecordCount' );
 
 		return FrmEntry::getRecordCount( $where );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function getPageCount( $p_size, $where = '' ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::getPageCount' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::getPageCount' );
 
 		return FrmEntry::getPageCount( $p_size, $where );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function sanitize_entry_post( &$values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::sanitize_entry_post' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::sanitize_entry_post' );
 
 		FrmEntry::sanitize_entry_post( $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function create_entry_from_xml( $values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::create_entry_from_xml' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::create_entry_from_xml' );
 
 		return FrmEntry::create_entry_from_xml( $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function update_entry_from_xml( $id, $values ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::update_entry_from_xml ' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::update_entry_from_xml ' );
 
 		return FrmEntry::update_entry_from_xml( $id, $values );
 	}
 
 	/**
-	 * @deprecated 2.03.04
+	 * @deprecated 2.03.05
 	 */
 	public static function get_id_by_key( $key ) {
-		_deprecated_function( __FUNCTION__, '2.03.04', 'FrmEntry::get_id_by_key' );
+		_deprecated_function( __FUNCTION__, '2.03.05', 'FrmEntry::get_id_by_key' );
 
 		return FrmEntry::get_id_by_key( $key );
 	}
