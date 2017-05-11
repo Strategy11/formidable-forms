6dabd2eb8604ad286f2af1014e96b0b040fa4da2
jwahlin <jamie@strategy11.com>
2017-05-11 12:22:58 -0700

    Decrease size of file path for automatic updates https://github.com/Strategy11/formidable/issues/1594

--- classes/controllers/FrmHooksController.php
+++ classes/controllers/FrmHooksController.php
@@ -50,6 +50,7 @@ class FrmHooksController {
 
         add_action( 'plugins_loaded', 'FrmAppController::load_lang' );
         add_filter( 'widget_text', 'FrmAppController::widget_text_filter', 8 );
+		add_filter( 'upgrader_pre_download', 'FrmAppController::add_shorten_edd_filename_filter', 10, 4 );
 
         // Entries controller
         add_action( 'wp_loaded', 'FrmEntriesController::process_entry', 10, 0 );
