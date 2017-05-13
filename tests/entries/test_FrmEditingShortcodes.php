9d9c7a2fcf13a9f58977b21f81133b2dac67d471
jwahlin <jamie@strategy11.com>
2017-05-12 18:24:47 -0700

    Add more subdirectories for better unit test organization Add more tests for update link https://github.com/Strategy11/formidable/issues/1302

--- tests/entries/test_FrmEditingShortcodes.php
+++ tests/entries/test_FrmEditingShortcodes.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @group editshortcodes
+ * @group edit-shortcodes
  * @group entries
  * @group pro
  * @group entry-editing
@@ -12,6 +12,7 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
 	public function setUp() {
 		parent::setUp();
 
+		unset( $GLOBALS['frm_update_link_count'] );
 	}
 
 	/**
@@ -23,27 +24,21 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
      *
 	 */
 	public function test_entry_update_link_with_yes() {
-
         $this->set_current_user_to_1();
 
-		$entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-		$field_id = FrmField::get_id_by_key( 'rsvp' );
-		$this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-		$this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
 
-		$test_value    = 'Yes';
 		$atts          = array(
 			'id'       => $entry_id,
 			'field_id' => $field_id,
-			'value'    => $test_value,
+			'value'    => 'Yes',
 		);
+
 		$update_link   = FrmProEntriesController::entry_update_field( $atts );
-		$expected_link = '<a href="#" onclick="frmUpdateField(';
-		$expected_link .= "{$entry_id},{$field_id}";
-		$expected_link .= ",'{$test_value}','',1);return false;\"";
-		$expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
-		$expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
-		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
+		$expected_link = $this->generate_expected_link( $atts );
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $atts['value'] );
 
 	}
 
@@ -53,11 +48,11 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
 	 * @covers FrmProEntriesController::entry_update_field
 	 */
 	public function test_entry_update_link_with_apostrophe() {
-
         $this->set_current_user_to_1();
 
-		$entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-		$field_id = FrmField::get_id_by_key( 'rsvp' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
+
 		$this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
 		$this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
 		$test_value    = "don't know";
@@ -88,10 +83,8 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
 
         $this->set_current_user_to_1();
 
-        $entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-        $field_id = FrmField::get_id_by_key( 'rsvp' );
-        $this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-        $this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
 
         $test_value    = 'No';
         $atts          = array(
@@ -99,8 +92,10 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
             'field_id' => $field_id,
             'value'    => $test_value,
         );
+
         $update_link   = FrmProEntriesController::entry_update_field( $atts );
-        $expected_link = null;
+        $expected_link = '';
+
         $this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value.' when it is the same as the stored value.' );
 
     }
@@ -114,10 +109,9 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
 
         $this->set_current_user_to_1();
 
-        $entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-        $field_id = FrmField::get_id_by_key( 'rsvp' );
-        $this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-        $this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
+
         $test_value    = "maybe yes & maybe no";
         $atts          = array(
             'id'       => $entry_id,
@@ -130,6 +124,7 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
         $expected_link .= ",'maybe yes &amp; maybe no','',1);return false;\"";
         $expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
         $expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
+
         $this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
 
     }
@@ -143,22 +138,23 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
 
         $this->set_current_user_to_1();
 
-        $entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-        $field_id = FrmField::get_id_by_key( 'rsvp' );
-        $this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-        $this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
+
         $test_value    = "<strong>Absolutely!</strong>";
         $atts          = array(
             'id'       => $entry_id,
             'field_id' => $field_id,
             'value'    => $test_value,
         );
+
         $update_link   = FrmProEntriesController::entry_update_field( $atts );
         $expected_link = '<a href="#" onclick="frmUpdateField(';
         $expected_link .= "{$entry_id},{$field_id}";
         $expected_link .= ",'&lt;strong&gt;Absolutely!&lt;/strong&gt;','',1);return false;\"";
         $expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
         $expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
+
         $this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
 
     }
@@ -173,11 +169,8 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
      */
     public function test_entry_update_link_without_editing_permission() {
 
-
         $entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
         $field_id = FrmField::get_id_by_key( 'rsvp' );
-        $this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-        $this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
 
         $test_value    = 'Yes';
         $atts          = array(
@@ -185,43 +178,135 @@ class WP_Test_FrmEditingShortcodes extends FrmUnitTest {
             'field_id' => $field_id,
             'value'    => $test_value,
         );
-        $update_link   = FrmProEntriesController::entry_update_field( $atts );
-        $expected_link = null;
-        $this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
+
+		$update_link   = FrmProEntriesController::entry_update_field( $atts );
+        $expected_link = '';
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
 
     }
 
-    /**
-     * tests update field with a simple, single word, no permission to edit, but allow is set to "everyone"
-     *
-     * @group updateyes
-     *
-     * @covers FrmProEntriesController::entry_update_field
-     *
-     */
-    public function test_entry_update_link_with_allow_everyone() {
+	/**
+	 * Tests update link with a class added
+	 *
+	 * @covers FrmProEntriesController::entry_update_field
+	 *
+	 */
+	public function test_entry_update_link_with_class() {
 
+		$this->set_current_user_to_1();
 
-        $entry_id = FrmEntry::get_id_by_key( 'steve_conference' );
-        $field_id = FrmField::get_id_by_key( 'rsvp' );
-        $this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
-        $this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
 
-        $test_value    = 'Yes';
-        $atts          = array(
-            'id'       => $entry_id,
-            'field_id' => $field_id,
-            'value'    => $test_value,
-            'allow'    => 'everyone',
-        );
-        $update_link   = FrmProEntriesController::entry_update_field( $atts );
-        $expected_link = '<a href="#" onclick="frmUpdateField(';
-        $expected_link .= "{$entry_id},{$field_id}";
-        $expected_link .= ",'{$test_value}','',1);return false;\"";
-        $expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
-        $expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
-        $this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value .' with allow set to everyone.');
+		$test_value    = 'Yes';
+		$atts          = array(
+			'id'       => $entry_id,
+			'field_id' => $field_id,
+			'value'    => $test_value,
+			'class'	   => 'my_custom_class invalid&character',
+		);
 
-    }
+		$update_link   = FrmProEntriesController::entry_update_field( $atts );
+		$expected_link = '<a href="#" onclick="frmUpdateField(';
+		$expected_link .= "{$entry_id},{$field_id}";
+		$expected_link .= ",'{$test_value}','',1);return false;\"";
+		$expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
+		$expected_link .= ' class="frm_update_field_link my_custom_class invalid&amp;character" title="Update">Update</a>';
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
+	}
+
+	/**
+	 * Tests two update links
+	 *
+	 * @covers FrmProEntriesController::entry_update_field
+	 *
+	 */
+	public function test_two_update_links() {
+
+		$this->set_current_user_to_1();
+
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
+
+		$test_value    = 'Yes';
+		$atts          = array(
+			'id'       => $entry_id,
+			'field_id' => $field_id,
+			'value'    => $test_value,
+		);
+		$update_link   = FrmProEntriesController::entry_update_field( $atts );
+		$expected_link = $this->generate_expected_link( $atts );
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
+
+		// Second link
+		$atts['value'] = "Yes, ma'am";
+
+		$update_link   = FrmProEntriesController::entry_update_field( $atts );
+		$expected_link = '<a href="#"';
+		$expected_link .= " onclick=\"frmUpdateField({$entry_id},{$field_id},'Yes, ma\'am','',2);return false;\"";
+		$expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_2\"";
+		$expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $atts['value'] );
+	}
+
+	/**
+	 * Tests update link with a message added
+	 *
+	 * @covers FrmProEntriesController::entry_update_field
+	 *
+	 */
+	public function test_entry_update_link_with_quotes_in_message() {
+		$this->set_current_user_to_1();
+
+		$entry_id = $this->get_entry_id( 'steve_conference' );
+		$field_id = $this->get_field_id( 'rsvp' );
+
+		$test_value    = 'Yes';
+		$atts          = array(
+			'id'       => $entry_id,
+			'field_id' => $field_id,
+			'value'    => $test_value,
+			'message'  => 'Congratulations! You\'ve done a great job.',
+		);
+
+		$update_link   = FrmProEntriesController::entry_update_field( $atts );
+
+		$expected_link = '<a href="#" onclick="frmUpdateField(';
+		$expected_link .= "{$entry_id},{$field_id}";
+		$expected_link .= ",'{$test_value}','Congratulations! You\'ve done a great job.',1);return false;\"";
+		$expected_link .= " id=\"frm_update_field_{$entry_id}_{$field_id}_1\"";
+		$expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
+
+		$this->assertSame( $expected_link, $update_link, 'Update link is not equal to expected link for ' . $test_value );
+	}
+
+	private function get_entry_id( $entry_key ) {
+		$entry_id = FrmEntry::get_id_by_key( $entry_key );
+		$this->assertTrue( is_numeric( $entry_id ), 'Entry ID for steve conference is not valid.' );
+
+		return $entry_id;
+	}
+
+	private function get_field_id( $field_key ) {
+		$field_id = FrmField::get_id_by_key( $field_key );
+		$this->assertTrue( is_numeric( $field_id ), 'Field ID for rsvp is not valid.' );
+
+		return $field_id;
+	}
+
+	private function generate_expected_link( $atts ) {
+		$expected_link = '<a href="#" onclick="frmUpdateField(';
+		$expected_link .= $atts['id'] . ',' . $atts['field_id'];
+		$expected_link .= ",'{$atts['value']}','',1);return false;\"";
+		$expected_link .= " id=\"frm_update_field_{$atts['id']}_{$atts['field_id']}_1\"";
+		$expected_link .= ' class="frm_update_field_link " title="Update">Update</a>';
+
+		return $expected_link;
+
+	}
 
 }
