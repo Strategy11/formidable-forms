3a55aa57edc53bad3ca8706af2830789cfc37f75
Steph <steph@strategy11.com>
2017-08-01 14:36:37 -0600

    Adjust pro call to actions https://github.com/Strategy11/formidable/issues/1643

--- classes/views/frm-settings/license_box.php
+++ classes/views/frm-settings/license_box.php
@@ -2,10 +2,11 @@
 <?php if ( ! is_multisite() || current_user_can( 'setup_network' ) ) { ?>
     <div class="postbox">
         <div class="inside">
-			<p class="alignright"><?php printf( __( '%1$sClick here%2$s to get it now', 'formidable' ), '<a href="' . esc_url( FrmAppHelper::make_affiliate_url( 'https://formidableforms.com' ) ) . '">', '</a>' ) ?> &#187;</p>
-			<p><?php _e( 'Ready to take your forms to the next level?<br/>Formidable Forms will help you create views, manage data, and get reports.', 'formidable' ) ?></p>
+			<p><?php _e( 'Get 15 more field types, including multiple file upload fields and cascading lookup fields. PLUS multi-page forms, calculations, repeatable sections, confirmation fields, conditional logic, front-end editing, views, data management, and graph & stat reporting.', 'formidable' ) ?>
+			<?php printf( __( '%1$sClick here%2$s to go Pro.', 'formidable' ), '<a href="' . esc_url( FrmAppHelper::make_affiliate_url( 'https://formidableforms.com' ) ) . '">', '</a>' ) ?>
+			</p>
 
-			<p>Already signed up? <a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( 'https://formidableforms.com/knowledgebase/install-formidable-forms/' ) ) ?>" target="_blank"><?php _e( 'Click here', 'formidable' ) ?></a> to get installation instructions and download the pro version.</p>
+			<p>Already have a Pro license? <a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( 'https://formidableforms.com/knowledgebase/install-formidable-forms/' ) ) ?>" target="_blank"><?php _e( 'Click here', 'formidable' ) ?></a> to get installation instructions and download the pro version.</p>
         </div>
     </div>
 <?php } ?>
