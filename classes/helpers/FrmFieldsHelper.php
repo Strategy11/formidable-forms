efdb6ad6e564446cae5329059317295edecc83bf
Steph <steph@strategy11.com>
2017-09-08 12:21:52 -0600

    set dependency on reCaptcha script to ensure it loads later

--- classes/helpers/FrmFieldsHelper.php
+++ classes/helpers/FrmFieldsHelper.php
@@ -534,7 +534,7 @@ DEFAULT_HTML;
 		}
 		$api_js_url = apply_filters( 'frm_recaptcha_js_url', $api_js_url );
 
-        wp_register_script( 'recaptcha-api', $api_js_url, '', true );
+        wp_register_script( 'recaptcha-api', $api_js_url, array( 'formidable' ), true );
         wp_enqueue_script( 'recaptcha-api' );
 
 		// for reverse compatibility
