9a303ee0cab02a56b9914d955a997757e837d5cb
John James <sojohnnysaid@gmail.com>
2017-09-28 16:16:34 -0400

    fixed tab issue on line 76

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -73,7 +73,7 @@ function frmAdminBuildJS(){
 			show='#'+deleteButton.closest('td').children('.frm_add_logic_link').attr('id');
 		}else if(id.indexOf('frm_posttax_') === 0){
             if(jQuery('#frm_posttax_rows .frm_posttax_row').length<2)
-				show='.frm_add_posttax_row.button';
+            	show='.frm_add_posttax_row.button';
 		}else if(id.indexOf('frm_postmeta_') === 0){
 			if(jQuery('#frm_postmeta_rows .frm_postmeta_row').length<2){
 				hide='#postcustomstuff';
