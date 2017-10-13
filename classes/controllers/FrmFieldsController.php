04758a9950429debca1ae6928212a9ac6f71073b
jwahlin <jamie@strategy11.com>
2017-10-12 21:26:03 -0600

    Keep same form ID when duplicating a field

--- classes/controllers/FrmFieldsController.php
+++ classes/controllers/FrmFieldsController.php
@@ -164,7 +164,7 @@ class FrmFieldsController {
 		do_action( 'frm_duplicate_field_' . $copy_field->type, $copy_field, $form_id );
 
 		$values = array();
-		FrmFieldsHelper::fill_field( $values, $copy_field, $form_id );
+		FrmFieldsHelper::fill_field( $values, $copy_field, $copy_field->form_id );
 		$values = apply_filters( 'frm_prepare_single_field_for_duplication', $values );
 
 		$field_id = FrmField::create( $values );
