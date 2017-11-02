49a9476f05c64b3896b9dc2b76daebd5ed692371
Steph <steph@strategy11.com>
2017-11-02 12:19:30 -0600

    get rid of two-column classes on entries listing page

--- classes/views/frm-entries/list.php
+++ classes/views/frm-entries/list.php
@@ -8,26 +8,17 @@
 
 	?>
 	<div class="wrap">
-
-    <form id="posts-filter" method="get">
-        <div id="poststuff">
-            <div id="post-body" class="metabox-holder columns-2">
-            <div id="postbox-container-1" class="postbox-container">
-                <input type="hidden" name="page" value="formidable-entries" />
-				<input type="hidden" name="form" value="<?php echo esc_attr( $form ? $form->id : '' ); ?>" />
-                <input type="hidden" name="frm_action" value="list" />
-                <?php $wp_list_table->search_box( __( 'Search', 'formidable' ), 'entry' ); ?>
-            </div>
-            <div class="clear"></div>
-            </div>
+		<form id="posts-filter" method="get">
+			<input type="hidden" name="page" value="formidable-entries" />
+			<input type="hidden" name="form" value="<?php echo esc_attr( $form ? $form->id : '' ); ?>" />
+			<input type="hidden" name="frm_action" value="list" />
+			<?php $wp_list_table->search_box( __( 'Search', 'formidable' ), 'entry' ); ?>
 
 			<?php include( FrmAppHelper::plugin_path() . '/classes/views/shared/errors.php' ); ?>
 			<?php FrmTipsHelper::pro_tip( 'get_entries_tip' ); ?>
 
-            <?php $wp_list_table->display(); ?>
-
-        </div>
-    </form>
+			<?php $wp_list_table->display(); ?>
+		</form>
 
 	</div>
 </div>
