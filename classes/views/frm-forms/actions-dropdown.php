b8f915a96a507b0030b4909caa4c221612454ccb
Steph <steph@strategy11.com>
2017-11-09 19:19:02 -0700

    switch to a better icon for the actions dropdown

--- classes/views/frm-forms/actions-dropdown.php
+++ classes/views/frm-forms/actions-dropdown.php
@@ -1,6 +1,6 @@
 <div class="frm_actions_dropdown dropdown <?php echo esc_attr( is_rtl() ? 'pull-right' : 'pull-left' ) ?>">
-	<a href="#" id="frm-navbarDrop" class="frm-dropdown-toggle frm-dots frm_icon_font frm_ellipsis-h_icon" data-toggle="dropdown" title="<?php esc_attr_e( 'Show options', 'formidable' ) ?>"></a>
-	<ul class="frm-dropdown-menu frm-on-top" role="menu" aria-labelledby="frm-navbarDrop">
+	<a href="#" id="frm-actionsDrop" class="frm-dropdown-toggle frm_icon_font frm-option-icon" data-toggle="dropdown" title="<?php esc_attr_e( 'Show options', 'formidable' ) ?>"></a>
+	<ul class="frm-dropdown-menu frm-on-top" role="menu" aria-labelledby="frm-actionsDrop">
 		<?php foreach ( $links as $link ) { ?>
 		<li>
 			<a href="<?php echo esc_url( $link['url'] ); ?>" tabindex="-1" <?php
