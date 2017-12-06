6acf9f8c610473ab300a263c58df35608b9662ed
Steph <steph@strategy11.com>
2017-12-06 12:17:38 -0700

    More from last merge

--- classes/views/frm-fields/back-end/html-content.php
+++ classes/views/frm-fields/back-end/html-content.php
@@ -1,5 +1,5 @@
 <tr>
-	<td colspan="2"><?php _e( 'Content', 'formidable-pro' ) ?><br/>
+	<td colspan="2"><?php esc_html_e( 'Content', 'formidable' ) ?><br/>
 		<textarea name="field_options[description_<?php echo absint( $field['id'] ) ?>]" class="frm_98_width" rows="8"><?php
 	echo FrmAppHelper::esc_textarea( $field['description'] );
 	?></textarea>
