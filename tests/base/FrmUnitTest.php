63717f54501e49d8d9d2b3444e76278aadb0d1f1
Steph <steph@strategy11.com>
2017-12-16 22:33:18 -0700

    switch back to SHOW TABLES instead of DESCRIBE for unit tests

--- tests/base/FrmUnitTest.php
+++ tests/base/FrmUnitTest.php
@@ -21,15 +21,6 @@ class FrmUnitTest extends WP_UnitTestCase {
 	function setUp() {
 		parent::setUp();
 
-		global $wp_version;
-		if ( $wp_version <= 4.6 ) {
-			// Prior to WP 4.7, the Formidable tables were deleted on tearDown and not restored with setUp
-			delete_option('frm_options');
-			delete_option('frm_db_version');
-		}
-
-		$this->empty_tables();
-
 		if ( is_multisite() ) {
 			$this->is_pro_active = get_site_option( 'frmpro-authorized' );
 		} else {
@@ -45,26 +36,31 @@ class FrmUnitTest extends WP_UnitTestCase {
 		$this->create_users();
 	}
 
+	function tearDown() {
+		parent::tearDown();
+
+		global $wp_version;
+		if ( $wp_version <= 4.6 ) {
+			// Prior to WP 4.7, the Formidable tables were deleted on tearDown and not restored with setUp
+			delete_option( 'frm_options' );
+			delete_option( 'frm_db_version' );
+		}
+
+		$this->empty_tables();
+	}
+
 	/**
 	 * Some of the tests for FrmDb are triggering a transaction commit, preventing further tests from working.
 	 * This is a temporary workaround until we review FrmDb tests in detail.
 	 */
 	private function empty_tables() {
 		global $wpdb;
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}frm_fields'" ) ) {
-			$wpdb->query( "TRUNCATE {$wpdb->prefix}frm_fields" );
-		}
-
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}frm_forms'" ) ) {
-			$wpdb->query( "TRUNCATE {$wpdb->prefix}frm_forms" );
-		}
-
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}frm_item_metas'" ) ) {
-			$wpdb->query( "TRUNCATE {$wpdb->prefix}frm_item_metas" );
-		}
-
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}frm_items'" ) ) {
-			$wpdb->query( "TRUNCATE {$wpdb->prefix}frm_items" );
+		$tables = $this->get_table_names();
+		foreach ( $tables as $table ){
+			$exists = $wpdb->get_var( 'DESCRIBE ' . $table );
+			if ( $exists ) {
+				$wpdb->query( "TRUNCATE $table" );
+			}
 		}
 	}
 
@@ -103,12 +99,10 @@ class FrmUnitTest extends WP_UnitTestCase {
 
 	function do_tables_exist( $should_exist = true ) {
 		global $wpdb;
-		$method = $should_exist ? 'assertEquals' : 'assertNotEquals';
+		$method = $should_exist ? 'assertNotEmpty' : 'assertEmpty';
 		foreach ( $this->get_table_names() as $table_name ) {
-			$this->$method(
-				$table_name, $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) ) ),
-				$table_name . ' table failed to ' . ( $should_exist ? 'install' : 'uninstall' )
-			);
+			$message = $table_name . ' table failed to ' . ( $should_exist ? 'install' : 'uninstall' );
+			$this->$method( $wpdb->query( 'DESCRIBE ' . $table_name ), $message );
 		}
 	}
 
