f9f8b760b3639be9d92aec087347d9aa6b5fdf2f
Steph <steph@strategy11.com>
2018-01-22 10:31:59 -0700

    Add frm-fade-in class for use in forms https://github.com/Strategy11/formidable/issues/1802

--- css/custom_theme.css.php
+++ css/custom_theme.css.php
@@ -271,11 +271,6 @@ legend.frm_hidden{
     animation:         spin 2s linear infinite;
 }
 
-@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-}
-
 <?php
 foreach ( $styles as $style ) {
 	include( dirname( __FILE__ ) . '/_single_theme.css.php' );
