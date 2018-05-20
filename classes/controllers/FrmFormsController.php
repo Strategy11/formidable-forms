f3e0ea58cd508aa60bec76ce448a1ac8452bcb92
Laura Steiner <steiner.laura@gmail.com>
2018-05-20 09:06:26 -0700

    Adds new action for adding submit conditions to frm_vars.

--- classes/controllers/FrmFormsController.php
+++ classes/controllers/FrmFormsController.php
@@ -1185,6 +1185,7 @@ class FrmFormsController {
 		} else if ( self::user_has_permission_to_view( $form ) ) {
 			$form = do_shortcode( $frm_settings->login_msg );
 		} else {
+			do_action( 'frm_pre_get_form', $form );
 			$form = self::get_form( $form, $title, $description, $atts );
 
 			/**
@@ -1228,7 +1229,7 @@ class FrmFormsController {
     public static function get_form( $form, $title, $description, $atts = array() ) {
         ob_start();
         
-		do_action( 'frm_before_get_form', $atts, $form );
+		do_action( 'frm_before_get_form', $atts );
 
         self::get_form_contents( $form, $title, $description, $atts );
 		self::enqueue_scripts( FrmForm::get_params( $form ) );
