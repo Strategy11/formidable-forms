bf0aa3df4bbdb653ae46ac02500e59e448294d39
Steph <steph@strategy11.com>
2018-08-06 15:45:32 -0600

    update version number remove unneeded code for setting collation

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable Forms
 Description: Quickly and easily create drag-and-drop forms
-Version: 3.03
+Version: 3.03.01
 Plugin URI: https://formidableforms.com/
 Author URI: https://formidableforms.com/
 Author: Strategy11
