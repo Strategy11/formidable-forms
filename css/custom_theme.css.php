ae922291371aab71f9eaf73f4b9dcd452d67a6db
Steph <steph@strategy11.com>
2018-09-14 16:16:34 -0600

    Remove login form css since it's duplicated https://github.com/Strategy11/formidable-registration/issues/104

--- css/custom_theme.css.php
+++ css/custom_theme.css.php
@@ -862,65 +862,6 @@ select.frm_loading_lookup{
     display:block;
 }
 
-/* Login form */
-.with_frm_style.frm_login_form,
-.with_frm_style.frm_login_form form{
-	clear:both;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login .login-remember input{
-	vertical-align:baseline;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login .login-submit{
-	float:left;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login label{
-	display:inline;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login .login-username,
-.with_frm_style.frm_login_form.frm_inline_login .login-password,
-.with_frm_style.frm_login_form.frm_inline_login .login-remember{
-	float:left;
-	margin-right:5px;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login form{
-	position:relative;
-	clear:none;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login .login-remember{
-	position:absolute;
-	top:35px;
-}
-
-.with_frm_style.frm_login_form.frm_inline_login input[type=submit]{
-	margin:0 !important;
-}
-
-.with_frm_style.frm_login_form.frm_no_labels .login-username label,
-.with_frm_style.frm_login_form.frm_no_labels .login-password label{
-	display:none;
-}
-
-.with_frm_style .frm-open-login{
-	float:left;
-	margin-right:15px;
-}
-
-.with_frm_style .frm-open-login a{
-	text-decoration:none;
-	border:none;
-	outline:none;
-}
-
-.with_frm_style.frm_slide.frm_login_form form{
-	display:none;
-}
-
 /* Fonts */
 @font-face {
 	font-family:'s11-fp';
