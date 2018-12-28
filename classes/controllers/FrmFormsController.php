0d9441890d95e6d70ed58692c8de9d0ea75dd2fd
Steph <steph@strategy11.com>
2018-12-28 09:47:33 -0700

    correctly allow svg in kses

--- classes/controllers/FrmFormsController.php
+++ classes/controllers/FrmFormsController.php
@@ -524,7 +524,7 @@ class FrmFormsController {
 			$menu_name = FrmAppHelper::get_menu_name();
 			$icon = apply_filters( 'frm_media_icon', FrmAppHelper::svg_logo() );
 			echo '<a href="#TB_inline?width=50&height=50&inlineId=frm_insert_form" class="thickbox button add_media frm_insert_form" title="' . esc_attr__( 'Add forms and content', 'formidable' ) . '">' .
-				$icon . // WPCS: XSS ok. (viewBox is stripped from HTML with FrmAppHelper::kses)
+				FrmAppHelper::kses( $icon, 'all' ) . // WPCS: XSS ok.
 				' ' . esc_html( $menu_name ) . '</a>';
 		}
 	}
