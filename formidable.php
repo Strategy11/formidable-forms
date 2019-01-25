977c419884d2c064a55d3bfea6b5ba3c206e6437
Steph <steph@strategy11.com>
2019-01-25 13:59:42 -0700

    Prepare for v3.06 release

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable Forms
 Description: Quickly and easily create drag-and-drop forms
-Version: 3.05
+Version: 3.06
 Plugin URI: https://formidableforms.com/
 Author URI: https://formidableforms.com/
 Author: Strategy11
