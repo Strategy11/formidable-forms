c0125ad44658fc36e43365bfeab79164e09887b6
Steph <steph@strategy11.com>
2019-05-24 13:21:03 -0600

    Live update for default values

--- classes/views/frm-fields/back-end/settings.php
+++ classes/views/frm-fields/back-end/settings.php
@@ -184,13 +184,13 @@ do_action( 'frm_before_field_options', $field );
 
 						if ( $display['type'] === 'textarea' || $display['type'] === 'rte' ) {
 							?>
-							<textarea name="<?php echo esc_attr( $default_name ); ?>" class="default-value-field" id="frm_default_value_<?php echo esc_attr( $field['id'] ); ?>" rows="5"><?php
+							<textarea name="<?php echo esc_attr( $default_name ); ?>" class="default-value-field" id="frm_default_value_<?php echo esc_attr( $field['id'] ); ?>" rows="5" data-changeme="field_<?php echo esc_attr( $field['field_key'] ); ?>"><?php
 								echo FrmAppHelper::esc_textarea( $default_value ); // WPCS: XSS ok.
 							?></textarea>
 							<?php
 						} else {
 							?>
-							<input type="text" name="<?php echo esc_attr( $default_name ); ?>" value="<?php echo esc_attr( $default_value ); ?>" id="frm_default_value_<?php echo esc_attr( $field['id'] ); ?>" class="default-value-field" />
+							<input type="text" name="<?php echo esc_attr( $default_name ); ?>" value="<?php echo esc_attr( $default_value ); ?>" id="frm_default_value_<?php echo esc_attr( $field['id'] ); ?>" class="default-value-field" data-changeme="field_<?php echo esc_attr( $field['field_key'] ); ?>" data-changeatt="value" />
 							<?php
 						}
 						?>
