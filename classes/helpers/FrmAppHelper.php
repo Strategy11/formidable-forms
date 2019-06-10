4f0ce60073bd8366af50417cbfbb77023011bf83
Steph <steph@strategy11.com>
2019-06-10 17:32:30 -0600

    Prepare for v4.0.04 release

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -11,7 +11,7 @@ class FrmAppHelper {
 	/**
 	 * @since 2.0
 	 */
-	public static $plug_version = '4.0.03';
+	public static $plug_version = '4.0.04';
 
 	/**
 	 * @since 1.07.02
--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable Forms
 Description: Quickly and easily create drag-and-drop forms
-Version: 4.0.03
+Version: 4.0.04
 Plugin URI: https://formidableforms.com/
 Author URI: https://formidableforms.com/
 Author: Strategy11
