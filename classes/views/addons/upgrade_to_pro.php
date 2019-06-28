466a17e5eef76282f4c52cbeffefa8b1a1887e6a
Steph <steph@strategy11.com>
2019-06-27 22:39:22 -0600

    Update the upgrade to pro page

--- classes/views/addons/upgrade_to_pro.php
+++ classes/views/addons/upgrade_to_pro.php
@@ -12,49 +12,16 @@
 	<p>Are you currently collecting data offline? Streamline your business by using your forms to get online. Whether you need surveys, polls, client contracts, mortgage calculators, or directories, we've got you covered. Save time by allowing clients to return and make changes to their own submissions, or let them contribute content to your site. Generate more leads by adding headings and page breaks, only showing the fields you need, and letting your clients repeat a section of fields as many times as they need.</p>
 	<p>Projects that once seemed impossible are within your reach with Pro. That project you’ve been dreaming of pursuing? Chances are <strong>Formidable Pro can handle it</strong>.</p><br/>
 
-	<table class="wp-list-table widefat fixed striped frm_pricing">
-		<thead>
-			<tr>
-				<th></th>
-			<?php foreach ( $pro_pricing as $price_info ) { ?>
-				<th>
-					<h3><?php echo esc_attr( ucfirst( $price_info['name'] ) ); ?></h3>
-					<a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" class="button-primary frm-button-primary" target="_blank">
-						<?php esc_html_e( 'Get Started', 'formidable' ); ?>
-					</a>
-				</th>
-			<?php } ?>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th>Number of Sites</th>
-				<td>1 Site</td>
-				<td>3 Sites</td>
-				<td>15 Sites</td>
-				<td>Unlimited Sites</td>
-			</tr>
-			<tr>
-				<th>Support Priority</th>
-				<td>Standard Support</td>
-				<td>Standard Support</td>
-				<td>Priority Support</td>
-				<td>Elite Support</td>
-			</tr>
-			<tr>
-				<th>Included Add-Ons</th>
-				<td>None</td>
-				<td><a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" target="_blank">Premium Add-Ons</a></td>
-				<td><a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" target="_blank">Advanced Add-Ons</a></td>
-				<td><a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" target="_blank">Elite Add-Ons</a></td>
-			</tr>
-		</tbody>
-	</table>
+
+	<p class="frmcenter">
+		<a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" class="button-primary frm-button-primary frm_large" target="_blank">
+			<?php esc_html_e( 'Get Started', 'formidable' ); ?>
+		</a>
+	</p>
 	<br/>
 
 	<?php do_action( 'frm_upgrade_page' ); ?>
 
-	<h2>Features</h2>
 	<table class="wp-list-table widefat fixed striped">
 		<thead>
 			<tr>
@@ -93,4 +60,11 @@
 			<?php } ?>
 		</tbody>
 	</table>
+
+	<br/>
+	<p class="frmcenter">
+		<a href="<?php echo esc_url( FrmAppHelper::make_affiliate_url( FrmAppHelper::admin_upgrade_link( $link_parts ) ) ); ?>" class="button-primary frm-button-primary frm_large" target="_blank">
+			<?php esc_html_e( 'Get Started', 'formidable' ); ?>
+		</a>
+	</p>
 </div>
