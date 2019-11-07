44e3504e1c1bd50229d0b74f5053bb1f39d58b4a
Steph <steph@strategy11.com>
2019-11-07 06:06:21 -0700

    Fix issue with excluding fields from embedded form in frm-show-entry shortcode https://github.com/Strategy11/formidable-pro/issues/1835

--- classes/models/FrmEntryValues.php
+++ classes/models/FrmEntryValues.php
@@ -245,12 +245,16 @@ class FrmEntryValues {
 	 * @return bool
 	 */
 	protected function is_field_included( $field ) {
+		$is_included = true;
 		if ( ! empty( $this->include_fields ) ) {
 			$is_included = $this->is_field_in_array( $field, $this->include_fields );
-		} elseif ( ! empty( $this->exclude_fields ) ) {
-			$is_included = ! $this->is_field_in_array( $field, $this->exclude_fields );
-		} else {
-			$is_included = true;
+		}
+
+		if ( ! empty( $this->exclude_fields ) ) {
+			$is_excluded = $this->is_field_in_array( $field, $this->exclude_fields );
+			if ( $is_excluded ) {
+				$is_included = false;
+			}
 		}
 
 		return $is_included;
