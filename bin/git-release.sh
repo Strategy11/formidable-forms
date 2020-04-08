cf901684fa6f70d0e2ff260d51758d55ea3df46f
Steph <steph@strategy11.com>
2020-04-08 15:32:02 -0600

    Prepare for v4.02.02 release

--- bin/git-release.sh
+++ bin/git-release.sh
@@ -46,11 +46,5 @@ github-release delete \
   --repo $repo \
   --tag "v$version" \
 export GIT_RELEASE_NOTES="$(git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:'%h %B')"
-github-release upload \
-	--owner Strategy11 \
-	--repo $repo \
-	--tag "v$version" \
-	--name "v$version" \
-	--body "${GIT_RELEASE_NOTES}" \
-		$attachments
+
 echo "New version created."
