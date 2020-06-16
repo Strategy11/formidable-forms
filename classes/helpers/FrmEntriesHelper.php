b224b6a912be67e3bef04a63eb2c29bd73551de4
Laura Steiner <steiner.laura@gmail.com>
2020-06-15 19:07:22 -0700

    Improves $image_option test, including Dynamic fields

--- classes/helpers/FrmEntriesHelper.php
+++ classes/helpers/FrmEntriesHelper.php
@@ -229,7 +229,7 @@ class FrmEntriesHelper {
 	 */
 	public static function display_value( $value, $field, $atts = array() ) {
 
-		$image_option = empty( $atts['dynamic'] ) ? FrmField::is_image_option( $field ) : false;
+		$image_option = empty( $atts['dynamic'] ) ? FrmField::is_image_option( $field ) : ! empty( $atts['show_image'] ) ? true: false;
 
 		$defaults = array(
 			'type'          => '',
