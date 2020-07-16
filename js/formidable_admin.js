b38a8f5266e7b7d693638f762b68200815e22f6f
Steph <steph@strategy11.com>
2020-07-16 10:03:40 -0600

    jscs

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -5627,14 +5627,14 @@ function frmAdminBuildJS() {
 
 		formData.forEach( function( value, key ) {
 			// Reflect.has in favor of: object.hasOwnProperty(key)
-			if ( ! Reflect.has( object, key ) ){
+			if ( ! Reflect.has( object, key ) ) {
 				object[key] = value;
 				return;
 			}
-			if ( ! Array.isArray( object[key] ) ) {
+			if ( ! Array.isArray( object[key]) ) {
 				object[key] = [ object[key] ];
 			}
-			object[key].push(value);
+			object[key].push( value );
 		});
 		return object;
 	}
