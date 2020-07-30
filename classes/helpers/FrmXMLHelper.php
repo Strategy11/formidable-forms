7e0b3043ae20c2d31fd54aba39d803886e50cada
Steph <steph@strategy11.com>
2020-07-29 22:59:57 -0600

    revert a change for imported form creation date

--- classes/helpers/FrmXMLHelper.php
+++ classes/helpers/FrmXMLHelper.php
@@ -224,8 +224,12 @@ class FrmXMLHelper {
 			'editable'       => (int) $item->editable,
 			'status'         => (string) $item->status,
 			'parent_form_id' => isset( $item->parent_form_id ) ? (int) $item->parent_form_id : 0,
-			'created_at'     => current_time( 'mysql', 1 ),
+			'created_at'     => gmdate( 'Y-m-d H:i:s', strtotime( (string) $item->created_at ) ),
 		);
+
+		if ( empty( $item->created_at ) ) {
+			$form['created_at'] = current_time( 'mysql', 1 );
+		}
 
 		$form['options'] = FrmAppHelper::maybe_json_decode( $form['options'] );
 
