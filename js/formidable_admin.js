94798fe6dd695981121bea55814550a77f741ffb
crabcyborg <mike@crabcyb.org>
2020-08-06 10:57:57 -0300

    missed the var declaration

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -5926,7 +5926,7 @@ function frmAdminBuildJS() {
 		},
 
 		settingsInit: function() {
-			var formSettings, $loggedIn, $cookieExp, $editable,
+			var formSettings, $loggedIn, $cookieExp, $editable, $protect_files,
 				$formActions = jQuery( document.getElementById( 'frm_notification_settings' ) );
 			//BCC, CC, and Reply To button functionality
 			$formActions.on( 'click', '.frm_email_buttons', showEmailRow );
