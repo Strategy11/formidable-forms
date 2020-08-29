9962ae6cabc257f6a62129ecad0361147ed7a260
crabcyborg <mike@crabcyb.org>
2020-08-29 00:05:06 -0300

    translate it

--- classes/views/frm-forms/accessibility.php
+++ classes/views/frm-forms/accessibility.php
@@ -1,4 +1,4 @@
 <div class="hidden">
-	<div id="frm_press_space_checked">Checked. To uncheck this option, press Space or Enter.</div>
-	<div id="frm_press_space_unchecked">Unchecked. To check this option, press Space or Enter.</div>
+	<div id="frm_press_space_checked"><?php echo esc_html__( 'Checked. To uncheck this option, press Space or Enter.', 'formidable' ); ?></div>
+	<div id="frm_press_space_unchecked"><?php echo esc_html__( 'Unchecked. To check this option, press Space or Enter.', 'formidable' ); ?></div>
 </div>
