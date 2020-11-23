3bf1b8ea6ba63ab9ecd7de94bd242d5c945e2867
crabcyborg <mike@crabcyb.org>
2020-11-23 09:27:27 -0400

    also include textarea for builder page

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -6629,7 +6629,7 @@ function frmAdminBuildJS() {
 			$builderForm.on( 'change', 'select[name^="field_options[form_select_"]', maybeChangeEmbedFormMsg );
 
 			jQuery( document ).on( 'submit', '#frm_js_build_form', buildSubmittedNoAjax );
-			jQuery( document ).on( 'change', '#frm_builder_page input:not(.frm-search-input), #frm_builder_page select', fieldUpdated );
+			jQuery( document ).on( 'change', '#frm_builder_page input:not(.frm-search-input), #frm_builder_page select, #frm_builder_page textarea', fieldUpdated );
 
 			popAllProductFields();
 
