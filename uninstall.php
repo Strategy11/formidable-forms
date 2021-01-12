508519038195b22dcf72909d0d64ed9037e293b0
Jairo Perez <jairo.preztaveras@gmail.com>
2021-01-12 05:48:29 -0400

    Create an uninstall.php file to do a cleanup when the plugin is uninstalled

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Delete Plugin Options
+delete_option( 'frm_welcome_screen_activation_redirect' );
