5a6a7550e1b73634303daab862c114ca155f191d
Mike Letellier <mike@crabcyb.org>
2021-05-26 16:54:07 -0300

    honeypot setting appears basic when off fix

--- classes/views/frm-forms/spam-settings/honeypot.php
+++ classes/views/frm-forms/spam-settings/honeypot.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<label for="honeypot"><?php esc_html_e( 'Use Honeypot to check entries for spam', 'formidable' ); ?></label>
 		<select id="honeypot" name="options[honeypot]">
 			<option value="off" <?php selected( $values['honeypot'], 'off' ); ?>><?php esc_html_e( 'Off', 'formidable' ); ?></option>
-			<option value="basic" <?php selected( ! empty( $values['honeypot'] ) && 'strict' !== $values['honeypot'], true ); ?>><?php esc_html_e( 'Basic', 'formidable' ); ?></option>
+			<option value="basic" <?php selected( $values['honeypot'], 'basic' ); ?>><?php esc_html_e( 'Basic', 'formidable' ); ?></option>
 			<option value="strict" <?php selected( $values['honeypot'], 'strict' ); ?>><?php esc_html_e( 'Strict', 'formidable' ); ?></option>
 		</select>
 	</td>
