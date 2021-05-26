1723dbfc9833a353e85a8ade7a24266948036da6
Mike Letellier <mike@crabcyb.org>
2021-05-26 17:10:50 -0300

    replace views education graphic with video

--- classes/views/shared/views-info.php
+++ classes/views/shared/views-info.php
@@ -15,7 +15,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 		);
 		?>
 		<div class="frmcenter" style="margin-top:10vh">
-			<img src="<?php echo esc_attr( FrmAppHelper::plugin_url() . '/images/views.svg' ); ?>" alt="<?php esc_attr_e( 'Create a View', 'formidable' ); ?>" width="403" height="243" />
+			<div class="frm-video-wrapper">
+				<iframe width="843" height="474" src="https://www.youtube.com/embed/3T-WMIvYadg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
 			<h2><?php esc_html_e( 'Show and Edit Entries with Views', 'formidable' ); ?></h2>
 			<p style="max-width:400px;margin:20px auto">
 				<?php esc_html_e( 'Bring entries to the front-end of your site for full-featured applications or just to show the content.', 'formidable' ); ?>
