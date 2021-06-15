18e9b4caeafd87e16ccac9238cc3a1cefc1e7eec
Truong Giang <truongwp@gmail.com>
2021-06-15 10:04:29 +0700

    Add frm_fields_in_entries_list_table filter

--- classes/controllers/FrmEntriesController.php
+++ classes/controllers/FrmEntriesController.php
@@ -110,6 +110,16 @@ class FrmEntriesController {
 	private static function get_columns_for_form( $form_id, &$columns ) {
 		$form_cols = FrmField::get_all_for_form( $form_id, '', 'include' );
 
+		/**
+		 * Allows changing fields in the Entries list table heading.
+		 *
+		 * @since 4.12
+		 *
+		 * @param array $fields  Array of fields.
+		 * @param int   $form_id Form ID.
+		 */
+		$form_cols = apply_filters( 'frm_fields_in_entries_list_table', $form_cols, $form_id );
+
 		foreach ( $form_cols as $form_col ) {
 			if ( FrmField::is_no_save_field( $form_col->type ) ) {
 				continue;
