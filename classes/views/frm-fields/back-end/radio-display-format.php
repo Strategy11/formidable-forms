6753e4d1e74ff587daccb9f564cef566934abed5
Mike <mike@crabcyb.org>
2021-08-11 11:13:04 -0300

    add a direct file access check to radio-display-format.php

--- classes/views/frm-fields/back-end/radio-display-format.php
+++ classes/views/frm-fields/back-end/radio-display-format.php
@@ -10,6 +10,9 @@
  * @var array $args    The arguments.
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
 ?>
 <div id="frm_display_format_<?php echo intval( $field['id'] ); ?>_container" class="frm_form_field">
 	<label for="frm_image_options_<?php echo intval( $field['id'] ); ?>"><?php esc_html_e( 'Display format', 'formidable' ); ?></label>
