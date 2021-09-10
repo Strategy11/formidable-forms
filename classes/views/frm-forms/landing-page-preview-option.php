43f3bdab97f572b11a17008a7d644de593623186
Mike <mike@crabcyb.org>
2021-09-10 15:40:10 -0300

    add the noallow classes

--- classes/views/frm-forms/landing-page-preview-option.php
+++ classes/views/frm-forms/landing-page-preview-option.php
@@ -3,6 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You are not allowed to call this page directly.' );
 }
 ?>
-<a href="<?php echo esc_attr( FrmAppHelper::admin_upgrade_link( 'landing-page' ) ); ?>" target="_blank">
+<a class="frm_show_upgrade frm_noallow" href="<?php echo esc_attr( FrmAppHelper::admin_upgrade_link( 'landing-page' ) ); ?>" target="_blank">
 	<?php esc_html_e( 'Generate Form Page', 'formidable' ); ?><span class="frm-new-pill"><?php esc_html_e( 'NEW', 'formidable' ); ?></span>
 </a>
