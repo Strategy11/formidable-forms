77a92a341684d4147c32e72c7364c2d59f07d942
Mike <mike@crabcyb.org>
2021-09-13 15:49:46 -0300

    tweak styles

--- classes/views/inbox/banner.php
+++ classes/views/inbox/banner.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php if ( ! empty( $message['emoji'] ) ) { ?>
 		<span class="frm-banner-emoji"><?php echo esc_html( $message['emoji'] ); ?></span>
 	<?php } ?>
-	<span class="frm-banner-title"><?php echo esc_html( $message['subject'] ); ?></span>
+	<strong class="frm-banner-title"><?php echo esc_html( $message['subject'] ); ?></strong>
 	<span class="frm-banner-content"><?php echo esc_html( $message['banner'] ); ?></span>
 	<span class="frm-banner-cta"><?php echo FrmAppHelper::kses( $message['cta'], 'all' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
 	<?php /* TODO: dismiss icon */ ?>
