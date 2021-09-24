34dd6d9b4e53df81c5a1cd44bea4b5944e14e2f1
Mike <mike@crabcyb.org>
2021-09-24 11:50:04 -0300

    fix issues with failing unit tests

--- tests/base/FrmUnitTest.php
+++ tests/base/FrmUnitTest.php
@@ -16,6 +16,11 @@ class FrmUnitTest extends WP_UnitTestCase {
 	protected $is_pro_active = false;
 
 	/**
+	 * @var FrmUnitTest $instance
+	 */
+	protected static $instance;
+
+	/**
 	 * Ensure that the plugin has been installed and activated.
 	 */
 	public static function wpSetUpBeforeClass() {
@@ -35,6 +40,7 @@ class FrmUnitTest extends WP_UnitTestCase {
 	}
 
 	public function setUp() {
+		self::$instance = $this;
 		parent::setUp();
 
 		// The JavaScript antispam check doesn't work with unit tests so turn it off.
@@ -183,8 +189,30 @@ class FrmUnitTest extends WP_UnitTestCase {
 		);
 
 		$_REQUEST['csv_files'] = 1;
+		$uploads_dir           = wp_upload_dir()['basedir'] . '/formidable/';
+		$test                  = new FrmUnitTest();
 		foreach ( $file_urls as $values ) {
-			$media_ids = FrmProFileImport::import_attachment( $values['val'], $values['field'] );
+			$vals      = (array) $values['val'];
+			$media_ids = false;
+			foreach ( $vals as $val ) {
+				$filename = basename( $val );
+				$path     = $uploads_dir . $filename;
+				if ( file_exists( $path ) ) {
+					error_log( '-- found a repeat file, saved a call to CDN. --' );
+					if ( ! is_array( $media_ids ) ) {
+						$media_ids = array();
+					}
+					$id          = $test->run_private_method( array( 'FrmProFileImport', 'attach_existing_image' ), array( $filename ) );
+					$media_ids[] = $id;
+				}
+			}
+			if ( is_array( $media_ids ) ) {
+				$media_ids = implode( ',', $media_ids );
+			}
+
+			if ( false === $media_ids ) {
+				$media_ids = FrmProFileImport::import_attachment( $values['val'], $values['field'] );
+			}
 
 			if ( is_array( $values['val'] ) ) {
 				$media_ids = explode( ',', $media_ids );
