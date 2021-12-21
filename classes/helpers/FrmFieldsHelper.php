f2c7e33107691c4f4c8193fd008859d57e53dbb1
Mike <mike@crabcyb.org>
2021-12-21 16:45:00 -0400

    do not set data-message if it is empty

--- classes/helpers/FrmFieldsHelper.php
+++ classes/helpers/FrmFieldsHelper.php
@@ -1852,8 +1852,22 @@ class FrmFieldsHelper {
 			}
 		}
 
+		$li_params = array(
+			'class'         => 'frmbutton frm6 ' . $args['no_allow_class'] . $single_no_allow . ' frm_t' . str_replace( '|', '-', $field_key ),
+			'id'            => $field_key,
+			'data-upgrade'  => $upgrade_label,
+			'data-link'     => $link,
+			'data-medium'   => 'builder',
+			'data-oneclick' => $install_data,
+			'data-content'  => $field_key,
+			'data-requires' => $requires,
+		);
+
+		if ( $upgrade_message ) {
+			$li_params['data-message'] = $upgrade_message;
+		}
 		?>
-		<li class="frmbutton frm6 <?php echo esc_attr( $args['no_allow_class'] . $single_no_allow . ' frm_t' . str_replace( '|', '-', $field_key ) ); ?>" id="<?php echo esc_attr( $field_key ); ?>" data-upgrade="<?php echo esc_attr( $upgrade_label ); ?>" data-message="<?php echo esc_attr( $upgrade_message ); ?>" data-link="<?php echo esc_attr( $link ); ?>" data-medium="builder" data-oneclick="<?php echo esc_attr( $install_data ); ?>" data-content="<?php echo esc_attr( $field_key ); ?>" data-requires="<?php echo esc_attr( $requires ); ?>">
+		<li <?php FrmAppHelper::array_to_html_params( $li_params, true ); ?>>
 		<?php
 		if ( $run_filter ) {
 			$field_label = apply_filters( 'frmpro_field_links', $field_label, $args['id'], $field_key );
