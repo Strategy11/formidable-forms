3f4b413a41bb52c59a1c30c8aee21589060a4ab6
Mike <mike@crabcyb.org>
2022-01-10 16:02:38 -0400

    add margin bottom under custom layout title, use the same gear icon

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -2024,7 +2024,7 @@ function frmAdminBuildJS() {
 		options = [ getDeleteActionOption( isFieldGroup ), getDuplicateActionOption( isFieldGroup ) ];
 		if ( ! isFieldGroup ) {
 			options.push(
-				{ class: 'frm_select', icon: 'frm_settings_icon', label: __( 'Field settings', 'formidable' ) }
+				{ class: 'frm_select', icon: 'frm_gear_icon', label: __( 'Field settings', 'formidable' ) }
 			);
 		}
 		options.forEach(
