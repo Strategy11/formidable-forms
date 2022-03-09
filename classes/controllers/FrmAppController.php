1a7a6dc610337693207391fbb9981b2c42b8349b
Mike <mike@crabcyb.org>
2022-03-09 12:53:16 -0400

    Only deregister before enqueing

--- classes/controllers/FrmAppController.php
+++ classes/controllers/FrmAppController.php
@@ -459,8 +459,6 @@ class FrmAppController {
 			$dependecies[] = 'wp-color-picker';
 		}
 
-		self::maybe_deregister_popper2();
-
 		wp_register_script( 'formidable_admin', FrmAppHelper::plugin_url() . '/js/formidable_admin.js', $dependecies, $version, true );
 		wp_register_style( 'formidable-admin', FrmAppHelper::plugin_url() . '/css/frm_admin.css', array(), $version );
 		wp_register_script( 'popper', FrmAppHelper::plugin_url() . '/js/popper.min.js', array( 'jquery' ), '1.16.0', true );
@@ -486,6 +484,7 @@ class FrmAppController {
 
 			wp_enqueue_script( 'admin-widgets' );
 			wp_enqueue_style( 'widgets' );
+			self::maybe_deregister_popper2();
 			wp_enqueue_script( 'formidable_admin' );
 			FrmAppHelper::localize_script( 'admin' );
 
@@ -521,6 +520,7 @@ class FrmAppController {
 			if ( $post_type === 'frm_display' ) {
 				wp_enqueue_style( 'formidable-grids' );
 				wp_enqueue_script( 'jquery-ui-draggable' );
+				self::maybe_deregister_popper2();
 				wp_enqueue_script( 'formidable_admin' );
 				wp_enqueue_style( 'formidable-admin' );
 				FrmAppHelper::localize_script( 'admin' );
@@ -548,7 +548,19 @@ class FrmAppController {
 		$popper = $wp_scripts->registered['popper'];
 		if ( version_compare( $popper->ver, '2.0', '>=' ) ) {
 			wp_deregister_script( 'popper' );
+			self::register_popper1();
 		}
+	}
+
+	/**
+	 * Register Popper required for Bootstrap 4.
+	 *
+	 * @since 5.2.02.01
+	 *
+	 * @return void
+	 */
+	private static function register_popper1() {
+		wp_register_script( 'popper', FrmAppHelper::plugin_url() . '/js/popper.min.js', array( 'jquery' ), '1.16.0', true );
 	}
 
 	/**
