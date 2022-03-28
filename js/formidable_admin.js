6b1a32c23389ee735b09df0474ff7b7ff144bd90
Mike <mike@crabcyb.org>
2022-03-28 13:54:19 -0300

    Drop polyfills

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -9771,19 +9771,3 @@ function frmImportCsv( formID ) {
 		}
 	});
 }
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill
-if ( ! String.prototype.trim ) {
-  String.prototype.trim = function() {
-    return this.replace( /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '' );
-  };
-}
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
-if ( ! String.prototype.startsWith ) {
-    Object.defineProperty( String.prototype, 'startsWith', {
-        value: function( search, pos ) {
-            pos = ! pos || pos < 0 ? 0 : +pos;
-            return this.substring( pos, pos + search.length ) === search;
-        }
-    });
-}
