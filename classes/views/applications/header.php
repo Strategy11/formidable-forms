59841dfcc2b6d0bd391c0d0537eea86f49a16342
Mike <mike@crabcyb.org>
2022-04-11 10:03:19 -0300

    Add upgrade button to application index header if pro is not installed

--- classes/views/applications/header.php
+++ classes/views/applications/header.php
@@ -9,6 +9,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</a>
 	<div id="frm_bs_dropdown">
 		<h1 style="width:auto;"><span><?php echo esc_html( $title ); ?></span></h1>
+		<?php if ( 'index' === $context && ! FrmAppHelper::pro_is_installed() ) { ?>
+			<?php
+			FrmAddonsController::show_conditional_action_button(
+				array(
+					'addon'        => array(
+						'slug'       => 'formidable-pro',
+						'categories' => array( 'Basic' ),
+					),
+					'license_type' => 'personal',
+					'upgrade_link' => FrmAppHelper::admin_upgrade_link(
+						array(
+							'medium'  => 'applications-header',
+							'content' => 'applications',
+						)
+					),
+				)
+			);
+			?>
+		<?php } ?>
 		<?php do_action( 'frm_applications_header_after_title', $context ); ?>
 	</div>
 	<div style="clear: both;"></div>
