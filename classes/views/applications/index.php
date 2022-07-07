cad5c24974753201dbe541a3c3872520c16d2ba1
Mike Letellier <mikeletellier@Mikes-MacBook-Air.local>
2022-07-07 10:06:08 -0300

    Also call print_admin_banner from applications index

--- classes/views/applications/index.php
+++ classes/views/applications/index.php
@@ -3,6 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You are not allowed to call this page directly.' );
 }
 FrmAppHelper::include_svg();
+FrmAppHelper::print_admin_banner( true );
 ?>
 <div class="frm_page_container frm_wrap">
 	<?php self::render_applications_header( __( 'Applications', 'formidable' ), 'index' ); ?>
