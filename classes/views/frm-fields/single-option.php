a3bbcc9f69512dbc15f1cb0d95d1288f231b45a7
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-07-28 22:33:46 +0300

    apply DRY.

--- classes/views/frm-fields/single-option.php
+++ classes/views/frm-fields/single-option.php
@@ -5,14 +5,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <li id="frm_delete_field_<?php echo esc_attr( $field['id'] . '-' . $opt_key ); ?>_container" data-optkey="<?php echo esc_attr( $opt_key ); ?>" class="frm_single_option <?php echo $opt_key === '000' ? 'frm_hidden frm_option_template' : ''; ?>">
 	<?php
+	$show_icons = false;
 	if ( isset( $include_icons ) ) {
 		if ( $include_icons ) {
-			FrmAppHelper::icon_by_class( 'frmfont frm_drag_icon frm-drag' );
+			$show_icons = true;
 		}
 	} else {
-		FrmAppHelper::icon_by_class( 'frmfont frm_drag_icon frm-drag' );
+		$show_icons = true;
 	}
 
+	if ( $show_icons ) {
+		FrmAppHelper::icon_by_class( 'frmfont frm_drag_icon frm-drag' );
+	}
 	?>
 	<input type="<?php echo esc_attr( $default_type ); ?>" name="<?php echo esc_attr( $field_name ); ?>" <?php echo ( isset( $checked ) && $checked ? 'checked="checked"' : '' ); ?> value="<?php echo esc_attr( $field_val ); ?>"/>
 
@@ -24,11 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<input type="<?php echo esc_attr( $default_type ); ?>" class="frm_invisible" />
 		<input type="text" name="field_options[options_<?php echo esc_attr( $field['id'] ); ?>][<?php echo esc_attr( $opt_key ); ?>][value]" id="field_key_<?php echo esc_attr( $field['id'] . '-' . $opt_key ); ?>" value="<?php echo esc_attr( $field_val ); ?>" placeholder="<?php esc_attr_e( 'Saved Value', 'formidable' ); ?>" data-frmchange="trim,updateDefault" />
 		<?php
-		if ( isset( $include_icons ) ) {
-			if ( $include_icons ) {
-				FrmAppHelper::icon_by_class( 'frmfont frm_save_icon' );
-			}
-		} else {
+		if ( $show_icons ) {
 			FrmAppHelper::icon_by_class( 'frmfont frm_save_icon' );
 		}
 		?>
