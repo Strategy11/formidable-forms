50f2ce57da0ebf21663a5c74c854921b9805c3db
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-08-05 22:46:53 +0300

    update template to support both the new update and old data rendering.

--- classes/views/frm-entries/sidebar-shared.php
+++ classes/views/frm-entries/sidebar-shared.php
@@ -121,7 +121,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="misc-pub-section frm_force_wrap">
 				<?php FrmAppHelper::icon_by_class( 'frmfont frm_history_icon', array( 'aria-hidden' => 'true' ) ); ?>
 				<?php esc_html_e( 'Referrer:', 'formidable' ); ?>
-				<?php echo wp_kses_post( str_replace( "\r\n", '<br/>', $data['referrer'] ) ); ?>
+				<?php
+				if ( is_string( $data['referrer'] ) ) {
+					echo wp_kses_post( str_replace( "\r\n", '<br/>', $data['referrer'] ) );
+				} else {
+					foreach ( $data['referrer'] as $referrer ) {
+						if ( is_array( $referrer ) ) {
+							foreach ( $referrer as $date_key=> $page_by_date ) {
+								if ( ! is_array( $page_by_date ) ) {
+									return;
+								}
+								echo '<br><b>' . $date_key . '</b>';
+								foreach ( $page_by_date as $page ) {
+									if ( is_array( $page ) ) {
+										echo '<br>' . gmdate( 'H:i', $page['timestamp'] ) . "\t" . $page['url'] . '<br>';
+									}
+								}
+							}
+						}
+					}
+				}
+				?>
 			</div>
 		<?php } ?>
 
