8cdefc48ebfe7a3274f0e043e5ca1d61e939c536
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-10-10 14:42:13 +0300

    apply email message filter conditionally

--- tests/emails/test_FrmEmail.php
+++ tests/emails/test_FrmEmail.php
@@ -695,8 +695,9 @@ class test_FrmEmail extends FrmUnitTest {
 			$action->post_content[ $setting_name ] = $setting;
 			$email                                 = new FrmEmail( $action, $this->entry, $this->contact_form );
 			$actual                                = $this->get_private_property( $email, $property );
-
-			$expected = apply_filters( 'frm_email_message', $expected, array( 'plain_text' => 0 ) );
+			if ( $property === 'message' ) {
+				$expected = apply_filters( 'frm_email_message', $expected, array( 'plain_text' => 0 ) );
+			}
 
 			$this->assertEquals( $expected, $actual );
 		}
