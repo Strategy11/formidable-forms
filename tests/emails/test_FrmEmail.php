284f834a7caf17d26fd6caea8172542ea5a98678
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-10-10 12:54:25 +0300

    apply frm_email_message filter

--- tests/emails/test_FrmEmail.php
+++ tests/emails/test_FrmEmail.php
@@ -696,9 +696,7 @@ class test_FrmEmail extends FrmUnitTest {
 			$email                                 = new FrmEmail( $action, $this->entry, $this->contact_form );
 			$actual                                = $this->get_private_property( $email, $property );
 
-			if ( $setting_name === 'email_message' ) {
-				$expected = apply_filters( 'frm_email_message', $expected, array( 'plain_text' => 0 ) );
-			}
+			$expected = apply_filters( 'frm_email_message', $expected, array( 'plain_text' => 0 ) );
 
 			$this->assertEquals( $expected, $actual );
 		}
