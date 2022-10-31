7204c1fdf1632de8ab30957a80b576977cb43b76
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-10-31 11:41:54 +0300

    add a different image for recaptcha v3

--- classes/views/frm-fields/back-end/field-captcha.php
+++ classes/views/frm-fields/back-end/field-captcha.php
@@ -6,7 +6,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 $frm_settings      = FrmAppHelper::get_settings();
 $active_captcha    = $frm_settings->active_captcha;
 $captcha_not_setup = $active_captcha === 'recaptcha' && empty( $frm_settings->recaptcha_pubkey ) || $active_captcha === 'hcaptcha' && empty( $frm_settings->hcaptcha_pubkey );
-$image_name        = $captcha_not_setup ? 'captcha_not_setup' : $frm_settings->active_captcha;
+
+if ( $captcha_not_setup ) {
+	$image_name = 'captcha_not_setup';
+} elseif ( $active_captcha === 'recaptcha' && $frm_settings->re_type === 'v3' ) {
+	$image_name = 'recaptcha_v3';
+} else {
+	$image_name = $active_captcha;
+}
+
 ?>
 <img src="<?php echo esc_url( FrmAppHelper::plugin_url() . '/images/' . $image_name . '.png' ); ?>" class="<?php echo esc_attr( $image_name ); ?>_placeholder" alt="<?php echo esc_attr( $image_name ); ?>"/>
 <input type="hidden" name="<?php echo esc_attr( $field_name ); ?>" value="1" />
