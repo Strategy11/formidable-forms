0f2d16c5fdf630a157db052c8dd66d1774187cab
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-11-08 12:05:42 +0300

    deprecate file

---
+++ classes/views/frm-settings/recaptcha.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+_deprecated_file( esc_html( basename( __FILE__ ) ), 'x.x', 'formidable/classes/views/frm-settings/captcha/captcha.php' );
+
+include FrmAppHelper::plugin_path() . '/classes/views/frm-settings/captcha/captcha.php';
