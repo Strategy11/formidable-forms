7189a1f15ddd618c0cb67657f5db2c99e192c877
AbdiTolessa <104570709+AbdiTolessa@users.noreply.github.com>
2022-11-13 11:45:34 +0300

    refactor captcha_keys view file

--- classes/views/frm-settings/captcha/captcha_keys.php
+++ classes/views/frm-settings/captcha/captcha_keys.php
@@ -24,17 +24,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	?>
 </p>
 <?php
-$prefix = $captcha === 'recaptcha' ? '' : 'hcaptcha_';
+if ( $captcha === 'recaptcha' ) {
+	$prefix = '';
+	$title  = __( 'reCAPTCHA is a free, accessible CAPTCHA service that helps to digitize books while blocking spam on your blog. reCAPTCHA asks commenters to retype two words scanned from a book to prove that they are a human. This verifies that they are not a spambot.', 'formidable' );
+} else {
+	$prefix = 'hcaptcha_';
+	$title  = __( 'hCaptcha is an anti-bot solution that protects user privacy and rewards websites. It is a privacy-focused drop-in replacement for reCAPTCHA.', 'formidable' );
+}
 ?>
 <p class="frm6 frm_form_field">
-	<label class="frm_help" for="frm_<?php echo esc_attr( $prefix ); ?>pubkey" title="
-	<?php
-	if ( $captcha === 'recaptcha' ) {
-		esc_attr_e( 'reCAPTCHA is a free, accessible CAPTCHA service that helps to digitize books while blocking spam on your blog. reCAPTCHA asks commenters to retype two words scanned from a book to prove that they are a human. This verifies that they are not a spambot.', 'formidable' );
-	} else {
-		esc_attr_e( 'hCaptcha is an anti-bot solution that protects user privacy and rewards websites. It is a privacy-focused drop-in replacement for reCAPTCHA.', 'formidable' );
-	}
-	?>">
+	<label class="frm_help" for="frm_<?php echo esc_attr( $prefix ); ?>pubkey" title="<?php echo esc_attr( $title ); ?>">
 		<?php esc_html_e( 'Site Key', 'formidable' ); ?>
 	</label>
 	<input type="text" name="frm_<?php echo esc_html( $prefix ); ?>pubkey" id="frm_<?php echo esc_html( $prefix ); ?>pubkey" size="42" value="<?php echo esc_attr( $frm_settings->{$prefix . 'pubkey'} ); ?>" />
