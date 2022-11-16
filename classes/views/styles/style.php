3d3893a06e0d8a93bfded6e9762dfa1e459db4fd
Mike <mike@crabcyb.org>
2022-11-16 16:26:17 -0400

    Show the images from the style API, add sidebar/preview grid container separation

--- classes/views/styles/style.php
+++ classes/views/styles/style.php
@@ -5,28 +5,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 // This is the view for the "style page" where you can assign a style to a form and view the list of style templates.
 // It is accessed from /wp-admin/admin.php?page=formidable&frm_action=style&id=782
 
-/*
-TODO:
-- Left sidebar with list of styles.
-  - My Styles includes custom styles.
-      - With a "New style" option.
-  - Formidable Styles includes templates from an API.
-      - TODO write unit tests for the API.
-- The right side body shows a preview (of the target form) so you can see the form you're actually styling.
-  - There is a floating button here that links to the Style editor page.
-*/
+?>
+<div class="frm_grid_container">
+	<?php // Sidebar. ?>
+	<div class="frm_grid_container frm4">
+		<?php
+		// TODO Render "My styles".
+		// TODO the design has a "New style" option here.
+		   // TODO this will trigger a new modal.
 
-$style_api = new FrmStyleApi();
-$info      = $style_api->get_api_info();
-
-foreach ( $info as $key => $style ) {
-	if ( ! is_numeric( $key ) ) {
-		// Skip active_sub/expires keys.
-		continue;
-	}
-	?>
-	<div>
-		<?php echo $style['name']; ?>
+		// Render "Formidable Styles".
+		$style_api = new FrmStyleApi();
+		$info      = $style_api->get_api_info();
+		foreach ( $info as $key => $style ) {
+			if ( ! is_numeric( $key ) ) {
+				// Skip active_sub/expires keys.
+				continue;
+			}
+			?>
+			<div class="frm6">
+				<img style="max-width: 100%; border-radius: 6px;" src="<?php echo esc_url( $style['icon'][0] ); ?>" />
+				<?php echo $style['name']; ?>
+			</div>
+			<?php
+		}
+		?>
 	</div>
-	<?php
-}
+	<?php // Preview area. ?>
+	<div class="frm8">
+		<?php
+		/**
+		 * The right side body shows a preview (of the target form) so you can see the form you're actually styling.
+		 * TODO: There is a floating button here that links to the Style editor page.
+		 */
+		?>
+		Preview Area
+		<?php // TODO: What's the best way to do this? Can I use an iframe? ?>
+	</div>
+</div>
