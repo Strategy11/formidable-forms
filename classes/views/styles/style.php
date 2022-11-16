3c00cabb264d871853322e16a244fb8fc999cbe0
Mike <mike@crabcyb.org>
2022-11-16 15:51:23 -0400

    Add TODO comments to view

--- classes/views/styles/style.php
+++ classes/views/styles/style.php
@@ -4,4 +4,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 // This is the view for the "style page" where you can assign a style to a form and view the list of style templates.
 // It is accessed from /wp-admin/admin.php?page=formidable&frm_action=style&id=782
-?>
+
+/*
+TODO:
+- Left sidebar with list of styles.
+  - My Styles includes custom styles.
+      - With a "New style" option.
+  - Formidable Styles includes templates from an API.
+      - TODO write unit tests for the API.
+- The right side body shows a preview (of the target form) so you can see the form you're actually styling.
+  - There is a floating button here that links to the Style editor page.
+*/
