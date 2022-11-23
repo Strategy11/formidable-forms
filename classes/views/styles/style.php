15304140b018636cfe4bbb26a4d7cb3541098a98
Mike <mike@crabcyb.org>
2022-11-23 16:44:04 -0400

    Fix up formatting

--- classes/views/styles/style.php
+++ classes/views/styles/style.php
@@ -68,14 +68,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 							<?php // Add a sample form to toggle between. This is toggled by the #frm_toggle_sample_form below this. ?>
 							<div id="frm_sample_form" class="frm_hidden">
 								<?php 
-								$style = $default_style;
+								$style        = $default_style;
 								$frm_settings = $frm_settings = FrmAppHelper::get_settings();
 								include $style_views_path . '_sample_form.php';
 								?>
 							</div>
 							<?php // TODO: Hide this button if it is not the active style. ?>
 							<a href="<?php echo esc_url( admin_url( 'admin.php?page=formidable-styles&frm_action=edit&id=' . $active_style->ID . '&form_id=' . $form->id ) ); ?>" id="frm_edit_style" class="frm_floating_style_button" tabindex="0" role="button">
-							<?php FrmAppHelper::icon_by_class( 'frmfont frm_pencil_icon', array( 'echo' => true ) ); ?> <?php esc_html_e( 'Edit style', 'formidable' ); ?>
+								<?php FrmAppHelper::icon_by_class( 'frmfont frm_pencil_icon', array( 'echo' => true ) ); ?> <?php esc_html_e( 'Edit style', 'formidable' ); ?>
 							</a>
 							<button id="frm_toggle_sample_form" class="frm_floating_style_button">
 								<?php FrmAppHelper::icon_by_class( 'frmfont frm_sample_form_icon', array( 'echo' => true ) ); ?> <span><?php esc_html_e( 'View sample form', 'formidable' ); ?></span>
@@ -86,4 +86,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</fieldset>
 		</div><?php // End .frm_form_fields ?>
 	</div><?php // End .frm_page_container ?>
-</div>
+</div><?php // End .frm_wrap ?>
