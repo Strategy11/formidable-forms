fce0467746ab84be43ff67ea04e8e2a1709f112c
Mike <mike@crabcyb.org>
2022-12-05 13:14:03 -0400

    Not all files were comitted

--- classes/views/styles/_styles-edit.php
+++ classes/views/styles/_styles-edit.php
@@ -6,11 +6,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 // It is accessed from /wp-admin/admin.php?page=formidable-styles&frm_action=edit&form_id=782
 ?>
 <div id="frm_style_sidebar" class="frm-right-panel styling_settings">
-	<form id="frm_edit_style_form" method="post" action="<?php echo esc_url( FrmStylesHelper::get_edit_url( $style, $form->id ) ); ?>">
+	<form id="frm_styling_form" method="post" action="<?php echo esc_url( FrmStylesHelper::get_edit_url( $style, $form->id ) ); ?>">
 		<input type="hidden" name="ID" value="<?php echo esc_attr( $style->ID ); ?>" />
 		<input type="hidden" name="frm_action" value="save" />
 		<?php wp_nonce_field( 'frm_style_nonce', 'frm_style' ); ?>
 
 		<?php include $style_views_path . '_style-options.php'; ?>
+
+		<div id="this_css"></div><?php // This holds the custom CSS for live updates to the preview. ?>
 	</form>
 </div>
