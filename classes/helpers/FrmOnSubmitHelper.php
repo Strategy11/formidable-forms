7ab4fe4e0cc5e2350a76aa9d4bc0026bd7bd983b
Truong Giang <truongwp@gmail.com>
2022-12-06 13:48:36 +0700

    Add On Submit setting HTML

---
+++ classes/helpers/FrmOnSubmitHelper.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * On Submit action helper
+ *
+ * @package Formidable
+ * @since 5.x.x
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+class FrmOnSubmitHelper {
+
+	/**
+	 * Shows message settings.
+	 *
+	 * @param array $args {
+	 *     The args.
+	 *
+	 *     @type object        $form_action    Form action post data.
+	 *     @type FrmFormAction $action_control Form action object.
+	 *     @type object        $form           Form data.
+	 *     @type string        $action_key     Action key.
+	 *     @type array         $values         Contains `fields` (form fields) and `id` (form ID).
+	 * }
+	 */
+	public static function show_message_settings( $args ) {
+		$id_attr = $args['action_control']->get_field_id( 'message' );
+		?>
+		<div>
+			<label for="<?php echo esc_attr( $id_attr ); ?>">
+				<?php esc_html_e( 'Message on submit', 'formidable' ); ?>
+			</label>
+			<textarea
+				name="<?php echo esc_attr( $args['action_control']->get_field_name( 'message' ) ); ?>"
+				id="<?php echo esc_attr( $id_attr ); ?>"
+				rows="4"
+			><?php echo esc_textarea( $args['form_action']->post_content['message'] ); ?></textarea>
+		</div>
+
+		<?php $id_attr = $args['action_control']->get_field_id( 'show_form' ); ?>
+		<div>
+			<?php
+			FrmProHtmlHelper::toggle(
+				$id_attr,
+				$args['action_control']->get_field_name( 'show_form' ),
+				array(
+					'div_class' => 'with_frm_style frm_toggle',
+					'checked'   => ! empty( $args['form_action']->post_content['show_form'] ),
+					'echo'      => true,
+				)
+			);
+			?>
+			<label for="<?php echo esc_attr( $id_attr ); ?>">
+				<?php esc_html_e( 'Show the form with the confirmation message', 'formidable' ); ?>
+			</label>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Shows redirect settings.
+	 *
+	 * @param array $args {
+	 *     The args.
+	 *
+	 *     @type object        $form_action    Form action post data.
+	 *     @type FrmFormAction $action_control Form action object.
+	 *     @type object        $form           Form data.
+	 *     @type string        $action_key     Action key.
+	 *     @type array         $values         Contains `fields` (form fields) and `id` (form ID).
+	 * }
+	 */
+	public static function show_redirect_settings( $args ) {
+		$id_attr = $args['action_control']->get_field_id( 'success_url' );
+		?>
+		<div>
+			<label for="<?php echo esc_attr( $id_attr ); ?>"><?php esc_html_e( 'Enter URL', 'formidable' ); ?></label>
+			<input
+				type="text"
+				id="<?php echo esc_attr( $id_attr ); ?>"
+				name="<?php echo esc_attr( $args['action_control']->get_field_name( 'success_url' ) ); ?>"
+				value="<?php echo esc_attr( $args['form_action']->post_content['success_url'] ); ?>"
+			/>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Shows page settings.
+	 *
+	 * @param array $args {
+	 *     The args.
+	 *
+	 *     @type object        $form_action    Form action post data.
+	 *     @type FrmFormAction $action_control Form action object.
+	 *     @type object        $form           Form data.
+	 *     @type string        $action_key     Action key.
+	 *     @type array         $values         Contains `fields` (form fields) and `id` (form ID).
+	 * }
+	 */
+	public static function show_page_settings( $args ) {
+		$name_attr = $args['action_control']->get_field_name( 'success_page_id' );
+		?>
+		<div>
+			<label for="<?php echo esc_attr( $name_attr ); ?>"><?php esc_html_e( 'Select a page', 'formidable' ); ?></label>
+			<?php
+			FrmAppHelper::maybe_autocomplete_pages_options(
+				array(
+					'field_name'  => $name_attr,
+					'page_id'     => $args['form_action']->post_content['success_page_id'],
+					'placeholder' => __( 'Select a Page', 'formidable' ),
+				)
+			);
+			?>
+		</div>
+		<?php
+	}
+}
---
+++ classes/views/frm-form-actions/on_submit_settings.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * On Submit form action settings
+ *
+ * @package Formidable
+ * @since 5.x.x
+ *
+ * @var object $form_action Form action post object.
+ * @var array  $args        Contains `form`, `action_key`, `values`.
+ * @var FrmFormAction $this FrmFormAction instance.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+$success_action = $form_action->post_content['success_action'];
+
+$types = array(
+	'message' => array(
+		'label' => __( 'Show Message', 'formidable' ),
+		'icon'  => 'frm_icon_font frm_chat_forms_icon',
+		'sub_settings' => array( 'FrmOnSubmitHelper', 'show_message_settings' ),
+	),
+	'redirect' => array(
+		'label' => __( 'Redirect to URL', 'formidable' ),
+		'icon'  => 'dashicons dashicons-admin-site-alt3',
+		'sub_settings' => array( 'FrmOnSubmitHelper', 'show_redirect_settings' ),
+	),
+	'page' => array(
+		'label' => __( 'Show Page Content', 'formidable' ),
+		'icon'  => 'dashicons dashicons-admin-page',
+		'sub_settings' => array( 'FrmOnSubmitHelper', 'show_page_settings' ),
+	),
+);
+
+$col_count = count( $types );
+if ( $col_count <= 4 ) {
+	$col_class = 'frm' . ( 12 / $col_count );
+} else {
+	$col_count = 'frm2';
+}
+?>
+<div class="frm_on_submit_type_setting">
+	<div class="frm_grid_container">
+		<?php
+		foreach ( $types as $type => $type_data ) :
+			$input_id = $this->get_field_id( 'success_action' );
+			?>
+			<div class="<?php echo esc_attr( $col_class ); ?>">
+				<input
+					type="radio"
+					id="<?php echo esc_attr( $input_id ); ?>"
+					name="<?php echo esc_attr( $this->get_field_name( 'success_action' ) ); ?>"
+					value="<?php echo esc_attr( $type ); ?>"
+					<?php checked( $type, $success_action ); ?>
+				/>
+				<label for="<?php echo esc_attr( $input_id ); ?>">
+					<?php FrmAppHelper::icon_by_class( $type_data['icon'], array( 'echo' => true ) ); ?>
+					<?php echo esc_html( $type_data['label'] ); ?>
+				</label>
+			</div>
+		<?php endforeach; ?>
+	</div>
+</div><!-- End .frm_on_submit_type_setting -->
+
+<?php
+$type_args                   = $args;
+$type_args['form_action']    = $form_action;
+$type_args['action_control'] = $this;
+foreach ( $types as $type_name => $type ) {
+	echo '<div class="frm_on_submit_' . esc_attr( $type_name ) . '_settings">';
+
+	if ( is_callable( $type['sub_settings'] ) ) {
+		call_user_func( $type['sub_settings'], $type_args );
+	}
+
+	echo '</div><!-- End .frm_on_submit_' . esc_attr( $type_name ) . '_settings -->';
+}
