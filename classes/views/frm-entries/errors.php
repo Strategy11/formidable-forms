2e5ef24c0eae22ac1f99cfdf4091e2f97a772cb8
AbdiTolesa <tolesaabdi@gmail.com>
2022-12-21 07:50:53 +0300

    remove filter from view

--- classes/views/frm-entries/errors.php
+++ classes/views/frm-entries/errors.php
@@ -7,7 +7,6 @@ if ( isset( $include_extra_container ) ) { ?>
 <div class="<?php echo esc_attr( $include_extra_container ); ?>" id="frm_form_<?php echo esc_attr( $form->id ); ?>_container">
 	<?php
 }
-
 if ( isset( $message ) && $message != '' ) {
 	if ( FrmAppHelper::is_admin() ) {
 		?>
