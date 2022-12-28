c0e7787aa8e669414f163253c4974cd8906f88c9
AbdiTolesa <tolesaabdi@gmail.com>
2022-12-28 14:16:26 +0300

    update textarea name

--- classes/views/frm-fields/back-end/settings.php
+++ classes/views/frm-fields/back-end/settings.php
@@ -181,7 +181,7 @@ do_action( 'frm_before_field_options', $field, compact( 'field_obj', 'display', 
 
 						if ( $display['type'] === 'rte' ) {
 							$e_args  = array(
-								'textarea_name' => 'field_options[default_value_' . absint( $field['id'] ) . ']',
+								'textarea_name' => 'default_value_' . absint( $field['id'] ),
 								'textarea_rows' => 8,
 							);
 							$html_id = 'frm_default_value_' . absint( $field['id'] );
