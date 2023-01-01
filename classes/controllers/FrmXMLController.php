95abd9daf20f52c8f906a5da8cc74f5ac75eca8c
AbdiTolesa <tolesaabdi@gmail.com>
2023-01-01 20:26:17 +0300

    add a check for null parent_form_id as well

--- classes/controllers/FrmXMLController.php
+++ classes/controllers/FrmXMLController.php
@@ -282,8 +282,8 @@ class FrmXMLController {
 
 	public static function form( $errors = array(), $message = '' ) {
 		$where = array(
-			'status' => array( null, '', 'published' ),
-			'parent_form_id' => 0,
+			'status'         => array( null, '', 'published' ),
+			'parent_form_id' => array( null, 0 ),
 		);
 		$forms = FrmForm::getAll( $where, 'name' );
 
