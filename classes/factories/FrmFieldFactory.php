0ddef5cc9e38213d1c04a25141d14ac758d3003d
Mike <mike@crabcyb.org>
2023-01-12 11:42:29 -0400

    Add back the logging

--- classes/factories/FrmFieldFactory.php
+++ classes/factories/FrmFieldFactory.php
@@ -54,6 +54,10 @@ class FrmFieldFactory {
 			$field = FrmField::getOne( $field );
 		}
 
+		if ( ! is_object( $field ) ) {
+			var_dump( debug_backtrace( 2 ) );
+		}
+
 		return self::get_field_type( $field->type, $field );
 	}
 
