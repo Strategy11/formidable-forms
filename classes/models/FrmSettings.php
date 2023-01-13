71b3a42adfe1f0fd4106c3877ab02b942bec2a73
Mike <mike@crabcyb.org>
2023-01-13 11:45:23 -0400

    Add a new maybe_sanitize_global_setting function with a sanitize_textarea_field call to custom_css (to preserve the old way it was being sanitized)

--- classes/models/FrmSettings.php
+++ classes/models/FrmSettings.php
@@ -158,6 +158,7 @@ class FrmSettings {
 
 	/**
 	 * @param array $params
+	 * @return void
 	 */
 	public function fill_with_defaults( $params = array() ) {
 		$settings    = $this->default_options();
@@ -183,12 +184,31 @@ class FrmSettings {
 				$this->{$setting} = $default;
 			}
 
-			if ( $filter_html && in_array( $setting, $filter_keys, true ) ) {
-				$this->{$setting} = FrmAppHelper::kses( $this->{$setting}, 'all' );
-			}
-
+			$this->{$setting} = $this->maybe_sanitize_global_setting( $this->{$setting}, $setting, $filter_keys );
 			unset( $setting, $default );
 		}
+	}
+
+	/**
+	 * Handle sanitizing for a target global setting key.
+	 *
+	 * @since x.x
+	 *
+	 * @param mixed  $value       The unsanitized global setting value.
+	 * @param string $key         The key of the global setting being saved.
+	 * @param array  $filter_keys These keys are are filtered with kses.
+	 * @return mixed
+	 */
+	private function maybe_sanitize_global_setting( $value, $key, $filter_keys ) {
+		if ( 'custom_css' === $key ) {
+			return sanitize_textarea_field( $value );
+		}
+
+		if ( in_array( $key, $filter_keys, true ) ) {
+			return FrmAppHelper::kses( $value, 'all' );
+		}
+
+		return $value;
 	}
 
 	private function fill_captcha_settings() {
@@ -296,6 +316,7 @@ class FrmSettings {
 		$this->re_lang          = $params['frm_re_lang'];
 		$this->re_threshold     = floatval( $params['frm_re_threshold'] );
 		$this->load_style       = $params['frm_load_style'];
+		$this->custom_css       = $params['frm_custom_css'];
 
 		$checkboxes = array( 'mu_menu', 're_multi', 'use_html', 'jquery_css', 'accordion_js', 'fade_form', 'no_ips', 'tracking', 'admin_bar' );
 		foreach ( $checkboxes as $set ) {
