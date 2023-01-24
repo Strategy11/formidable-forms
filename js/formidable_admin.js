7db4ea6580069c17b96ae451c680b7fc9e444d7c
AbdiTolesa <tolesaabdi@gmail.com>
2023-01-24 23:58:27 +0300

    remove debug code

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -2882,7 +2882,7 @@ function frmAdminBuildJS() {
 			newValue = this.value,
 			changes = document.getElementById( this.getAttribute( 'data-changeme' ) ),
 			att = this.getAttribute( 'data-changeatt' );
-		console.log( 'HERE' );
+			
 		if ( changes === null ) {
 			return;
 		}
