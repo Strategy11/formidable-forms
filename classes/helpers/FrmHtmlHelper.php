d1653cf6d7ce8f640fb7c548a7826b1559ea4860
stephywells <steph@strategy11.com>
2023-01-30 17:54:51 -0700

    Add new admin toggle element from https://github.com/Strategy11/formidable-forms/commit/f5269f9ada5a22dc8dcd8876aa5d26dee3057886

---
+++ classes/helpers/FrmHtmlHelper.php
@@ -0,0 +1,34 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+/**
+ * @since x.x
+ */
+class FrmHtmlHelper {
+
+	/**
+	 * Create a toggle and either echo or return the string.
+	 * This isn't completely compatible with Pro. The input_html option has been omitted as it isn't secure.
+	 * This is intended for use on admin pages only. The CSS is included in frm_admin.css.
+	 *
+	 * @since x.x
+	 *
+	 * @param string $id
+	 * @param string $name
+	 * @param array  $args {
+	 *     @param bool|null $echo True if you want the toggle to echo. False if you want it to return an HTML string.
+	 * }
+	 *
+	 * @return string|void
+	 */
+	public static function toggle( $id, $name, $args ) {
+		return FrmAppHelper::clip(
+			function() use ( $id, $name, $args ) {
+				require FrmAppHelper::plugin_path() . '/classes/views/shared/toggle.php';
+			},
+			isset( $args['echo'] ) ? $args['echo'] : false
+		);
+	}
+}
---
+++ classes/views/shared/toggle.php
@@ -0,0 +1,55 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+?>
+<?php
+$div_class    = isset( $args['div_class'] ) ? $args['div_class'] : false;
+$show_labels  = isset( $args['show_labels'] ) ? $args['show_labels'] : false;
+$off_label    = isset( $args['off_label'] ) ? $args['off_label'] : '';
+$on_label     = isset( $args['on_label'] ) ? $args['on_label'] : 1;
+$checked      = isset( $args['checked'] ) && ( true === $args['checked'] || false !== strpos( $args['checked'], 'checked="checked"' ) );
+$aria_checked = $checked ? 'true' : 'false';
+
+$use_container = false;
+
+$div_params = array(
+	// This is important when the default style is !important as Pro styling may cause conflicts.
+	// It uses --toggle-on-color so just set the variable.
+	'style' => '--toggle-on-color:var(--primary-color);'
+);
+if ( $div_class ) {
+	$use_container       = true;
+	$div_params['class'] = $div_class;
+}
+
+if ( $use_container ) {
+	?>
+<div <?php FrmAppHelper::array_to_html_params( $div_params, true ); ?>>
+	<?php
+	$div_params = array();
+}
+?>
+	<label class="frm_switch_block" <?php FrmAppHelper::array_to_html_params( $div_params, true ); ?>>
+		<?php if ( $show_labels && $off_label ) { ?>
+			<span class="frm_off_label frm_switch_opt"><?php echo esc_html( $off_label ); ?></span>
+		<?php } ?>
+
+		<input type="checkbox" name="<?php echo esc_attr( $name ); ?>[]" id="<?php echo esc_attr( $id ); ?>" value="<?php echo esc_attr( $on_label ); ?>"
+			<?php checked( $checked, true ); ?>
+			<?php if ( $show_labels && $off_label ) { ?>
+				data-off="<?php echo esc_attr( $off_label ); ?>"
+			<?php } ?>
+		/>
+
+		<span class="frm_switch" tabindex="0" role="switch" aria-labelledby="<?php echo esc_attr( $id ); ?>_label" aria-checked="<?php echo esc_attr( $aria_checked ); ?>">
+			<span class="frm_slider"></span>
+		</span>
+
+		<?php if ( $show_labels && $on_label != 1 ) { ?>
+			<span class="frm_on_label frm_switch_opt"><?php echo FrmAppHelper::kses( $on_label, 'all' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
+		<?php } ?>
+	</label>
+<?php if ( $use_container ) { ?>
+</div>
+<?php } ?>
