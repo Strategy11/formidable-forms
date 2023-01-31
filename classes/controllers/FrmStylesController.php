66c696d3b88648c2b8054c9cd86459aeddc5d65b
Mike <mike@crabcyb.org>
2023-01-31 16:05:07 -0400

    Avoid browser cache after saving a style to avoid outdated styles (like a missing new template)

--- classes/controllers/FrmStylesController.php
+++ classes/controllers/FrmStylesController.php
@@ -129,7 +129,13 @@ class FrmStylesController {
 			wp_enqueue_style( 'wp-color-picker' );
 		}
 
-		wp_enqueue_style( 'frm-custom-theme', admin_url( 'admin-ajax.php?action=frmpro_css' ), array(), $version );
+		$css_url = admin_url( 'admin-ajax.php?action=frmpro_css' );
+		if ( 'assign_style' === FrmAppHelper::get_post_param( 'frm_action', '', 'sanitize_title' ) ) {
+			// Make sure that we don't load a cached CSS file right after we save a style.
+			$css_url .= '&unique=' . uniqid();
+		}
+
+		wp_enqueue_style( 'frm-custom-theme', $css_url, array(), $version );
 
 		$style = apply_filters( 'frm_style_head', false );
 		if ( $style ) {
