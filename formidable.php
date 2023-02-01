499b7fddeba2a3cfe067f3a0785884ed8834dde9
Mike <mike@crabcyb.org>
2023-02-01 11:01:51 -0400

    Prepare for 5.5.7

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable Forms
 Description: Quickly and easily create drag-and-drop forms
-Version: 5.5.6
+Version: 5.5.7
 Plugin URI: https://formidableforms.com/
 Author URI: https://formidableforms.com/
 Author: Strategy11 Form Builder Team
