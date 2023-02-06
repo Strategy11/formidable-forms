b45fb13a3f90037cbe392d7266235acdc501b874
Mike <mike@crabcyb.org>
2023-02-06 11:56:51 -0400

    Update a comment

--- classes/helpers/FrmFormsListHelper.php
+++ classes/helpers/FrmFormsListHelper.php
@@ -292,6 +292,7 @@ class FrmFormsListHelper extends FrmListHelper {
 
 	/**
 	 * Get the HTML for the Actions column in the form list.
+	 * This includes multiple icons for triggering the embed modal, the visual styler, and an active landing page.
 	 *
 	 * @since x.x
 	 *
@@ -299,11 +300,11 @@ class FrmFormsListHelper extends FrmListHelper {
 	 * @return string
 	 */
 	protected function column_shortcode( $form ) {
-		$val = '<a href="#" class="frm-embed-form" role="button" aria-label="' . esc_html__( 'Embed Form', 'formidable' ) . '">' . FrmAppHelper::icon_by_class( 'frmfont frm_code_icon', array( 'echo' => false ) ) . '</a>';
+		$val  = '<a href="#" class="frm-embed-form" role="button" aria-label="' . esc_html__( 'Embed Form', 'formidable' ) . '">' . FrmAppHelper::icon_by_class( 'frmfont frm_code_icon', array( 'echo' => false ) ) . '</a>';
 		$val .= $this->column_style( $form );
-		$val = apply_filters( 'frm_form_list_actions', $val, array( 'form' => $form ) );
-		$val = str_replace( '&nbsp;', '', $val ); // Remove the space hardeded in Landing pages.
-		$val = '<div>' . $val . '</div>';
+		$val  = apply_filters( 'frm_form_list_actions', $val, array( 'form' => $form ) );
+		$val  = str_replace( '&nbsp;', '', $val ); // Remove the space hard coded in Landing pages.
+		$val  = '<div>' . $val . '</div>';
 		return $val;
 	}
 
