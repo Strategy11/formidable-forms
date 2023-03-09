d474de551a8420c477ff76d260ba80eeb75a511e
Mike <mike@crabcyb.org>
2023-03-09 16:34:54 -0400

    Fix a formidable-payments text domain

--- stripe/views/payments/sidebar_actions.php
+++ stripe/views/payments/sidebar_actions.php
@@ -1,14 +1,14 @@
 <div id="misc-publishing-actions">
 	<div class="misc-pub-section curtime misc-pub-curtime">
     	<span id="timestamp">
-		    <?php printf( esc_html__( 'Created on: %1$s', 'formidable-payments' ), '<b>' . esc_html( $created_at ) . '</b>' ); ?>
+		    <?php printf( esc_html__( 'Created on: %1$s', 'formidable' ), '<b>' . esc_html( $created_at ) . '</b>' ); ?>
 		</span>
 	</div>
 
 	<?php if ( $payment->status === 'complete' && ! empty( $payment->receipt_id ) ) { ?>
 		<div class="misc-pub-section">
 			<span class="dashicons dashicons-cart wp-media-buttons-icon"></span>
-			<?php esc_html_e( 'Payment:', 'formidable-payments' ); ?>
+			<?php esc_html_e( 'Payment:', 'formidable' ); ?>
 			<?php FrmTransLitePaymentsController::show_refund_link( $payment ); ?>
 		</div>
 	<?php } ?>
