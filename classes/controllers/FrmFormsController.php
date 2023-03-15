7a4b6c8d0c03df3e21e8da22fbd00ef577154585
Truong Giang <truongwp@gmail.com>
2023-03-15 09:51:28 +0700

    Fix wrong parameter

--- classes/controllers/FrmFormsController.php
+++ classes/controllers/FrmFormsController.php
@@ -2564,8 +2564,11 @@ class FrmFormsController {
 
 		if ( ! isset( $atts['form']->options['show_form'] ) || $atts['form']->options['show_form'] ) {
 			if ( isset( $atts['action'] ) && 'update' === $atts['action'] && is_callable( array( 'FrmProEntriesController', 'show_front_end_form_with_entry' ) ) ) {
-				$atts['show_form'] = FrmProEntriesController::is_form_displayed_after_edit( $atts['form'] );
-				FrmProEntriesController::show_front_end_form_with_entry( $atts, $atts );
+				$entry = FrmEntry::getOne( $atts['entry_id'] );
+				if ( $entry ) {
+					$atts['show_form'] = FrmProEntriesController::is_form_displayed_after_edit( $atts['form'] );
+					FrmProEntriesController::show_front_end_form_with_entry( $entry, $atts );
+				}
 			} else {
 				self::show_form_after_submit( $atts );
 			}
