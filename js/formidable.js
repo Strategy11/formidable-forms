275b33045c4ef207fc3f68bc23faed048904c972
AbdiTolesa <tolesaabdi@gmail.com>
2023-04-07 23:33:57 +0300

    add exception to camelcase, move variable declaration to top

--- js/formidable.js
+++ js/formidable.js
@@ -17,6 +17,7 @@ function frmFrontFormJS() {
 	 * @since 5.4
 	 */
 	function maybeAddPolyfills() {
+		var i;
 		if ( ! Element.prototype.matches ) {
 			// IE9 supports matches but as msMatchesSelector instead.
 			Element.prototype.matches = Element.prototype.msMatchesSelector;
@@ -41,7 +42,7 @@ function frmFrontFormJS() {
 		if ( window.NodeList && ! NodeList.prototype.forEach ) {
 			NodeList.prototype.forEach = function( callback, thisArg ) {
 				thisArg = thisArg || window;
-				for ( var i = 0; i < this.length; i++ ) {
+				for ( i = 0; i < this.length; i++ ) {
 					callback.call( thisArg, this[ i ], i, this );
 				}
 			};
@@ -585,7 +586,7 @@ function frmFrontFormJS() {
 		fieldset = jQuery( object ).find( '.frm_form_field' );
 		fieldset.addClass( 'frm_doing_ajax' );
 
-		data               = jQuery( object ).serialize() + '&action=frm_entries_' + action + '&nonce=' + frm_js.nonce;
+		data               = jQuery( object ).serialize() + '&action=frm_entries_' + action + '&nonce=' + frm_js.nonce; // eslint-disable-line camelcase
 		shouldTriggerEvent = object.classList.contains( 'frm_trigger_event_on_submit' );
 
 		success = function( response ) {
@@ -626,7 +627,7 @@ function frmFrontFormJS() {
 				}
 
 				removeSubmitLoading( jQuery( object ) );
-				if ( frm_js.offset != -1 ) {
+				if ( frm_js.offset != -1 ) { // eslint-disable-line camelcase
 					frmFrontForm.scrollMsg( jQuery( object ), false );
 				}
 
@@ -746,7 +747,7 @@ function frmFrontFormJS() {
 	function postToAjaxUrl( form, data, success, error ) {
 		var ajaxUrl, action, ajaxParams;
 
-		ajaxUrl = frm_js.ajax_url;
+		ajaxUrl = frm_js.ajax_url; // eslint-disable-line camelcase
 		action = form.getAttribute( 'action' );
 
 		if ( 'string' === typeof action && -1 !== action.indexOf( '?action=frm_forms_preview' ) ) {
@@ -849,7 +850,7 @@ function frmFrontFormJS() {
 						jsErrors[key]
 					);
 				} else {
-					roleString = frm_js.include_alert_role ? 'role="alert"' : '';
+					roleString = frm_js.include_alert_role ? 'role="alert"' : ''; // eslint-disable-line camelcase
 					$fieldCont.append( '<div class="frm_error" ' + roleString + ' id="' + id + '">' + jsErrors[key] + '</div>' );
 				}
 
@@ -997,12 +998,12 @@ function frmFrontFormJS() {
 
 		jQuery.ajax({
 			type: 'POST',
-			url: frm_js.ajax_url,
+			url: frm_js.ajax_url, // eslint-disable-line camelcase
 			data: {
 				action: 'frm_entries_send_email',
 				entry_id: entryId,
 				form_id: formId,
-				nonce: frm_js.nonce
+				nonce: frm_js.nonce // eslint-disable-line camelcase
 			},
 			success: function( msg ) {
 				var admin = document.getElementById( 'wpbody' );
@@ -1153,7 +1154,7 @@ function frmFrontFormJS() {
 	function checkForErrorsAndMaybeSetFocus() {
 		var errors, element, timeoutCallback;
 
-		if ( ! frm_js.focus_first_error ) {
+		if ( ! frm_js.focus_first_error ) { // eslint-disable-line camelcase
 			return;
 		}
 
@@ -1610,10 +1611,10 @@ function frmFrontFormJS() {
 
 			jQuery( scrollObj ).trigger( 'focus' );
 			newPos = scrollObj.offset().top;
-			if ( ! newPos || frm_js.offset === '-1' ) {
+			if ( ! newPos || frm_js.offset === '-1' ) { // eslint-disable-line camelcase
 				return;
 			}
-			newPos = newPos - frm_js.offset;
+			newPos = newPos - frm_js.offset; // eslint-disable-line camelcase
 
 			m = jQuery( 'html' ).css( 'margin-top' );
 			b = jQuery( 'body' ).css( 'margin-top' );
@@ -1743,13 +1744,13 @@ function frmUpdateField( entryId, fieldId, value, message, num ) {
 	jQuery( document.getElementById( 'frm_update_field_' + entryId + '_' + fieldId + '_' + num ) ).html( '<span class="frm-loading-img"></span>' );
 	jQuery.ajax({
 		type: 'POST',
-		url: frm_js.ajax_url,
+		url: frm_js.ajax_url, // eslint-disable-line camelcase
 		data: {
 			action: 'frm_entries_update_field_ajax',
 			entry_id: entryId,
 			field_id: fieldId,
 			value: value,
-			nonce: frm_js.nonce
+			nonce: frm_js.nonce // eslint-disable-line camelcase
 		},
 		success: function() {
 			if ( message.replace( /^\s+|\s+$/g, '' ) === '' ) {
@@ -1766,11 +1767,11 @@ function frmDeleteEntry( entryId, prefix ) {
 	jQuery( document.getElementById( 'frm_delete_' + entryId ) ).replaceWith( '<span class="frm-loading-img" id="frm_delete_' + entryId + '"></span>' );
 	jQuery.ajax({
 		type: 'POST',
-		url: frm_js.ajax_url,
+		url: frm_js.ajax_url, // eslint-disable-line camelcase
 		data: {
 			action: 'frm_entries_destroy',
 			entry: entryId,
-			nonce: frm_js.nonce
+			nonce: frm_js.nonce // eslint-disable-line camelcase
 		},
 		success: function( html ) {
 			if ( html.replace( /^\s+|\s+$/g, '' ) === 'success' ) {
@@ -1793,12 +1794,12 @@ function frm_resend_email( entryId, formId ) { // eslint-disable-line camelcase
 	$link.append( '<span class="spinner" style="display:inline"></span>' );
 	jQuery.ajax({
 		type: 'POST',
-		url: frm_js.ajax_url,
+		url: frm_js.ajax_url, // eslint-disable-line camelcase
 		data: {
 			action: 'frm_entries_send_email',
 			entry_id: entryId,
 			form_id: formId,
-			nonce: frm_js.nonce
+			nonce: frm_js.nonce // eslint-disable-line camelcase
 		},
 		success: function( msg ) {
 			$link.replaceWith( msg );
