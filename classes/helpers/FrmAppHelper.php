a8c4068027cb89c47f431d95479db1f6cc11e371
Sherv Elmi <sherv.elmi@gmail.com>
2023-04-20 18:58:17 +0300

    Prepare and refactor a bit `add_dismissable_warning_message` method for PHPUnit tests

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -3857,17 +3857,11 @@ class FrmAppHelper {
 			return;
 		}
 
-		// Check if the option doesn't start with 'frm_'
-		if ( strpos( $option, 'frm_' ) !== 0 ) {
-			// Add the 'frm_' prefix to the option
-			$option = 'frm_' . $option;
-		}
-
-		$ajax_callback = function() use ( $option ) {
+		// We're handling JS codes with `doJsonPost` and it adds 'frm_' to the beginning of the action.
+		// To prevent any issues, we add 'frm_' from the beginning of the action.
+		add_action( 'wp_ajax_frm_' . $option, function() use ( $option ) {
 			self::dismiss_warning_message( $option );
-		};
-
-		add_action( 'wp_ajax_' . $option, $ajax_callback );
+		});
 
 		add_filter(
 			'frm_message_list',
