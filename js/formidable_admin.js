c0e99a38160bb58844bb5bcdbb1a56e5ff9f1e7e
Sherv Elmi <sherv.elmi@gmail.com>
2023-04-26 00:29:33 +0300

    Revert: Prevent .frm_show_upgrade elements from being draggable

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -913,8 +913,7 @@ function frmAdminBuildJS() {
 			cursorAt: {
 				top: 0,
 				left: 90 // The width of draggable button is 180. 90 should center the draggable on the cursor.
-			},
-			cancel: '.frm_show_upgrade' // Prevent .frm_show_upgrade elements from being draggable.
+			}
 		};
 		if ( 'string' === typeof handle ) {
 			settings.handle = handle;
