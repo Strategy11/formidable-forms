230010f59803af692ce6299efaa6732dfac4d7f2
Omid <omidakingroid@gmail.com>
2023-05-26 11:30:24 +0300

    Update test usl_is_allowed

--- tests/misc/test_FrmAddonsController.php
+++ tests/misc/test_FrmAddonsController.php
@@ -13,11 +13,8 @@ class test_FrmAddonsController extends FrmUnitTest {
 	 * @covers FrmAddonsController::url_is_allowed
 	 */
 	public function test_url_is_allowed() {
-		$allowed_download_urls = array(
-			'https://downloads.wordpress.org/plugin/formidable-gravity-forms-importer.zip',
-			'https://downloads.wordpress.org/plugin/formidable-import-pirate-forms.zip',
-			'https://s3.amazonaws.com/fp.strategy11.com/releases/acf/formidable-acf-1.0.zip',
-		);
+		$allowed_download_urls = FrmAddonsController::WHITE_LIST;
+		array_push( $allowed_download_urls, 'https://s3.amazonaws.com/fp.strategy11.com/releases/acf/formidable-acf-1.0.zip' );
 
 		$disallowed_download_urls = array(
 			'https://unsafeurl.com?empty_my_whole_wallet&something_to_bypass_this_check=https://downloads.wordpress.org/plugin',
