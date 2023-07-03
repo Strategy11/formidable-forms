c8944a1db94dcbe310644c0dccf451fb65728297
Sherv <sherv.elmi@gmail.com>
2023-07-03 15:55:05 +0300

    Fix: Form can be submitted in block editor

--- js/src/form/formshortcode.js
+++ js/src/form/formshortcode.js
@@ -1,18 +1,18 @@
 /**
  * Form shortcode of current form
  */
-const { Component } = wp.element;
 
-import {
-	setTextAttribute,
-} from '../common/utilities/values';
+/**
+ * Internal dependencies
+ */
+import { setTextAttribute } from '../common/utilities/values';
 
 function createParamsText( atts ) {
 	const {
 		formId,
 		title,
 		description,
-		minimize,
+		minimize
 	} = atts;
 
 	let paramsText = '';
@@ -25,14 +25,21 @@ function createParamsText( atts ) {
 	return paramsText;
 }
 
-export default class FormShortcode extends Component {
-	render() {
-		return (
-			<div>
-				[formidable
-				{ createParamsText( this.props ) }
-				]
-			</div>
-		);
-	}
+function FormShortcode( attributes ) {
+	// NOTE: Due to the outdated version of our block implementation, the following solution
+	// has been adopted to handle form submission within the WP Block Editor.
+	// TODO: Refactor to use the useEffect and useCallback hooks in future versions.
+	document.addEventListener( 'submit', function( event ) {
+		if ( event.target && event.target.matches( '.frm-show-form' ) ) {
+			event.preventDefault();
+		}
+	}, true ); // useCapture = true
+
+	return (
+		<div>
+			[formidable { createParamsText( attributes ) }]
+		</div>
+	);
 }
+
+export default FormShortcode;
