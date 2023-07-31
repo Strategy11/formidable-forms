4d466ea177be7f996478154fa8c2b0024c3510ff
Sherv <sherv.elmi@gmail.com>
2023-07-31 22:22:34 +0300

    Fix links issues

--- classes/views/shared/admin-footer-links.php
+++ classes/views/shared/admin-footer-links.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Determine the support link based on lite vs pro.
-$support_link = FrmAppHelper::pro_is_installed() ? 'https://wordpress.org/support/plugin/formidable/' : 'https://formidableforms.com/new-topic/';
+$support_link = ! FrmAppHelper::pro_is_installed() ? 'https://wordpress.org/support/plugin/formidable/' : 'https://formidableforms.com/new-topic/';
 
-// Determine the upgrade link based on lite vs elite.
-$upgrade_link = FrmAppHelper::pro_is_installed() ? 'https://formidableforms.com/lite-upgrade/' : 'https://formidableforms.com/account/downloads/';
+// Determine the upgrade link based on lite vs pro.
+$upgrade_link = ! FrmAppHelper::pro_is_installed() ? 'https://formidableforms.com/lite-upgrade/' : 'https://formidableforms.com/account/downloads/';
 ?>
 
 <div class="frm-admin-footer-links">
@@ -22,19 +22,23 @@ $upgrade_link = FrmAppHelper::pro_is_installed() ? 'https://formidableforms.com/
 	</span><!-- .frm-admin-footer-links-text -->
 
 	<div class="frm-admin-footer-links-nav">
-		<a href="<?php echo esc_url( $support_link ); ?>"><?php esc_html_e( 'Support', 'formidable' ); ?></a> /
-		<a href="https://formidableforms.com/knowledgebase/"><?php esc_html_e( 'Docs', 'formidable' ); ?></a> /
-		<a href="<?php echo esc_url( $upgrade_link ); ?>"><?php esc_html_e( 'Upgrade', 'formidable' ); ?></a>
+		<a href="<?php echo esc_url( $support_link ); ?>" target="_blank"><?php esc_html_e( 'Support', 'formidable' ); ?></a>
+		<span>/</span>
+		<a href="https://formidableforms.com/knowledgebase/" target="_blank"><?php esc_html_e( 'Docs', 'formidable' ); ?></a>
+		<?php if ( 'elite' !== FrmAddonsController::license_type() ) : ?>
+			<span>/</span>
+			<a href="<?php echo esc_url( $upgrade_link ); ?>" target="_blank"><?php esc_html_e( 'Upgrade', 'formidable' ); ?></a>
+		<?php endif; ?>
 	</div><!-- .frm-admin-footer-links-nav -->
 
 	<div class="frm-admin-footer-links-socials">
 		<!-- Facebook link -->
-		<a href="https://www.facebook.com/formidableforms/"><span class="dashicons dashicons-facebook"></span></a>
+		<a href="https://www.facebook.com/formidableforms/" target="_blank"><span class="dashicons dashicons-facebook"></span></a>
 		<!-- Instagram link -->
-		<a href="https://www.instagram.com/formidableforms/"><span class="dashicons dashicons-instagram"></span></a>
+		<a href="https://www.instagram.com/formidableforms/" target="_blank"><span class="dashicons dashicons-instagram"></span></a>
 		<!-- Twitter link -->
-		<a href="https://twitter.com/formidableforms/"><span class="dashicons dashicons-twitter"></span></a>
+		<a href="https://twitter.com/formidableforms/" target="_blank"><span class="dashicons dashicons-twitter"></span></a>
 		<!-- Youtube link -->
-		<a href="https://www.youtube.com/c/FormidableFormsPlugin/"><span class="dashicons dashicons-youtube"></span></a>
+		<a href="https://www.youtube.com/c/FormidableFormsPlugin/" target="_blank"><span class="dashicons dashicons-youtube"></span></a>
 	</div><!-- .frm-admin-footer-links-socials -->
 </div><!-- .frm-admin-footer-links -->
