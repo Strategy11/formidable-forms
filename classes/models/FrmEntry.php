1a4516728fce4ff43679dc367da96e9e5534c9d7
Omid <omidakingroid@gmail.com>
2023-08-03 03:13:20 +0300

    Return draft value itself instead of returning binary

--- classes/models/FrmEntry.php
+++ classes/models/FrmEntry.php
@@ -755,7 +755,11 @@ class FrmEntry {
 	 * @return int
 	 */
 	private static function get_is_draft_value( $values ) {
-		return ( ( isset( $values['frm_saving_draft'] ) && $values['frm_saving_draft'] == 1 ) || ( isset( $values['is_draft'] ) && $values['is_draft'] == 1 ) ) ? 1 : 0;
+		if ( isset( $values['frm_saving_draft'] ) ) {
+			return absint( $values['frm_saving_draft'] );
+		}
+
+		return absint( $values['is_draft'] );
 	}
 
 	/**
