798946245b510d4acb8b5ecfc5bddcc2fad16111
Mike Letellier <mike@crabcyb.org>
2023-08-10 12:52:16 -0300

    Add a FrmStrpLiteApiHelper for supporting refunds when Authorize.Net is active

---
+++ stripe/helpers/FrmStrpLiteApiHelper.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+class FrmStrpLiteApiHelper {
+
+	/**
+	 * The payments submodule calls this function.
+	 * This function exists so payments can be refunded when Authorize.Net is active.
+	 *
+	 * @param int $payment_id
+	 * @return bool
+	 */
+	public static function refund_payment( $payment_id ) {
+		if ( ! class_exists( 'FrmStrpLiteConnectApiAdapter' ) ) {
+			require dirname( __FILE__ ) . '/FrmStrpLiteConnectApiAdapter.php';
+		}
+		return FrmStrpLiteConnectApiAdapter::refund_payment( $payment_id );
+	}
+}
