e60f5b0e2636125c9631e59949c7d123ee9cedaa
Mike Letellier <mike@crabcyb.org>
2023-08-10 12:54:23 -0300

    Remove frm_trans_sub_opts class as it is not required in Stripe Lite, remove JS logic about toggling on/off settings based on gateway settings (this will be kept in the payments submodule)

--- stripe/js/frmtrans_admin.js
+++ stripe/js/frmtrans_admin.js
@@ -1,77 +1,7 @@
 ( function() {
 	function frmTransLiteAdminJS() {
-		function toggleSub() {
-			var val  = this.value;
-			var show = val === 'recurring';
-			slideOpts( this, show, '.frm_trans_sub_opts' );
-			toggleOpts( this, ! show, '.frm_gateway_no_recur' );
-		}
-
-		function slideOpts( opt, show, c ) {
-			var opts = jQuery( opt ).closest( '.frm_form_action_settings' ).find( c );
-			if ( show ) {
-				opts.slideDown( 'fast' );
-			} else {
-				opts.slideUp( 'fast' );
-			}
-		}
-
-		function toggleOpts( opt, show, c ) {
-			var opts = jQuery( opt ).closest( '.frm_form_action_settings' ).find( c );
-			if ( show ) {
-				opts.show();
-			} else {
-				opts.hide();
-			}
-		}
-
-		function runAjaxLink( e ) {
-			var $link, href, loadingImage, handleConfirmedClick;
-	
-			e.preventDefault();
-	
-			$link = jQuery( this );
-			handleConfirmedClick = ( e ) => {
-				e.preventDefault();
-	
-				href = $link.attr( 'href' );
-				loadingImage = document.createElement( 'span' );
-				loadingImage.className = 'frm-loading-img';
-				$link.replaceWith( loadingImage );
-				jQuery.ajax({
-					type: 'GET',
-					url: href,
-					data: {
-						nonce: frm_trans_vars.nonce
-					},
-					success: function( html ) {
-						jQuery( loadingImage ).replaceWith( html );
-					}
-				});
-			};
-	
-			jQuery( '#frm-confirmed-click' ).one( 'click', handleConfirmedClick );
-	
-			// prevent handleConfirmedClick from triggering when the current modal is closed so that it won't be run by other elements
-			const unbindHandleConfirmedClick = ( e ) => {
-				if ( e.target.matches( '.ui-widget-overlay, .dismiss' ) ) {
-					jQuery( '#frm-confirmed-click' ).unbind( 'click', handleConfirmedClick );
-					document.removeEventListener( 'click', unbindHandleConfirmedClick );
-				}
-			};
-	
-			document.addEventListener( 'click', unbindHandleConfirmedClick );
-	
-			return false;
-		}
-
 		return {
 			init: function() {
-				var actions = document.getElementById( 'frm_notification_settings' );
-				if ( actions !== null ) {
-					jQuery( actions ).on( 'change', '.frm_trans_type', toggleSub );
-				}
-
 				document.querySelectorAll( '.frm_trans_ajax_link' ).forEach(
 					link => link.addEventListener(
 						'click',
@@ -82,6 +12,44 @@
 				);
 			}
 		};
+	}
+
+	function runAjaxLink( e ) {
+		e.preventDefault();
+
+		const $link                = jQuery( this );
+		const handleConfirmedClick = e => {
+			e.preventDefault();
+
+			const href             = $link.attr( 'href' );
+			const loadingImage     = document.createElement( 'span' );
+			loadingImage.className = 'frm-loading-img';
+
+			$link.replaceWith( loadingImage );
+			jQuery.ajax({
+				type: 'GET',
+				url: href,
+				data: {
+					nonce: frm_trans_vars.nonce
+				},
+				success: function( html ) {
+					jQuery( loadingImage ).replaceWith( html );
+				}
+			});
+		};
+
+		jQuery( '#frm-confirmed-click' ).one( 'click', handleConfirmedClick );
+
+		// Prevent handleConfirmedClick from triggering when the current modal is closed so that it won't be run by other elements.
+		const unbindHandleConfirmedClick = e => {
+			if ( e.target.matches( '.ui-widget-overlay, .dismiss' ) ) {
+				jQuery( '#frm-confirmed-click' ).unbind( 'click', handleConfirmedClick );
+				document.removeEventListener( 'click', unbindHandleConfirmedClick );
+			}
+		};
+
+		document.addEventListener( 'click', unbindHandleConfirmedClick );
+		return false;
 	}
 
 	jQuery( document ).ready( frmTransLiteAdminJS().init );
