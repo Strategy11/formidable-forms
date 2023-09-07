531c72f1a29403e4785180ea6156b78549de1089
Truong Giang <truongwp@gmail.com>
2023-09-07 14:59:46 +0700

    Fix phpcs

--- tests/emails/test_FrmSummaryEmailsHelper.php
+++ tests/emails/test_FrmSummaryEmailsHelper.php
@@ -70,23 +70,23 @@ class test_FrmSummaryEmailsHelper extends FrmUnitTest {
 		$this->assertEquals( array( 'monthly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// Yearly was sent less than 1 month ago.
-		$options['last_yearly'] = date( 'Y-m-d', strtotime( '-29 days' ) );
+		$options['last_yearly'] = gmdate( 'Y-m-d', strtotime( '-29 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array(), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// Yearly was sent less than 1 month ago, and monthly was sent over 1 month ago.
-		$options['last_monthly'] = date( 'Y-m-d', strtotime( '-30 days' ) );
+		$options['last_monthly'] = gmdate( 'Y-m-d', strtotime( '-30 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array(), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// Both yearly and monthly were sent over 1 month ago.
-		$options['last_monthly'] = date( 'Y-m-d', strtotime( '-31 days' ) );
-		$options['last_yearly'] = date( 'Y-m-d', strtotime( '-31 days' ) );
+		$options['last_monthly'] = gmdate( 'Y-m-d', strtotime( '-31 days' ) );
+		$options['last_yearly'] = gmdate( 'Y-m-d', strtotime( '-31 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array( 'monthly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// Monthly was sent over 1 month ago, yearly was sent over 1 year ago.
-		$options['last_yearly'] = date( 'Y-m-d', strtotime( '-365 days' ) );
+		$options['last_yearly'] = gmdate( 'Y-m-d', strtotime( '-365 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array( 'yearly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
@@ -95,25 +95,25 @@ class test_FrmSummaryEmailsHelper extends FrmUnitTest {
 			'last_monthly' => '',
 			'last_yearly'  => '',
 			'last_license' => '',
-			'renewal_date' => date( 'Y-m-d', strtotime( '+1 day' ) ),
+			'renewal_date' => gmdate( 'Y-m-d', strtotime( '+1 day' ) ),
 		);
 		$this->save_options( $options );
 		$this->assertEquals( array( 'yearly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// Nothing set, renewal date is coming in more than 45 days.
-		$options['renewal_date'] = date( 'Y-m-d', strtotime( '+46 days' ) );
+		$options['renewal_date'] = gmdate( 'Y-m-d', strtotime( '+46 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array( 'monthly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// renewal date is coming, but monthly was sent less than 30 days ago.
-		$options['last_monthly'] = date( 'Y-m-d', strtotime( '-29 days' ) );
-		$options['renewal_date'] = date( 'Y-m-d', strtotime( '+1 day' ) );
+		$options['last_monthly'] = gmdate( 'Y-m-d', strtotime( '-29 days' ) );
+		$options['renewal_date'] = gmdate( 'Y-m-d', strtotime( '+1 day' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array(), FrmSummaryEmailsHelper::should_send_emails() );
 
 		// renewal date is coming, but yearly was sent less than 1 year ago.
 		$options['last_monthly'] = '';
-		$options['last_yearly']  = date( 'Y-m-d', strtotime( '-300 days' ) );
+		$options['last_yearly']  = gmdate( 'Y-m-d', strtotime( '-300 days' ) );
 		$this->save_options( $options );
 		$this->assertEquals( array( 'monthly' ), FrmSummaryEmailsHelper::should_send_emails() );
 
