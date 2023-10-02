fb5c4f63bc733b94a5f34bbe87013d90301ad99b
Sherv <sherv.elmi@gmail.com>
2023-10-02 10:01:06 +0300

    Remove unnecessary secondary license from file

--- js/form-templates/src/templates/index.js
+++ js/form-templates/src/templates/index.js
@@ -1,16 +1,1 @@
-/**
- * Copyright (C) 2023 Formidable Forms
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-
 export * from './categorizeTemplates';
--- js/form-templates/src/utils/validation.js
+++ js/form-templates/src/utils/validation.js
@@ -1,19 +1,4 @@
 /**
- * Copyright (C) 2023 Formidable Forms
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-
-/**
  * Internal dependencies
  */
 import { PREFIX, VIEW_SLUGS, FEATURED_TEMPLATES_KEYS } from '../shared';
