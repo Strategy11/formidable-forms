bbb82500be647163bbb1db873b5fbf240445fc74
Sherv <sherv.elmi@gmail.com>
2023-10-03 17:28:50 +0300

    Remove HTML comments from admin-cta.php to reduce page size

--- classes/views/shared/admin-cta.php
+++ classes/views/shared/admin-cta.php
@@ -15,14 +15,14 @@ if ( $id ) {
 	<div class="frm-cta-content">
 		<h4 class="frm-cta-title">
 			<?php echo FrmAppHelper::kses( $title, array( 'a', 'br', 'span', 'p', 'svg', 'use' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		</h4><!-- .frm-cta-title -->
+		</h4>
 
 		<span class="frm-cta-text">
 			<?php echo FrmAppHelper::kses( $description, array( 'a', 'br', 'span', 'p', 'svg', 'use' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		</span><!-- .frm-cta-text -->
-	</div><!-- .frm-cta-content -->
+		</span>
+	</div>
 
 	<a href="<?php echo esc_url( $link_url ); ?>" target="_blank" class="frm-cta-link button button-primary frm-button-primary">
 		<?php echo esc_html( $link_text ); ?>
-	</a><!-- .frm-cta-link -->
-</div><!-- .frm-cta -->
+	</a>
+</div>
