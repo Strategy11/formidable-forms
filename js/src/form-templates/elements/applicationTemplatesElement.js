be8b1f36c2a6105b5136764e75538ffa8e63a71e
Sherv <sherv.elmi@gmail.com>
2023-12-06 15:01:09 +0300

    Update frm_grid_container to frm-form-templates-grid-layout

--- js/src/form-templates/elements/applicationTemplatesElement.js
+++ js/src/form-templates/elements/applicationTemplatesElement.js
@@ -37,7 +37,7 @@ export function createApplicationTemplates( applications ) {
 				className: 'frm-text-sm frm-font-medium frm-mb-sm'
 			}),
 			tag( 'ul', {
-				className: `${PREFIX}-list frm_grid_container`,
+				className: `${PREFIX}-list ${PREFIX}-grid-layout`,
 				children: templateItems
 			})
 		]
@@ -58,7 +58,7 @@ function createTemplateItem( template ) {
 		`${thumbnailBaseURL}/placeholder.svg`;
 
 	return tag( 'li', {
-		className: `${PREFIX}-application-item frm-card-item frm4`,
+		className: `${PREFIX}-application-item frm-card-item`,
 		data: {
 			href: `${applicationsUrl}&triggerViewApplicationModal=1&template=${key}`,
 			'frm-search-text': name.toLowerCase()
