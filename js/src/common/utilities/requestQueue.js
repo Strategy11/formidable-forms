da3b84f13351f448efdfc9d86073c7cf7c6c261e
Sherv <sherv.elmi@gmail.com>
2023-12-07 23:25:42 +0300

    Refactor: Simplify addToRequestQueue for efficient promise execution

--- js/src/common/utilities/requestQueue.js
+++ js/src/common/utilities/requestQueue.js
@@ -7,4 +7,4 @@ let lastPromise = Promise.resolve();
  * @param {function(): Promise<any>} task A function that returns a promise.
  * @return {Promise<any>} The new last promise in the queue.
  */
-export const addToRequestQueue = task => lastPromise = lastPromise.then( () => task() ).catch( () => task() );
+export const addToRequestQueue = task => lastPromise = lastPromise.then( task ).catch( task );
