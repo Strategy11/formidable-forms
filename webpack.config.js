c33a58226be5cabfb2fe37c48a35f0528ab61b9f
stephywells <steph@strategy11.com>
2023-12-11 19:13:17 -0700

    Update node to resolve outdated dependencies Update production js minify to remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 const webpack = require( 'webpack' );
 const path = require( 'path' );
+const terser = require('terser-webpack-plugin');
 
 // Webpack configuration.
 const config = {
@@ -19,6 +20,18 @@ const config = {
 		formidable_blocks: './js/src/blocks.js',
 		formidable_overlay: './js/src/overlay.js',
 		'form-templates': './js/src/form-templates/index.js'
+	},
+	optimization: {
+		minimizer: [
+			new terser({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+				},
+			})
+		],
 	},
 	output: {
 		filename: '[name].js',
