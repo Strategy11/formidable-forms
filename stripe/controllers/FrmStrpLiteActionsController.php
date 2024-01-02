677176a2f909daf747925868b9d9a232f0fe6479
Mike Letellier <mike@crabcyb.org>
2024-01-02 16:35:49 -0400

    Add a nonce ignore line

--- stripe/controllers/FrmStrpLiteActionsController.php
+++ stripe/controllers/FrmStrpLiteActionsController.php
@@ -571,7 +571,7 @@ class FrmStrpLiteActionsController extends FrmTransLiteActionsController {
 	 * @return array
 	 */
 	public static function remove_cc_validation( $errors, $field, $values ) {
-		$has_processed = isset( $_POST[ 'frmintent' . $field->form_id ] );
+		$has_processed = isset( $_POST[ 'frmintent' . $field->form_id ] ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
 		if ( ! $has_processed ) {
 			return $errors;
 		}
