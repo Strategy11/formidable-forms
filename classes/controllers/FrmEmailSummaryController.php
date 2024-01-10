694f599c4269f07ee23cb3babc915ca787783cd3
Truong Giang <truongwp@gmail.com>
2024-01-10 19:31:16 +0700

    Skip yearly email if no entry in that period

--- classes/controllers/FrmEmailSummaryController.php
+++ classes/controllers/FrmEmailSummaryController.php
@@ -20,6 +20,8 @@ class FrmEmailSummaryController {
 	 */
 	public static function maybe_send_emails() {
 		$emails = FrmEmailSummaryHelper::should_send_emails();
+		self::maybe_skip_email( $emails );
+
 		if ( ! $emails ) {
 			return;
 		}
@@ -28,6 +30,27 @@ class FrmEmailSummaryController {
 			if ( method_exists( 'FrmEmailSummaryHelper', 'send_' . $email ) ) {
 				call_user_func( array( 'FrmEmailSummaryHelper', 'send_' . $email ) );
 			}
+		}
+	}
+
+	/**
+	 * Skips yearly email if there is no entry in that period.
+	 *
+	 * @since x.x
+	 *
+	 * @param array $emails Summary emails should be sent.
+	 */
+	private static function maybe_skip_email( &$emails ) {
+		$index = array_search( FrmEmailSummaryHelper::YEARLY, $emails );
+		if ( false === $index ) {
+			return;
+		}
+
+		$to_date       = FrmEmailSummaryHelper::get_date_from_today();
+		$from_date     = gmdate( 'Y-m-d', strtotime( $to_date . '-364 days' ) );
+		$entries_count = FrmEmailSummaryHelper::get_entries_count( $from_date, $to_date );
+		if ( ! $entries_count ) {
+			unset( $emails[ $index ] );
 		}
 	}
 }
