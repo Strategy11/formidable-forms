0c830b682cf78a8e1daafe4622dbf35a588a0af3
liviu13 <popa.liviu91@gmail.com>
2024-01-16 17:13:52 -0800

    bootstrap update v5

--- classes/controllers/FrmAppController.php
+++ classes/controllers/FrmAppController.php
@@ -632,7 +632,7 @@ class FrmAppController {
 		wp_register_script( 'formidable_dom', $plugin_url . '/js/admin/dom.js', array( 'jquery', 'jquery-ui-dialog', 'wp-i18n' ), $version, true );
 		wp_register_script( 'formidable_embed', $plugin_url . '/js/admin/embed.js', array( 'formidable_dom', 'jquery-ui-autocomplete' ), $version, true );
 		self::register_popper1();
-		wp_register_script( 'bootstrap_tooltip', $plugin_url . '/js/bootstrap.min.js', array( 'jquery', 'popper' ), '4.6.1', true );
+		wp_register_script( 'bootstrap_tooltip', $plugin_url . '/js/bootstrap.min.js', array( 'jquery', 'popper' ), '5.0.2', true );
 		wp_register_script( 'formidable_settings', $plugin_url . '/js/admin/settings.js', array(), $version, true );
 
 		$page = FrmAppHelper::simple_get( 'page', 'sanitize_title' );
@@ -827,7 +827,7 @@ class FrmAppController {
 	 * @return void
 	 */
 	private static function register_popper1() {
-		wp_register_script( 'popper', FrmAppHelper::plugin_url() . '/js/popper.min.js', array( 'jquery' ), '1.16.0', true );
+		wp_register_script( 'popper', FrmAppHelper::plugin_url() . '/js/popper.min.js', array( 'jquery' ), '2.9.2', true );
 	}
 
 	/**
