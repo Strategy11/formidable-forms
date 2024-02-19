fedd1ea8b6432dfb50e0e55dcdf58d1eb1bdbfbb
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-02-19 14:51:22 +0300

    Avoid phpcs ignore

--- classes/views/xml/posts_xml.php
+++ classes/views/xml/posts_xml.php
@@ -109,10 +109,12 @@ foreach ( (array) $terms as $term ) {
 
 	$frm_inc_tax[] = $term->term_id;
 	$label = ( 'category' === $term->taxonomy || 'tag' === $term->taxonomy ) ? $term->taxonomy : 'term';
-	$term_parent = ! empty( $term->parent ) ? '<term_parent>' . esc_html( $term->parent ) . '</term_parent>' : '';
 	?>
-	<term><term_id><?php echo esc_html( $term->term_id ); ?></term_id><?php echo $term_parent; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?><term_taxonomy><?php echo esc_html( $term->taxonomy ); ?></term_taxonomy>
+	<term><term_id><?php echo esc_html( $term->term_id ); ?></term_id><term_taxonomy><?php echo esc_html( $term->taxonomy ); ?></term_taxonomy>
 	<?php
+	if ( ! empty( $term->parent ) ) {
+		echo '<term_parent>' . esc_html( $term->parent ) . '</term_parent>';
+	}
 	if ( ! empty( $term->name ) ) {
 		echo '<term_name>' . FrmXMLHelper::cdata( $term->name ) . '</term_name>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
