f042ccd75007ebc955e5122283870ea4f09a7038
Truong Giang <truongwp@gmail.com>
2024-03-06 15:33:11 +0700

    Fix get submit field

--- classes/controllers/FrmSubmitButtonController.php
+++ classes/controllers/FrmSubmitButtonController.php
@@ -11,12 +11,7 @@ class FrmSubmitButtonController {
 	const FIELD_TYPE = 'submit';
 
 	public static function get_submit_field( $form_id ) {
-		$fields = FrmField::get_all_types_in_form( $form_id, self::FIELD_TYPE, 1 );
-		if ( ! $fields ) {
-			return false;
-		}
-
-		return reset( $fields );
+		return FrmField::get_all_types_in_form( $form_id, self::FIELD_TYPE, 1 );
 	}
 
 	/**
