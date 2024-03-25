75df03612388312312aede01d32222b61f107edd
Truong Giang <truongwp@gmail.com>
2024-03-25 20:10:55 +0700

    Fix unit tests

--- tests/fields/test_FrmSubmitHelper.php
+++ tests/fields/test_FrmSubmitHelper.php
@@ -2,6 +2,20 @@
 
 class test_FrmSubmitHelper extends FrmUnitTest {
 
+	public function test_copy_submit_field_settings_to_form() {
+		$form = $this->factory->form->create_and_get();
+		$this->assertEquals( $form, FrmProSubmitHelper::copy_submit_field_settings_to_form( $form ) );
+
+		$this->factory->field->create( array(
+			'form_id' => $form->id,
+			'type'    => 'submit',
+			'name'    => 'Submit form',
+		) );
+
+		$new_form = FrmProSubmitHelper::copy_submit_field_settings_to_form( $form );
+		$this->assertEquals( $new_form->options['submit_value'], 'Submit form' );
+	}
+
 	public function test_only_contains_submit_field() {
 		$fields = array(
 			array( 'type' => 'text' ),
