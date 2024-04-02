c5b101da31ed857f6da1d565b344425bf2359b9c
Truong Giang <truongwp@gmail.com>
2024-04-02 21:04:50 +0700

    Use note style for message

--- classes/views/frm-forms/settings-buttons.php
+++ classes/views/frm-forms/settings-buttons.php
@@ -18,7 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</tr>
 	<tr>
 		<td colspan="2">
-			<?php esc_html_e( 'Submit button settings were moved to the Submit button in the form builder.', 'formidable' ); ?>
+			<div class="frm_note_style" style="margin-top: 0;">
+				<?php esc_html_e( 'Submit button settings were moved to the Submit button in the form builder.', 'formidable' ); ?>
+			</div>
 		</td>
 	</tr>
 	<?php do_action( 'frm_add_form_button_options', $values ); ?>
