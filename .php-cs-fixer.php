89f862b051f78b56ea6047e49c68fa977fd02512
Mike Letellier <mike@crabcyb.org>
2024-04-17 21:55:40 -0300

    Add more to the php cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,11 +2,20 @@
 
 $finder = ( new PhpCsFixer\Finder() )->in( __DIR__ );
 $rules  = array(
-	'phpdoc_order'  => true,
-	'phpdoc_scalar' => true,
+	// Keep these rules for sure.
+	// 'phpdoc_order'  => true,
+	// 'phpdoc_scalar' => true,
+	// 'phpdoc_trim'   => true,
 );
 
 $config = new PhpCsFixer\Config();
 $config->setRules( $rules );
 
 return $config->setFinder( $finder );
+
+// Probably include these (but not right away).
+// 'phpdoc_types_order' => true,
+
+// Maybe include these.
+// 'phpdoc_separation' => true,
+// 'phpdoc_summary' => true,
