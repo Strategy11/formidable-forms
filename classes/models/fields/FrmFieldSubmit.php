fcefe309086c64dca308c086dbe6adb429188ad9
Truong Giang <truongwp@gmail.com>
2024-04-25 03:05:27 +0700

    Use the right form object

--- classes/models/fields/FrmFieldSubmit.php
+++ classes/models/fields/FrmFieldSubmit.php
@@ -94,23 +94,22 @@ DEFAULT_HTML;
 	 * @return string
 	 */
 	public function front_field_input( $args, $shortcode_atts ) {
-		$form = FrmForm::getOne( $this->field['form_id'] );
-		if ( ! FrmForm::show_submit( $form ) ) {
+		if ( ! FrmForm::show_submit( $args['form'] ) ) {
 			return '';
 		}
 
-		$form_action = FrmSubmitHelper::get_current_action_from_global_var( $form->id );
-		$values      = FrmAppHelper::setup_edit_vars( $form, 'forms' );
+		$form_action = FrmSubmitHelper::get_current_action_from_global_var( $args['form']->id );
+		$values      = FrmAppHelper::setup_edit_vars( $args['form'], 'forms' );
 		$filter_args = array(
 			'action' => $form_action,
 			'values' => $values,
-			'form'   => $form,
+			'form'   => $args['form'],
 		);
 
 		$submit_label = apply_filters( 'frm_submit_label', $this->field['name'], $filter_args );
 
 		ob_start();
-		FrmFormsHelper::get_custom_submit( $values['submit_html'], $form, $submit_label, $form_action, $values );
+		FrmFormsHelper::get_custom_submit( $values['submit_html'], $args['form'], $submit_label, $form_action, $values );
 		return ob_get_clean();
 	}
 }
