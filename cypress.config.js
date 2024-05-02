e985acc7765dd11078cd37ad8ecb2d66ca721d74
Mike Letellier <mike@crabcyb.org>
2024-05-02 09:53:53 -0300

    Try adding WP details to cypress workflow

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   fixturesFolder: "tests/cypress/fixtures",
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:8080',
     //baseUrl: 'http://devsite.formidableforms.com:8889',
     supportFile: "tests/cypress/support/e2e.js",
     specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
