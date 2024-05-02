7803708a77c4a061c41d8ab02ae41270ed3f7776
Mike Letellier <mike@crabcyb.org>
2024-05-02 09:23:48 -0300

    Update the spec (wip)

--- tests/cypress/e2e/spec.cy.js
+++ tests/cypress/e2e/spec.cy.js
@@ -1,33 +1,34 @@
 const login = name => {
-	cy.getCookies().then(cookies => {
-        let hasMatch = false;
-        cookies.forEach((cookie) => {
-            if (cookie.name.substr(0, 20) === 'wordpress_logged_in_') {
-                hasMatch = true;
-            }
-        });
-        if ( ! hasMatch ) {
-            cy.visit( 'http://localhost:8889/wp-login.php' ).wait( 1000 );
+    cy.session( name, () => {
+        cy.visit( 'http://devsite.formidableforms.com:8889/wp-login.php' );
 
-			// User login.
-			cy.get( '#user_login' ).type( name );
+        // User login.
+        cy.get( '#user_login' ).type( name );
 
-			// User pass.
-			cy.get( '#user_pass' ).type( 'password' );
+        // User pass.
+        cy.get( '#user_pass' ).type( 'password' );
 
-			// WP submit.
-			cy.get( '#wp-submit' ).click();
-        }
-	});
+        // WP submit.
+        cy.get( '#wp-submit' ).click();
+
+        cy.url().should('include', 'wp-admin');
+        cy.get( 'h1' ).should('contain.text', 'Dashboard');
+    });
 };
 
 describe( 'Configure WordPress', function() {
-    before(function () {
+    before(() => {
         login( 'admin' );
     });
 
-    it('Can visit admin page', function() {
-        cy.visit( 'http://localhost:8889/wp-admin' );
+    it('Can visit forms list and navigate to form templates page', () => {
+        cy.visit( 'http://devsite.formidableforms.com:8889/wp-admin/admin.php?page=formidable' );
+        cy.get( 'h1' ).should('contain.text', 'Forms');
+        const submitButton = cy.get( '#frm-publishing a.frm-button-primary' );
+        submitButton.should( 'contain.text', 'Add New' );
+        submitButton.click();
+
+        cy.url().should('include', 'admin.php?page=formidable-form-templates');
     });
 
 });
