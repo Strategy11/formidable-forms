1a9a237435ef0e56606a6d40ab099656fc9c547b
Mike Letellier <mike@crabcyb.org>
2024-05-02 14:01:06 -0300

    Try using the wp env defaults instead of a config function

--- cypress.config.js
+++ cypress.config.js
@@ -4,27 +4,12 @@ module.exports = defineConfig({
   fixturesFolder: "tests/cypress/fixtures",
   defaultCommandTimeout: 10000,
   e2e: {
-    //baseUrl: 'http://devsite.formidableforms.com:8889',
+    baseUrl: 'http://localhost:3000',
     supportFile: "tests/cypress/support/index.js",
     specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     video: false,
     retries: {
       runMode: 1,
-    },
-    async setupNodeEvents(on, config) {
-			const { loadConfig } = require('@wordpress/env/lib/config');
-
-			const wpEnvConfig = await loadConfig('../..');
-
-			if (wpEnvConfig) {
-				const port = wpEnvConfig.env.tests.port || null;
-
-				if (port) {
-					config.baseUrl = `http://${wpEnvConfig.env.tests.config.WP_TESTS_DOMAIN}:${port}`;
-				}
-			}
-
-			return config;
-		}
+    }
   },
 });
