e608f773f0aa600daacc497f6e0bceafb4d04d2a
Mike Letellier <mike@crabcyb.org>
2024-05-02 13:34:09 -0300

    Try another way using wp cli

--- cypress.config.js
+++ cypress.config.js
@@ -4,28 +4,13 @@ module.exports = defineConfig({
   fixturesFolder: "tests/cypress/fixtures",
   defaultCommandTimeout: 10000,
   e2e: {
-    //baseUrl: 'http://localhost:8889',
+    baseUrl: 'http://localhost:8889',
     //baseUrl: 'http://devsite.formidableforms.com:8889',
     supportFile: "tests/cypress/support/index.js",
     specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     video: false,
     retries: {
       runMode: 1,
-    },
-    async setupNodeEvents(on, config) {
-			const { loadConfig } = require('@wordpress/env/lib/config');
-
-			const wpEnvConfig = await loadConfig('../..');
-
-			if (wpEnvConfig) {
-				const port = wpEnvConfig.env.tests.port || null;
-
-				if (port) {
-					config.baseUrl = wpEnvConfig.env.tests.config.WP_SITEURL;
-				}
-			}
-
-			return config;
-		}
+    }
   },
 });
