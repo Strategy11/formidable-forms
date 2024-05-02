86d3b91405eb7f7a0d2d7ff69459a6acb4bb1a56
Mike Letellier <mike@crabcyb.org>
2024-05-02 14:09:30 -0300

    Try something more minimal

--- tests/cypress/e2e/spec.cy.js
+++ tests/cypress/e2e/spec.cy.js
@@ -1,17 +1,6 @@
-
-const login = () => {
-    cy.session('admin', () => {
-        cy.login();
-        cy.visit( '/wp-admin' );
-        cy.url().should('include', 'wp-admin');
-        cy.get( 'h1' ).should('contain.text', 'Dashboard');
-    });
-};
-
 describe( 'Run some basic Formidale tests', function() {
-    before( login );
-
     it('Can visit forms list and navigate to form templates page', () => {
+        cy.login();
         cy.visit( '/wp-admin/admin.php?page=formidable' );
         cy.get( 'h1' ).should('contain.text', 'Forms');
         const submitButton = cy.get( '#frm-publishing a.frm-button-primary' );
@@ -20,6 +9,4 @@ describe( 'Run some basic Formidale tests', function() {
 
         cy.url().should('include', 'admin.php?page=formidable-form-templates');
     });
-
 });
-
