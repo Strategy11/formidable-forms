949af874939141ee6b967d336250ddc2e76d453e
Mike Letellier <mike@crabcyb.org>
2024-05-22 10:34:20 -0300

    Add string as a type for the sanitize param

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -577,10 +577,10 @@ class FrmAppHelper {
 	/**
 	 * Get a value from $_POST data.
 	 *
-	 * @param string   $param      The key we are trying to access data from in $_POST.
-	 * @param mixed    $default    The default if nothing is being sent.
-	 * @param callable $sanitize   Make sure to pass a sanitize method here. This function will NOT sanitize by default.
-	 * @param bool     $serialized
+	 * @param string          $param      The key we are trying to access data from in $_POST.
+	 * @param mixed           $default    The default if nothing is being sent.
+	 * @param callable|string $sanitize   Make sure to pass a sanitize method here. This function will NOT sanitize by default.
+	 * @param bool            $serialized
 	 * @return mixed
 	 */
 	public static function get_post_param( $param, $default = '', $sanitize = '', $serialized = false ) {
