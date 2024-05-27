a2ffa89d42657f36016e2c9875c5d791416751c4
lauramekaj1 <lauramekaj@gmail.com>
2024-05-27 23:29:30 +0200

    create multiple forms and bulk delete them

--- tests/cypress/e2e/Forms/duplicateForm.cy.js
+++ tests/cypress/e2e/Forms/duplicateForm.cy.js
@@ -1,3 +1,4 @@
+import { createNewForm, deleteForm } from "../../support/commonActions.cy";
 describe("Duplicating a form from the form list page", () => {
     beforeEach(() => {
         cy.login();
@@ -6,15 +7,7 @@ describe("Duplicating a form from the form list page", () => {
 
     it("should create a duplicate form", () => {
         // Create a blank form
-        cy.log("Create a blank form");
-        cy.contains(".frm_nav_bar .button-primary", "Add New").click();
-        cy.get(".frm-form-templates-grid-layout #frm-form-templates-create-form").should("contain", "Create a blank form").click();
-        cy.get("#frm_submit_side_top").should("contain", "Save").click();
-        cy.get("#frm-form-templates-modal").should("exist");
-        cy.get(".frm-modal-title").should("contain", "Name your form");
-        cy.get("#frm_new_form_name_input").type("Test Form");
-        cy.get("#frm-save-form-name-button").should("contain", "Save").click();
-        cy.get("a[aria-label='Close']", { timeout: 5000 }).click();
+        createNewForm();
 
         // Duplicate the newly created form
         cy.log("Duplicate the newly created form");
@@ -36,25 +29,15 @@ describe("Duplicating a form from the form list page", () => {
 
         // Teardown: Delete Test Form and its duplicate
         cy.log("Teardown");
-        cy.log("Delete Test Form and its duplicate");
+        deleteForm();
+        
+        cy.log("Delete duplicated form")
         cy.contains('#the-list tr', 'Test Form').trigger('mouseover').then(($row) => {
-            console.log('Hovered Row:', $row);
-
-            // Find the visible element with class "trash" within the hovered row and click it
             cy.wrap($row).within(() => {
                 cy.get('.row-actions .trash .frm-trash-link').should('be.visible').click({ force: true });
             });
             cy.get("div[role='dialog']").should("contain", "Do you want to move this form to the trash?");
             cy.xpath("//a[@id='frm-confirmed-click']").should("contain", "Confirm").click({ force: true });
-
-            // To ensure elements are not chained from previous action's results
-            cy.contains('#the-list tr', 'Test Form').trigger('mouseover').then(($row) => {
-                cy.wrap($row).within(() => {
-                    cy.get('.row-actions .trash .frm-trash-link').should('be.visible').click({ force: true });
-                });
-                cy.get("div[role='dialog']").should("contain", "Do you want to move this form to the trash?");
-                cy.xpath("//a[@id='frm-confirmed-click']").should("contain", "Confirm").click({ force: true });
-            });
         });
     });
 });
