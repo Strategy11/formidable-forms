e094bd2e33344893728b6be419739996e3c7e42a
Mike Letellier <mike@crabcyb.org>
2024-06-13 12:30:49 -0300

    Test 404 issue

--- tests/cypress/e2e/validate-css.cy.js
+++ tests/cypress/e2e/validate-css.cy.js
@@ -48,14 +48,14 @@ describe( 'Run some CSS validation', function() {
     };
 
     it('Check frm_admin.css for valid CSS', () => {
-        validateCSS( '/wp-content/plugins/formidable/css/frm_admin.css' );
+        validateCSS( '/wp-content/plugins/formidable-forms/css/frm_admin.css' );
     });
 
     it('Check genarated CSS for valid CSS', () => {
         cy.login();
         cy.visit( '/wp-admin/admin.php?page=formidable-styles' );
         cy.get( '#frm_submit_side_top' ).click();
-        validateCSS( '/wp-content/plugins/formidable/css/formidableforms.css' );
+        validateCSS( '/wp-content/plugins/formidable-forms/css/formidableforms.css' );
     });
 
 });
