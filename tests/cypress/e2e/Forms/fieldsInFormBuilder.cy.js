60db714650e2206ea7d88813f29dd715d5577bde
lauramekaj1 <lauramekaj@gmail.com>
2024-06-16 17:47:58 +0200

    set fields as required in the form builder

--- tests/cypress/e2e/Forms/fieldsInFormBuilder.cy.js
+++ tests/cypress/e2e/Forms/fieldsInFormBuilder.cy.js
@@ -123,12 +123,94 @@ describe("Fields in the form builder", () => {
         });
     });
 
-    afterEach(() => {
+    it("should set fields as required and validate them in frontend", () => {
 
-        cy.log("Save the form");
-        cy.get("a[aria-label='Close']", { timeout: 5000 }).click({ force: true });
+        const fieldTypes = ['Text','Paragraph','Checkboxes','Radio Buttons','Dropdown','Email','Website/URL','Number','Name','Phone'];
 
-        cy.log("Teardown-Delete Form");
-        cy.deleteForm();
-    })
+        const openForm = () => {
+            cy.log("Click on the created form");
+            cy.contains('#the-list tr', 'Test Form').trigger('mouseover').then(($row) => {
+                cy.wrap($row).within(() => {
+                    cy.get('.column-name .row-title').should('exist').and('be.visible').then(($elem) => {
+                        console.log('Element is:', $elem);
+                        cy.wrap($elem).click({ force: true });
+                    });
+                });
+            });
+
+            cy.get('h1 > .frm_bstooltip').should("contain", "Test Form");
+            cy.get('.current_page').should("contain", "Build");
+            cy.get('.frm_field_list > #frm-nav-tabs > .frm-tabs > #frm_insert_fields_tab').should("contain", "Add Fields");
+        };
+
+        const createField = (fieldId, fieldType) => {
+            cy.log(`Create a ${fieldType} field`);
+            cy.get(`li[id="${fieldId}"] a[title="${fieldType}"]`).click({ force: true });
+        };
+
+        const requiredField = (fieldId, fieldType) => {
+            cy.log(`Set ${fieldType} field as require`);
+            cy.get(`li[data-ftype="${fieldId}"] [id^="field_"][id$="_inner_container"] > .frm-field-action-icons > .dropdown > .frm_bstooltip > .frmsvg > use`, { timeout: 10000 }).click({ force: true });
+            cy.get(`li[data-ftype="${fieldId}"] .frm_select_field > span`).should("contain", "Field Settings").click({ force: true });
+            cy.get('.frm_field_list div[id^="frm-single-settings-"] .frm_grid_container .frm-hide-empty input[type="checkbox"]', { timeout: 10000 }).check({ force: true });
+        };
+
+        openForm();
+
+        const fieldsToSetAsRequired = [
+            { fieldId: "text", fieldType: "Text" },
+            { fieldId: "textarea", fieldType: "Paragraph" },
+            { fieldId: "checkbox", fieldType: "Checkboxes" },
+            { fieldId: "radio", fieldType: "Radio Buttons" },
+            { fieldId: "select", fieldType: "Dropdown" },
+            { fieldId: "email", fieldType: "Email" },
+            { fieldId: "url", fieldType: "Website/URL" },
+            { fieldId: "number", fieldType: "Number" },
+            { fieldId: "name", fieldType: "Name" },
+            { fieldId: "phone", fieldType: "Phone" }
+        ];
+
+        fieldsToSetAsRequired.forEach(field => {
+            createField(field.fieldId, field.fieldType);
+            requiredField(field.fieldId, field.fieldType);
+        });
+
+        cy.log("Update form");
+        cy.get('#frm_submit_side_top').should("contain", "Update").click();
+        cy.wait(1000);
+
+        cy.log("Click on Preview - Blank Page");
+        cy.get("#frm-previewDrop").should("contain", "Preview").click();
+        cy.get('.preview > .frm-dropdown-menu > :nth-child(1) > a').should("contain", "On Blank Page").invoke('removeAttr', 'target').click();
+
+        cy.get("button[type='submit']").should("contain", "Submit").click();
+        cy.log("Check on error messages - Blank Page");
+        cy.get('.frm_error_style').should("contain", "There was a problem with your submission. Errors are marked below.");
+
+        fieldTypes.forEach(fieldType => {
+            cy.contains(`[id^="frm_error_field_"]`, `${fieldType} cannot be blank.`);
+        });
+
+        cy.log("Navigate back to the formidable form page");
+        cy.go(-2);
+
+        cy.log("Click on Preview - In Theme");
+        cy.get("#frm-previewDrop").should("contain", "Preview").click();
+        cy.get('.preview > .frm-dropdown-menu > :nth-child(2) > a').should("contain", "In Theme").invoke('removeAttr', 'target').click();
+
+        cy.get("button[type='submit']").should("contain", "Submit").click();
+        cy.log("Check on error messages - In Theme");
+        cy.get('.frm_error_style').should("contain", "There was a problem with your submission. Errors are marked below.");
+
+        fieldTypes.forEach(fieldType => {
+            cy.contains(`[id^="frm_error_field_"]`, `${fieldType} cannot be blank.`);
+        });
+    });    
+        afterEach(() => {
+            cy.log("Save the form");
+            cy.get("a[aria-label='Close']", { timeout: 5000 }).click({ force: true });
+    
+            cy.log("Teardown-Delete Form");
+            cy.deleteForm();
+    });
 });
