f0f22befeb48da4eba61aa76c369ac6924ffab52
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-07-08 18:15:26 +0300

    Deprecate FrmFieldType::get_select_atributes

--- classes/models/fields/FrmFieldType.php
+++ classes/models/fields/FrmFieldType.php
@@ -1207,7 +1207,7 @@ DEFAULT_HTML;
 	protected function select_tag( $values ) {
 		$field       = isset( $values['field'] ) ? $values['field'] : $this->field;
 		$input_html  = $this->get_field_input_html_hook( $field );
-		$select_atts = $this->get_select_atributes( $values );
+		$select_atts = $this->get_select_attributes( $values );
 		$select      = FrmAppHelper::array_to_html_params( $select_atts ) . ' ';
 
 		return '<select' . $select . $input_html . '>';
@@ -1216,7 +1216,7 @@ DEFAULT_HTML;
 	/**
 	 * @since 3.0
 	 */
-	protected function get_select_atributes( $values ) {
+	protected function get_select_attributes( $values ) {
 		$readonly    = ( FrmField::is_read_only( $this->field ) && ! FrmAppHelper::is_admin() );
 		$select_atts = array();
 		if ( ! $readonly ) {
@@ -1677,5 +1677,17 @@ DEFAULT_HTML;
 	protected function default_invalid_msg() {
 		_deprecated_function( __METHOD__, '6.8.3', 'FrmFieldsHelper::default_invalid_msg' );
 		return FrmFieldsHelper::default_invalid_msg();
+	}
+
+	/**
+	 * @deprecated x.x
+	 *
+	 * @param array $values
+	 *
+	 * @return array
+	 */
+	protected function get_select_atributes( $values ) {
+		_deprecated_function( __METHOD__, 'x.x', 'FrmFieldType::get_select_attributes' );
+		return $this->get_select_attributes( $values );
 	}
 }
