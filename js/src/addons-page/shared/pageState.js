cf940f076fba8da62d8dad1300a09381f4c1b01f
Sherv <sherv.elmi@gmail.com>
2024-07-09 16:17:01 +0300

    Remove empty setState and add comment

--- js/src/addons-page/shared/pageState.js
+++ js/src/addons-page/shared/pageState.js
@@ -3,7 +3,8 @@
  */
 import { getState, getSingleState, setState, setSingleState } from 'core/page-skeleton';
 
-setState({
-});
+// NOTE:
+// Use setState() here to add new states on the Add-Ons page.
+// Check the 'core/page-skeleton' package for shared states you can use.
 
 export { getState, getSingleState, setState, setSingleState };
