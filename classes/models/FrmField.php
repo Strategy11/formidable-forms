cc234b70da755d33dd31e0fb44d07267c03b8e5f
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-07-16 01:24:23 +0300

    Remove extra space

--- classes/models/FrmField.php
+++ classes/models/FrmField.php
@@ -211,7 +211,7 @@ class FrmField {
 				'require' => 'Simply Schedule Appointments',
 				'message' => sprintf(
 					/* translators: %1$s: Link opening HTML, %2$s: Link tag closing, %3$s: Image HTML */
-					__( 'Appointment fields are an integration with %1$s Simply Schedule Appointments%2$s. Get started now to schedule appointments directly from your forms. %3$s', 'formidable' ),
+					__( 'Appointment fields are an integration with %1$sSimply Schedule Appointments%2$s. Get started now to schedule appointments directly from your forms. %3$s', 'formidable' ),
 					'<a href="https://simplyscheduleappointments.com/meet/formidable/">',
 					'</a>',
 					'<img src="' . $images_url . 'appointments.png" alt="' . __( 'Scheduling', 'formidable' ) . '" />'
