3d656232b3a035a0730e1bf8e19707b2966516e8
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-07-22 14:49:32 +0300

    Revert "Add new field to fields shortcode before reload"
    This reverts commit aafa6f9a7eeebc8fd9993cc1c7d62e1cb5d5e390.

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -4847,7 +4847,6 @@ function frmAdminBuildJS() {
 					// prevent "More Options" tooltips from staying around after their target field is deleted.
 					deleteTooltips();
 				});
-				wp.hooks.doAction( 'frm_after_delete_field', $thisField[0] );
 			}
 		});
 	}
