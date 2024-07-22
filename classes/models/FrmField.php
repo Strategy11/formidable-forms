48c04dbada850f72348469c28081b0be3ac68aff
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-07-22 15:25:57 +0300

    Fix syntax error

--- classes/models/FrmField.php
+++ classes/models/FrmField.php
@@ -210,11 +210,12 @@ class FrmField {
 				'icon'    => 'frm_icon_font frm_calendar_icon frm_show_upgrade',
 				'require' => 'Simply Schedule Appointments',
 				'message' => sprintf(
-					/* translators: %1$s: Link opening HTML, %2$s: Link tag closing */
-					__( 'Appointment fields are an integration with %1$sSimply Schedule Appointments%2$s. Get started now to schedule appointments directly from your forms.', 'formidable' ),
-					'<a href="https://simplyscheduleappointments.com/meet/formidable/">',
-					'</a>',
-				) . '<img src="' . $images_url . 'appointments.png" alt="' . __( 'Scheduling', 'formidable' ) . '" />',
+								/* translators: %1$s: Link opening HTML, %2$s: Link tag closing */
+								__( 'Appointment fields are an integration with %1$sSimply Schedule Appointments%2$s. Get started now to schedule appointments directly from your forms.', 'formidable' ),
+								'<a href="https://simplyscheduleappointments.com/meet/formidable/">',
+								'</a>',
+							)
+							. '<img src="' . $images_url . 'appointments.png" alt="' . __( 'Scheduling', 'formidable' ) . '" />',
 				'link'    => 'https://simplyscheduleappointments.com/meet/formidable/',
 			),
 			'product'         => array(
