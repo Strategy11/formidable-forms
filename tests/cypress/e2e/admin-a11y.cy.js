bd5de6e96f2063505bc578ecfe96c41d51ea6c6d
lauramekaj1 <lauramekaj@gmail.com>
2024-07-26 11:14:21 +0200

    improve maintainability

--- tests/cypress/e2e/admin-a11y.cy.js
+++ tests/cypress/e2e/admin-a11y.cy.js
@@ -1,266 +1,166 @@
-describe( 'Run some accessibility tests', function() {
-    beforeEach( cy.login );
+describe('Run some accessibility tests', function() {
+    beforeEach(cy.login);
+
+    const configureAxeWithBaselineIgnoredRuleset = (rules) => {
+        cy.configureAxe({ rules });
+    };
+
+    const baselineRules = [
+        { id: 'color-contrast', enabled: false },
+        { id: 'aria-allowed-role', enabled: false },
+        { id: 'link-name', enabled: false },
+        { id: 'link-in-text-block', enabled: false }
+    ];
 
     it('Check the dashboard page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'heading-order', enabled: false },
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-dashboard' );
+        cy.visit('/wp-admin/admin.php?page=formidable-dashboard');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'heading-order', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the form list is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'empty-table-header', enabled: false },
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }
-                   ]
-            });
-        };
-        
-        cy.visit( '/wp-admin/admin.php?page=formidable' );
+        cy.visit('/wp-admin/admin.php?page=formidable');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'empty-table-header', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the entries page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'empty-table-header', enabled: false },
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'label', enabled:false },
-                    { id: 'select-name', enabled:false }      
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-entries' );
+        cy.visit('/wp-admin/admin.php?page=formidable-entries');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'empty-table-header', enabled: false },
+            { id: 'label', enabled: false },
+            { id: 'select-name', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the views page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'empty-table-header', enabled: false },
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'label', enabled:false },
-                    { id: 'select-name', enabled:false }                 
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-entries' );
+        cy.visit('/wp-admin/admin.php?page=formidable-entries');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'empty-table-header', enabled: false },
+            { id: 'label', enabled: false },
+            { id: 'select-name', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the styles page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'label', enabled:false },
-                    { id: 'label-title-only', enabled:false }                  
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-styles' );
+        cy.visit('/wp-admin/admin.php?page=formidable-styles');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            { id: 'aria-allowed-role', enabled: false },
+            { id: 'link-name', enabled: false },
+            { id: 'label', enabled: false },
+            { id: 'label-title-only', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the applications page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'image-alt', enabled: false }                
-                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-applications' );
+        cy.visit('/wp-admin/admin.php?page=formidable-applications');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'image-alt', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the form templates page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-form-templates' );
+        cy.visit('/wp-admin/admin.php?page=formidable-form-templates');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            { id: 'color-contrast', enabled: false },
+            { id: 'aria-allowed-role', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the payments page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-settings&t=stripe_settings' );
+        cy.visit('/wp-admin/admin.php?page=formidable-settings&t=stripe_settings');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules
+        ]);
         cy.checkA11y();
     });
 
     it('Check the import/export page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'heading-order', enabled: false }
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-import' );
+        cy.visit('/wp-admin/admin.php?page=formidable-import');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'heading-order', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the global settings page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-settings' );
+        cy.visit('/wp-admin/admin.php?page=formidable-settings');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules
+        ]);
         cy.checkA11y();
     });
 
     it('Check the Add-Ons page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }                   
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-addons' );
+        cy.visit('/wp-admin/admin.php?page=formidable-addons');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules
+        ]);
         cy.checkA11y();
     });
 
     it('Check the upgrade page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false },
-                    { id: 'empty-table-header', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-pro-upgrade' );
+        cy.visit('/wp-admin/admin.php?page=formidable-pro-upgrade');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'empty-table-header', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the SMTP page is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'landmark-unique', enabled: false },
-                    { id: 'link-name', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-smtp' );
+        cy.visit('/wp-admin/admin.php?page=formidable-smtp');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules,
+            { id: 'landmark-unique', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the form creation is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false }                 
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable-form-templates&return_page=forms' );
+        cy.visit('/wp-admin/admin.php?page=formidable-form-templates&return_page=forms');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            { id: 'color-contrast', enabled: false },
+            { id: 'aria-allowed-role', enabled: false }
+        ]);
         cy.checkA11y();
     });
 
     it('Check the list of deleted forms is accessible', () => {
-        const configureAxeWithBaselineIgnoredRuleset = () => {
-            cy.configureAxe({
-                rules: [
-                    { id: 'color-contrast', enabled: false },
-                    { id: 'aria-allowed-role', enabled: false } ,
-                    { id: 'link-in-text-block', enabled: false },
-                    { id: 'link-name', enabled: false }                
-                ]
-            });
-        };
-        cy.visit( '/wp-admin/admin.php?page=formidable&form_type=trash' );
+        cy.visit('/wp-admin/admin.php?page=formidable&form_type=trash');
         cy.injectAxe();
-        configureAxeWithBaselineIgnoredRuleset();
+        configureAxeWithBaselineIgnoredRuleset([
+            ...baselineRules
+        ]);
         cy.checkA11y();
     });
 });
