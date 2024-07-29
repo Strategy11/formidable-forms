fe1a4bef9e9017314b716a69f26b828aba47c396
Sherv <sherv.elmi@gmail.com>
2024-07-30 00:04:54 +0300

    Optimize add-ons `elements.js` file

--- js/src/addons-page/elements/elements.js
+++ js/src/addons-page/elements/elements.js
@@ -1,7 +1,11 @@
 /**
  * External dependencies
  */
-import { getElements, addElements, PREFIX as SKELETON_PREFIX } from 'core/page-skeleton';
+import {
+	getElements,
+	addElements,
+	PREFIX as SKELETON_PREFIX,
+} from 'core/page-skeleton';
 
 /**
  * Internal dependencies
@@ -10,60 +14,69 @@ import { PLANS, PREFIX, VIEWS } from '../constants';
 
 const { bodyContent, sidebar } = getElements();
 
-const categories = {
+bodyContent
+	.querySelectorAll( '.frm-card-item:not(.plugin-card-formidable-pro)' )
+	.forEach( ( addon ) => {
+		const categories = addon.dataset.categories;
+		switch ( true ) {
+			case categories.includes( PLANS.BUSINESS ):
+				addon.setAttribute(
+					'data-categories',
+					`${ categories },${ PLANS.ELITE }`
+				);
+				break;
+			case categories.includes( PLANS.PLUS ):
+				addon.setAttribute(
+					'data-categories',
+					`${ categories },${ PLANS.BUSINESS },${ PLANS.ELITE }`
+				);
+				break;
+			case categories.includes( PLANS.BASIC ):
+				addon.setAttribute(
+					'data-categories',
+					`${ categories },${ PLANS.PLUS },${ PLANS.BUSINESS },${ PLANS.ELITE }`
+				);
+				break;
+		}
+	} );
+
+addElements( {
+	// Body elements
+	upgradeBanner: document.getElementById( 'frm-upgrade-banner' ),
+
+	// Category elements
 	availableCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${VIEWS.AVAILABLE}"]`
+		`.${ SKELETON_PREFIX }-cat[data-category="${ VIEWS.AVAILABLE }"]`
 	),
 	activeCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${VIEWS.ACTIVE}"]`,
+		`.${ SKELETON_PREFIX }-cat[data-category="${ VIEWS.ACTIVE }"]`
 	),
-	categoriesTopDivider: sidebar.querySelector( `.${SKELETON_PREFIX}-divider` ),
+	categoriesTopDivider: sidebar.querySelector(
+		`.${ SKELETON_PREFIX }-divider`
+	),
 	basicPlanCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${PLANS.BASIC}"]`,
+		`.${ SKELETON_PREFIX }-cat[data-category="${ PLANS.BASIC }"]`
 	),
 	plusPlanCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${PLANS.PLUS}"]`,
+		`.${ SKELETON_PREFIX }-cat[data-category="${ PLANS.PLUS }"]`
 	),
 	businessPlanCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${PLANS.BUSINESS}"]`,
+		`.${ SKELETON_PREFIX }-cat[data-category="${ PLANS.BUSINESS }"]`
 	),
 	elitePlanCategory: sidebar.querySelector(
-		`.${SKELETON_PREFIX}-cat[data-category="${PLANS.ELITE}"]`,
+		`.${ SKELETON_PREFIX }-cat[data-category="${ PLANS.ELITE }"]`
 	),
-};
 
-bodyContent.querySelectorAll( '.frm-card-item:not(.plugin-card-formidable-pro)' ).forEach(addon => {
-	const categories = addon.dataset.categories;
-	switch (true) {
-		case categories.includes(PLANS.BUSINESS):
-			addon.setAttribute('data-categories', `${categories},${PLANS.ELITE}`);
-			break;
-		case categories.includes(PLANS.PLUS):
-			addon.setAttribute('data-categories', `${categories},${PLANS.BUSINESS},${PLANS.ELITE}`);
-			break;
-		case categories.includes(PLANS.BASIC):
-			addon.setAttribute('data-categories', `${categories},${PLANS.PLUS},${PLANS.BUSINESS},${PLANS.ELITE}`);
-			break;
-	}
-});
-
-const cards = {
-	addonsList: document.getElementById(`${PREFIX}-list`),
+	// Card elements
+	addonsList: document.getElementById( `${ PREFIX }-list` ),
 	addons: bodyContent.querySelectorAll( '.frm-card-item' ),
-	availableAddons: bodyContent.querySelectorAll('.frm-card-item:not(.frm-locked-item)'),
-	addonsToggle: bodyContent.querySelectorAll( '.frm_toggle_block' )
-};
+	availableAddons: bodyContent.querySelectorAll(
+		'.frm-card-item:not(.frm-locked-item)'
+	),
+	addonsToggle: bodyContent.querySelectorAll( '.frm_toggle_block' ),
 
-const upgradeBanner = document.getElementById( 'frm-upgrade-banner' );
-
-// Add children of the bodyContent to the elements object.
-const bodyContentChildren = bodyContent?.children;
-
-addElements({
-	...categories,
-	...cards,
-	upgradeBanner,
-	bodyContentChildren
-});
+	// Add children of the bodyContent to the elements object
+	bodyContentChildren: bodyContent?.children,
+} );
 
 export { getElements, addElements };
