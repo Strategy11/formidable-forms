4e16795268cd06b2ae65ba67e97b7d96659ef85c
Sherv <sherv.elmi@gmail.com>
2024-07-29 22:22:43 +0300

    Enhance `elements.js` file

--- js/src/form-templates/elements/elements.js
+++ js/src/form-templates/elements/elements.js
@@ -9,43 +9,39 @@ import { getElements, addElements, PREFIX as SKELETON_PREFIX } from 'core/page-s
 import { PREFIX, VIEW_SLUGS } from '../shared';
 
 const { bodyContent } = getElements();
+const templatesList = document.getElementById( `${PREFIX}-list` );
+const customTemplatesSection = document.getElementById( `${PREFIX}-custom-list-section` );
+const favoritesCategory = document.querySelector(
+	`.${SKELETON_PREFIX}-cat[data-category="${VIEW_SLUGS.FAVORITES}"]`
+);
+const modal = document.getElementById( `${PREFIX}-modal` );
 
-const bodyElements = {
-	headerCancelButton: document.getElementById( 'frm-publishing' ).querySelector( 'a' ),
+addElements({
+	// Body elements
+	headerCancelButton: document.getElementById( 'frm-publishing' )?.querySelector( 'a' ),
 	createFormButton: document.getElementById( `${PREFIX}-create-form` ),
 	pageTitle: document.getElementById( `${PREFIX}-page-title` ),
 	pageTitleText: document.getElementById( `${PREFIX}-page-title-text` ),
 	pageTitleDivider: document.getElementById( `${PREFIX}-page-title-divider` ),
 	upsellBanner: document.getElementById( `${PREFIX}-upsell-banner` ),
-	extraTemplateCountElements: document.querySelectorAll( `.${PREFIX}-extra-templates-count` )
-};
+	extraTemplateCountElements: document.querySelectorAll( `.${PREFIX}-extra-templates-count` ),
 
-// Templates Elements
-const templatesList = document.getElementById( `${PREFIX}-list` );
-const templates = {
+	// Templates elements
 	templatesList,
 	templateItems: templatesList.querySelectorAll( '.frm-card-item' ),
 	availableTemplateItems: templatesList.querySelectorAll( `.frm-card-item:not(.${PREFIX}-locked-item)` ),
 	freeTemplateItems: templatesList.querySelectorAll( '.frm-card-item.frm-free-template' ),
 	twinFeaturedTemplateItems: templatesList.querySelectorAll( `.${PREFIX}-featured-item` ),
 	firstLockedFreeTemplate: templatesList.querySelector( '.frm-free-template' ),
-	featuredTemplatesList: document.getElementById( `${PREFIX}-featured-list` )
-};
+	featuredTemplatesList: document.getElementById( `${PREFIX}-featured-list` ),
 
-// Custom Templates Section Element
-const customTemplatesSection = document.getElementById( `${PREFIX}-custom-list-section` );
-const customTemplates = {
+	// Custom Templates Section elements
 	customTemplatesSection,
 	customTemplateItems: customTemplatesSection.querySelectorAll( '.frm-card-item' ),
 	customTemplatesTitle: document.getElementById( `${PREFIX}-custom-list-title` ),
-	customTemplatesList: document.getElementById( `${PREFIX}-custom-list` )
-};
+	customTemplatesList: document.getElementById( `${PREFIX}-custom-list` ),
 
-// Sidebar Elements
-const favoritesCategory = document.querySelector(
-	`.${SKELETON_PREFIX}-cat[data-category="${VIEW_SLUGS.FAVORITES}"]`
-);
-const sidebarElements = {
+	// Sidebar elements
 	favoritesCategory,
 	favoritesCategoryCountEl: favoritesCategory?.querySelector(
 		`.${SKELETON_PREFIX}-cat-count`
@@ -55,12 +51,9 @@ const sidebarElements = {
 	),
 	freeTemplatesCategory: document.querySelector(
 		`.${SKELETON_PREFIX}-cat[data-category="${VIEW_SLUGS.FREE_TEMPLATES}"]`
-	)
-};
+	),
 
-// Modal Elements
-const modal = document.getElementById( `${PREFIX}-modal` );
-const modalElements = {
+	// Modal elements
 	modal,
 	modalItems: modal?.querySelectorAll( `.${PREFIX}-modal-item` ),
 	// Create New Template Modal
@@ -84,30 +77,17 @@ const modalElements = {
 	upgradeModal: document.getElementById( 'frm-form-upgrade-modal' ),
 	upgradeModalTemplateNames: modal?.querySelectorAll( '.frm-upgrade-modal-template-name' ),
 	upgradeModalPlansIcons: modal?.querySelectorAll( '.frm-upgrade-modal-plan-icon' ),
-	upgradeModalLink: document.getElementById( 'frm-upgrade-modal-link' )
-};
+	upgradeModalLink: document.getElementById( 'frm-upgrade-modal-link' ),
 
-// New Template Form Elements
-const newTemplateForm = document.getElementById( 'frm-new-template' );
-const newTemplateFormElements = {
-	newTemplateForm,
+	// New Template Form elements
+	newTemplateForm: document.getElementById( 'frm-new-template' ),
 	newTemplateNameInput: document.getElementById( 'frm_template_name' ),
 	newTemplateDescriptionInput: document.getElementById( 'frm_template_desc' ),
 	newTemplateLinkInput: document.getElementById( 'frm_link' ),
-	newTemplateActionInput: document.getElementById( 'frm_action_type' )
-};
+	newTemplateActionInput: document.getElementById( 'frm_action_type' ),
 
-// Add children of the bodyContent to the elements object.
-const bodyContentChildren = bodyContent?.children;
-
-addElements({
-	...bodyElements,
-	...templates,
-	...customTemplates,
-	...sidebarElements,
-	...modalElements,
-	...newTemplateFormElements,
-	bodyContentChildren
+	// Add children of the bodyContent to the elements object.
+	bodyContentChildren: bodyContent?.children
 });
 
 export { getElements, addElements };
