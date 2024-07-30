44607b1af366d1fddc5a642264c1e861e905f5af
Sherv <sherv.elmi@gmail.com>
2024-07-30 17:12:11 +0300

    Convert repeated .${SKELETON_PREFIX}-cat-count class into a variable

--- js/src/addons-page/ui/setupInitialView.js
+++ js/src/addons-page/ui/setupInitialView.js
@@ -12,6 +12,8 @@ import { getElements } from '../elements';
 import { categorizedAddons } from '../addons';
 import { PREFIX, VIEWS } from '../constants';
 
+const CAT_COUNT_CLASS = `.${ SKELETON_PREFIX }-cat-count`;
+
 /**
  * Sets up the initial view, performing any required
  * DOM manipulations for proper element presentation.
@@ -62,9 +64,7 @@ export function setupActiveCategory() {
 	categorizedAddons[ VIEWS.ACTIVE ] = activeAddons;
 
 	// Set "Active" category count
-	activeCategory.querySelector(
-		`.${ SKELETON_PREFIX }-cat-count`
-	).textContent = activeAddons.length;
+	activeCategory.querySelector( CAT_COUNT_CLASS ).textContent = activeAddons.length;
 }
 
 /**
@@ -81,9 +81,7 @@ function setupAvailableCategory() {
 
 	// Set "Available" category count
 	if ( availableCategory ) {
-		availableCategory.querySelector(
-			`.${ SKELETON_PREFIX }-cat-count`
-		).textContent = availableAddons.length;
+		availableCategory.querySelector( CAT_COUNT_CLASS ).textContent = availableAddons.length;
 
 		const upgradeBannerAvailableCount = bodyContent.querySelector(
 			`.${ PREFIX }-available-count`
@@ -105,9 +103,7 @@ function setupAllAddonsCategory() {
 	const { allItemsCategory, addons } = getElements();
 
 	// Set "All Add-Ons" category count
-	allItemsCategory.querySelector(
-		`.${ SKELETON_PREFIX }-cat-count`
-	).textContent = addons.length;
+	allItemsCategory.querySelector( CAT_COUNT_CLASS ).textContent = addons.length;
 }
 
 /**
@@ -125,12 +121,7 @@ function setupPlansCategory() {
 		elitePlanCategory,
 	} = getElements();
 
-	const getCount = ( category ) =>
-		parseInt(
-			category.querySelector( `.${ SKELETON_PREFIX }-cat-count` )
-				.textContent,
-			10
-		) || 0;
+	const getCount = ( category ) => parseInt( category.querySelector( CAT_COUNT_CLASS ).textContent, 10 ) || 0;
 
 	// The "Formidable Pro" add-on is included in all plans, so we just consider that in the basicCount
 	const basicCount = getCount( basicPlanCategory );
@@ -139,13 +130,7 @@ function setupPlansCategory() {
 	const eliteCount = getCount( elitePlanCategory ) - 1;
 
 	// Update the text content for each category
-	plusPlanCategory.querySelector(
-		`.${ SKELETON_PREFIX }-cat-count`
-	).textContent = basicCount + plusCount;
-	businessPlanCategory.querySelector(
-		`.${ SKELETON_PREFIX }-cat-count`
-	).textContent = basicCount + plusCount + businessCount;
-	elitePlanCategory.querySelector(
-		`.${ SKELETON_PREFIX }-cat-count`
-	).textContent = basicCount + plusCount + businessCount + eliteCount;
+	plusPlanCategory.querySelector( CAT_COUNT_CLASS ).textContent = basicCount + plusCount;
+	businessPlanCategory.querySelector( CAT_COUNT_CLASS ).textContent = basicCount + plusCount + businessCount;
+	elitePlanCategory.querySelector( CAT_COUNT_CLASS ).textContent = basicCount + plusCount + businessCount + eliteCount;
 }
