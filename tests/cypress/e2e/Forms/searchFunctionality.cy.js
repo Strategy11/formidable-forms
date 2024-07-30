8e0ae0d92d79dbb6826ad5588b84f886455bd7b0
lauramekaj1 <lauramekaj@gmail.com>
2024-07-30 22:31:55 +0200

    fix typo

--- tests/cypress/e2e/Forms/searchFunctionality.cy.js
+++ tests/cypress/e2e/Forms/searchFunctionality.cy.js
@@ -1,4 +1,4 @@
-describe("Seach functionality", () => {
+describe("Search functionality", () => {
     beforeEach(() => {
         cy.login();
         cy.visit('/wp-admin/admin.php?page=formidable');
@@ -16,19 +16,19 @@ describe("Seach functionality", () => {
         cy.get('#search-submit').click();
 
         cy.get('.published > .current').should("exist");
-        cy.get('.current > .count').should("contain","1");
-        cy.get('.displaying-num').should("contain","1");
+        cy.get('.current > .count').should("contain", "1");
+        cy.get('.displaying-num').should("contain", "1");
         cy.contains('#the-list tr', 'Test Form').should("exist");
 
         cy.log("Search for an invalid form title");
         cy.get('#entry-search-input').type("Test Form").clear();
         cy.get('#entry-search-input').type("Invalid Test Form {enter}");
-        cy.get('.current > .count').should("contain","0");
-        
+        cy.get('.current > .count').should("contain", "0");
+
         cy.get('#entry-search-input').clear();
         cy.get('#search-submit').click();
 
         cy.log("Teardown - Delete Test Form");
-        cy.deleteForm();     
+        cy.deleteForm();
     });
 });
