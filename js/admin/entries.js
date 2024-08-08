fce5e53431f8d9c27aa636bbb03189cb765c9bb5
Mike Letellier <mike@crabcyb.org>
2024-08-08 15:17:40 -0300

    Move frmtoggle logic from front end scripts to admin entries JS

--- js/admin/entries.js
+++ js/admin/entries.js
@@ -26,6 +26,11 @@ wp.domReady( () => {
 	 */
 	manageShowEmptyFieldsButton();
 
+	/**
+	 * This is used for the "Show empty fields" toggle.
+	 */
+	jQuery( 'a[data-frmtoggle]' ).on( 'click', toggleDiv );
+
 	function manageShowEmptyFieldsButton() {
 		const showEmptyFieldsButton = document.getElementById( 'frm-entry-show-empty-fields' );
 
@@ -48,4 +53,19 @@ wp.domReady( () => {
 			}, newShowState === 'true' ? 0 : 200 );
 		});
 	}
+
+	/**
+	 * @returns Toggle a target element open or closed.
+	 */
+	function toggleDiv() {
+		/*jshint validthis:true */
+		const div = jQuery( this ).data( 'frmtoggle' );
+		if ( jQuery( div ).is( ':visible' ) ) {
+			jQuery( div ).slideUp( 'fast' );
+		} else {
+			jQuery( div ).slideDown( 'fast' );
+		}
+		return false;
+	}
+
 });
