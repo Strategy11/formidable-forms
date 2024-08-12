1da5aca72421086e9a9aefd1afa8ee66f7492ba4
Truong Giang <truongwp@gmail.com>
2024-08-12 22:02:06 +0700

    Handled submission deactivation feedback

--- classes/controllers/FrmDeactivationFeedbackController.php
+++ classes/controllers/FrmDeactivationFeedbackController.php
@@ -20,6 +20,14 @@ class FrmDeactivationFeedbackController {
         wp_enqueue_style( 'formidable-admin' );
 
 		FrmAppHelper::localize_script( 'front' );
+
+		wp_localize_script(
+			'frm-deactivation-feedback',
+			'FrmDeactivationFeedbackI18n',
+			array(
+				'skip_text' => __( 'Skip & Deactivate', 'formidable' ),
+			)
+		);
 	}
 
 	public static function footer_html() {
@@ -27,17 +35,75 @@ class FrmDeactivationFeedbackController {
 			return;
 		}
 		?>
-		<div id="frmapi-feedback" class="frmapi-form" data-url="https://sandbox.formidableforms.com/api/wp-json/frm/v2/forms/feedback?return=html&exclude_script=jquery&exclude_style=formidable-css">
-			<span class="frm-wait frm_visible_spinner"></span>
-		</div>
-		<div id="frm-deactivation-popup" class="">
+		<div id="frm-deactivation-modal" class="">
 			<div class="metabox-holder">
 				<div class="postbox">
-					<?php FrmAppHelper::icon_by_class( 'frmfont frm_logo_icon' ); ?>
-                    <strong><?php esc_html_e( 'Quick Feedback', 'formidable' ); ?></strong>
+					<div class="inside">
+						<?php self::modal_icon(); ?>
+						<div id="frm-deactivation-form-wrapper" class="frmapi-form">
+							<span class="frm-wait frm_visible_spinner"></span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div><!-- End #frm-deactivation-popup -->
+		<style>
+			#frm-deactivation-modal {
+				--padding: 24px;
+			}
+			.frm_screen_reader {
+				border: 0;
+				clip: rect(1px, 1px, 1px, 1px);
+				-webkit-clip-path: inset(50%);
+				clip-path: inset(50%);
+				height: 1px;
+				margin: -1px;
+				overflow: hidden;
+				padding: 0;
+				position: absolute;
+				width: 1px;
+				word-wrap: normal !important; /* many screen reader and browser combinations announce broken words as they would appear visually */
+			}
+
+			.frm_deactivation_modal_icon {
+				position: absolute;
+				top: calc(var(--padding) - 1px);
+				left: var(--padding);
+			}
+
+			#frm-deactivation-modal .frm_form_title {
+				padding: 20px var(--padding) 20px calc(2 * var(--padding));
+				margin-top: 0;
+				border-bottom: 1px solid #f2f4f7;
+			}
+
+			#frm-deactivation-modal .frm_description,
+			#frm-deactivation-modal .frm_form_field {
+				padding-left: var(--padding);
+				padding-right: var(--padding);
+			}
+
+			#frm-deactivation-modal .inside {
+				padding-left: 0;
+				padding-right: 0;
+			}
+
+			#frm-deactivation-modal input[name="item_key"] + div {
+				display: none;
+			}
+
+			#frm-deactivation-modal .frm_submit {
+				text-align: right;
+			}
+		</style>
+		<?php
+	}
+
+	private static function modal_icon() {
+		?>
+		<svg class="frmsvg frm_deactivation_modal_icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20 20" xml:space="preserve" fill="#000000">
+			<path d="M9.6 12.8h4.8v2.5H9.6v-2.5zm3.8-8H6.7a1 1 0 0 0-1 1v1.5h8.7V4.8h-1z"/><path d="M13.3 8.8H5.6v6.5h2.5v-4h5.2a1 1 0 0 0 1-.7V8.8h-1z"/><path d="M10 20A10 10 0 0 1 0 10 10 10 0 0 1 10 0a10 10 0 0 1 10 10 10 10 0 0 1-10 10zm0-18.7A8.7 8.7 0 0 0 1.3 10a8.7 8.7 0 0 0 8.7 8.7 8.7 8.7 0 0 0 8.7-8.7A8.7 8.7 0 0 0 10 1.3z"/>
+		</svg>
 		<?php
 	}
 }
--- js/admin/deactivation-feedback.js
+++ js/admin/deactivation-feedback.js
@@ -3,32 +3,109 @@
 
 	const selectors = '#deactivate-formidable, #deactivate-formidable-pro';
 
-	function frmapiGetData( frmcont ) {
-		jQuery.ajax({
-			dataType: 'json',
-			url: 'http://frmdev.test/wp-json/frm/v2/forms/deactivation-feedback?plugin_name=Formidable&return=html&exclude_script=jquery&exclude_style=formidable-css',
-			success: ( json ) => {
-				console.log( json );
-				const form = json.renderedHtml.replace( /<link\b[^>]*(formidableforms.css|action=frmpro_css)[^>]*>/gi, '' );
-				jQuery( '#frmapi-feedback' ).html( form );
+	let deactivationModal, deactivationUrl;
+
+	const Modal = {
+		init: function( id, width ) {
+			const $info = jQuery( id );
+			const self  = this;
+
+			if ( ! $info.length ) {
+				return false;
 			}
+
+			if ( typeof width === 'undefined' ) {
+				width = '550px';
+			}
+
+			const dialogArgs = {
+				dialogClass: 'frm-dialog',
+				modal: true,
+				autoOpen: false,
+				closeOnEscape: true,
+				width: width,
+				resizable: false,
+				draggable: false,
+				open: function() {
+					jQuery( '.ui-dialog-titlebar' ).addClass( 'frm_hidden' ).removeClass( 'ui-helper-clearfix' );
+					jQuery( '#wpwrap' ).addClass( 'frm_overlay' );
+					jQuery( '.frm-dialog' ).removeClass( 'ui-widget ui-widget-content ui-corner-all' );
+					$info.removeClass( 'ui-dialog-content ui-widget-content' );
+					self.bindClickForDialogClose( $info );
+				},
+				close: function() {
+					jQuery( '#wpwrap' ).removeClass( 'frm_overlay' );
+					jQuery( '.spinner' ).css( 'visibility', 'hidden' );
+
+					this.removeAttribute( 'data-option-type' );
+					const optionType = document.getElementById( 'bulk-option-type' );
+					if ( optionType ) {
+						optionType.value = '';
+					}
+				}
+			};
+
+			$info.dialog( dialogArgs );
+
+			return $info;
+		},
+
+		bindClickForDialogClose: function( $modal ) {
+			const closeModal = function() {
+				$modal.dialog( 'close' );
+			};
+			jQuery( '.ui-widget-overlay' ).on( 'click', closeModal );
+			$modal.on( 'click', 'a.dismiss', closeModal );
+		}
+	};
+
+	const addSkipBtn = formEl => {
+		const btn = frmDom.a( {
+			text: FrmDeactivationFeedbackI18n.skip_text,
+			href: deactivationUrl
 		});
-	}
+
+		formEl.querySelector( '.frm_submit' ).prepend( btn );
+	};
 
 	const onClickDeactivate = event => {
 		event.preventDefault();
-		frmapiGetData( jQuery( event.target ) );
+
+		if ( ! deactivationModal ) {
+			deactivationModal = Modal.init(
+				'#frm-deactivation-modal',
+				'440px'
+			);
+		}
+
+		deactivationUrl = event.target.href;
+
+		const pluginName = event.target.closest( '.plugin-title' ).querySelector( 'strong' ).innerText;
+
+		const url = 'http://frmdev.test/wp-json/frm/v2/forms/deactivation-feedback?plugin_name=' + pluginName + '&return=html&exclude_script=jquery&exclude_style=formidable-css';
+
+		const response = fetch( url, {
+			method: 'GET'
+		});
+
+		response
+			.then( response => response.json() )
+			.then( response => {
+				const wrapper = document.getElementById( 'frm-deactivation-form-wrapper' );
+				const form    = response.renderedHtml.replace( /<link\b[^>]*(formidableforms.css|action=frmpro_css)[^>]*>/gi, '' );
+
+				wrapper.innerHTML = form;
+				addSkipBtn( wrapper );
+				deactivationModal.dialog( 'open' );
+			})
+			.catch( error => {
+				console.error( error );
+			});
 	};
 
 	frmDom.util.documentOn( 'click', selectors, onClickDeactivate );
 
-	/*document.addEventListener( 'frmFormCompleteBeforeReplace', function( a, b ) {
-		console.log( a );
-		console.log( b );
-	});*/
-
-	jQuery.on( document, 'frmFormComplete', function( a, b ) {
-		console.log( a );
-		console.log( b );
+	document.addEventListener( 'frmFormCompleteBeforeReplace', function( event ) {
+		window.location.href = deactivationUrl;
 	});
 }() );
