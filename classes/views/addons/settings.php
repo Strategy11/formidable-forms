2b11fa974a94e514031a4e3923f715633f15de08
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-08-14 09:02:17 +0300

    Echo dynamic icon class

--- classes/views/addons/settings.php
+++ classes/views/addons/settings.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</div>
 			<div class="edd_frm_unauthorized frm8 frm_form_field <?php echo esc_attr( $activate === 'deactivate' ) ? 'frm_hidden' : ''; ?>">
 				<input id="edd_<?php echo esc_attr( $slug ); ?>_license_key" name="edd_<?php echo esc_attr( $slug ); ?>_license_key" type="text" class="frm_addon_license_key auto_width" value="" />
-				<span><?php FrmAppHelper::icon_by_class( 'frm_icon_font frm_cancel1_icon edd_frm_status_icon' ); ?></span>
+				<span><?php FrmAppHelper::icon_by_class( 'frm_icon_font frm_cancel1_icon edd_frm_status_icon ' . esc_attr( $icon_class ) ); ?></span>
 				<input type="button" class="button-secondary frm-button-secondary edd_frm_save_license" data-plugin="<?php echo esc_attr( $slug ); ?>" name="edd_<?php echo esc_attr( $slug ); ?>_license_activate" value="<?php esc_attr_e( 'Activate', 'formidable' ); ?>"/>
 				<p class="frm_license_msg"></p>
 			</div>
