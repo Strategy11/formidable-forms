076445f97e5712ddf8eea7ab3d7c19e072fab6d2
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-08-14 08:50:34 +0300

    Fix regression

--- classes/views/addons/settings.php
+++ classes/views/addons/settings.php
@@ -27,7 +27,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</div>
 			<div class="edd_frm_unauthorized frm8 frm_form_field <?php echo esc_attr( $activate === 'deactivate' ) ? 'frm_hidden' : ''; ?>">
 				<input id="edd_<?php echo esc_attr( $slug ); ?>_license_key" name="edd_<?php echo esc_attr( $slug ); ?>_license_key" type="text" class="frm_addon_license_key auto_width" value="" />
-				<span class="frm_icon_font frm_error_icon edd_frm_status_icon <?php echo esc_attr( $icon_class ); ?>"></span>
 				<span><?php FrmAppHelper::icon_by_class( 'frm_icon_font frm_cancel1_icon edd_frm_status_icon' ); ?></span>
 				<input type="button" class="button-secondary frm-button-secondary edd_frm_save_license" data-plugin="<?php echo esc_attr( $slug ); ?>" name="edd_<?php echo esc_attr( $slug ); ?>_license_activate" value="<?php esc_attr_e( 'Activate', 'formidable' ); ?>"/>
 				<p class="frm_license_msg"></p>
