0d253ec79a9049f817c5e0c7a53f8fdc654e38fb
Truong Giang <truongwp@gmail.com>
2024-09-13 17:30:26 +0700

    Add doc comment

--- js/src/onboarding-wizard/ui/showError.js
+++ js/src/onboarding-wizard/ui/showError.js
@@ -1,13 +1,15 @@
 /**
  * Internal dependencies
  */
-import { PREFIX } from '../shared';
 import { showFormError } from '../utils';
 
 /**
  * Displays errors related to the email address field.
  *
- * @param {string} type The categorization of the error (e.g., "invalid", "empty").
+ * @since x.x Added the `input` param.
+ *
+ * @param {string}           type  The categorization of the error (e.g., "invalid", "empty").
+ * @param {HTMLInputElement} input The input element to which the error is related.
  * @return {void}
  */
 export const showEmailAddressError = ( type, input ) => {
