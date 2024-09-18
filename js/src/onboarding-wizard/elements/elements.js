bd5c3c2d46241655c76afed5194e69d3e9fac596
Sherv <sherv.elmi@gmail.com>
2024-09-18 17:14:22 +0300

    Improve: Onboarding Wizard elements file

--- js/src/onboarding-wizard/elements/elements.js
+++ js/src/onboarding-wizard/elements/elements.js
@@ -8,62 +8,40 @@ import { createPageElements } from 'core/factory';
  */
 import { PREFIX, WELCOME_STEP_ID } from '../shared';
 
-// Body Elements
-const bodyElements = {
+export const { getElements, addElements } = createPageElements({
+	// Body Elements
 	onboardingWizardPage: document.getElementById( `${PREFIX}-wizard-page` ),
 	pageBackground: document.getElementById( `${PREFIX}-bg` ),
 	container: document.getElementById( `${PREFIX}-container` ),
 	steps: document.querySelectorAll( `.${PREFIX}-step` ),
 	skipStepButtons: document.querySelectorAll( `.${PREFIX}-skip-step` ),
 	backButtons: document.querySelectorAll( `.${PREFIX}-back-button` ),
-	hiddenLicenseKeyInput: document.getElementById( 'frm-license-key' )
-};
+	hiddenLicenseKeyInput: document.getElementById( 'frm-license-key' ),
 
-// Welcome Step Elements
-const welcomeStep = {
-	welcomeStep: document.getElementById( WELCOME_STEP_ID )
-};
-
-// Install Formidable Pro Step Elements
-const installFormidableProStep = {
+	// Install Formidable Pro Step Elements
 	installFormidableProStep: document.getElementById( `${PREFIX}-install-formidable-pro-step` ),
 	checkProInstallationButton: document.getElementById( `${PREFIX}-check-pro-installation-button` ),
 	skipProInstallationButton: document.getElementById( `${PREFIX}-skip-pro-installation-button` ),
-	checkProInstallationError: document.getElementById( `${PREFIX}-check-pro-installation-error` )
-};
+	checkProInstallationError: document.getElementById( `${PREFIX}-check-pro-installation-error` ),
 
-// License Management Step Elements
-const licenseManagementStep = {
+	// License Management Step Elements
 	licenseManagementStep: document.getElementById( `${PREFIX}-license-management-step` ),
 	licenseKeyInput: document.getElementById( 'edd_formidable_pro_license_key' ),
-	saveLicenseButton: document.getElementById( `${PREFIX}-save-license-button` )
-};
+	saveLicenseButton: document.getElementById( `${PREFIX}-save-license-button` ),
 
-// Default Email Address Step Elements
-const emailStep = {
+	// Welcome Step Elements
+	welcomeStep: document.getElementById( WELCOME_STEP_ID ),
+
+	// Default Email Address Step Elements
 	setupEmailStepButton: document.getElementById( `${PREFIX}-setup-email-step-button` ),
 	defaultEmailField: document.getElementById( `${PREFIX}-default-email-field` ),
 	subscribeCheckbox: document.getElementById( `${PREFIX}-subscribe` ),
 	summaryEmailsCheckbox: document.getElementById( `${PREFIX}-summary-emails` ),
-	allowTrackingCheckbox: document.getElementById( `${PREFIX}-allow-tracking` )
-};
+	allowTrackingCheckbox: document.getElementById( `${PREFIX}-allow-tracking` ),
 
-// Install Formidable Add-ons Step Elements
-const installAddonsStep = {
-	installAddonsButton: document.getElementById( `${PREFIX}-install-addons-button` )
-};
+	// Install Formidable Add-ons Step Elements
+	installAddonsButton: document.getElementById( `${PREFIX}-install-addons-button` ),
 
-// Success Step Elements
-const successStep = {
+	// Success Step Elements
 	successStep: document.getElementById( `${PREFIX}-success-step` )
-};
-
-export const { getElements, addElements } = createPageElements({
-	...bodyElements,
-	...installFormidableProStep,
-	...licenseManagementStep,
-	...welcomeStep,
-	...emailStep,
-	...installAddonsStep,
-	...successStep
 });
