d34a3ee25c87dbcacceb1c1fab6c93937b2fc91b
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-10-09 15:37:23 +0300

    Cut down repeated code

--- classes/models/FrmFieldFormHtml.php
+++ classes/models/FrmFieldFormHtml.php
@@ -395,16 +395,7 @@ class FrmFieldFormHtml {
 		}
 
 		if ( $this->field_obj->get_field_column( 'type' ) === 'name' ) {
-			if ( isset( $this->pass_args['errors'][ 'field' . $this->field_id . '-first' ] ) ) {
-				$shortcode_atts['aria-invalid-first'] = 'true';
-			} else {
-				$shortcode_atts['aria-invalid-first'] = 'false';
-			}
-			if ( isset( $this->pass_args['errors'][ 'field' . $this->field_id . '-last' ] ) ) {
-				$shortcode_atts['aria-invalid-last'] = 'true';
-			} else {
-				$shortcode_atts['aria-invalid-last'] = 'false';
-			}
+			$this->set_aria_invalid_error_for_name_part( $shortcode_atts );
 		} else {
 			$shortcode_atts['aria-invalid'] = isset( $this->pass_args['errors'][ 'field' . $this->field_id ] ) ? 'true' : 'false';
 		}
@@ -412,6 +403,20 @@ class FrmFieldFormHtml {
 		$this->field_obj->set_field_column( 'shortcodes', $shortcode_atts );
 
 		return $shortcode_atts;
+	}
+
+	/**
+	 * Set the aria-invalid attribute for the first and last name fields.
+	 *
+	 * @since x.x
+	 *
+	 * @param array $shortcode_atts
+	 * @return void
+	 */
+	private function set_aria_invalid_error_for_name_part( &$shortcode_atts ) {
+		foreach ( array( 'first', 'last' ) as $name_part ) {
+			$shortcode_atts[ 'aria-invalid-' . $name_part ] = isset( $this->pass_args['errors'][ 'field' . $this->field_id . '-' . $name_part ] ) ? 'true' : 'false';
+		}
 	}
 
 	/**
