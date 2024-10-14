9ef1d1d337d6bccea6e1e0cb6de6797c8273f172
Sherv <sherv.elmi@gmail.com>
2024-10-14 17:45:15 +0300

    Finish consent tracking step

--- classes/views/onboarding-wizard/steps/consent-tracking-step.php
+++ classes/views/onboarding-wizard/steps/consent-tracking-step.php
@@ -44,12 +44,74 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</div>
 
 		<div class="frm-dropdown-menu frm-mt-sm" aria-labelledby="frm-onboarding-consent-tracking-list">
-			<div class="frm-flex frm-gap-xs frm-items-center">
-				<span><?php FrmAppHelper::icon_by_class( 'frmfont frm_user_icon', array( 'aria-hidden' => 'true' ) ); ?></span>
+			<div class="frm-flex frm-gap-xs frm-items-center frm-py-sm">
+				<span><?php FrmAppHelper::icon_by_class( 'frmfont frm_user_icon frm_svg15', array( 'aria-hidden' => 'true' ) ); ?></span>
 
 				<div class="frm-flex-col frm-gap-2xs frm-ml-2xs">
-					<h4 class="frm-text-sm frm-font-medium frm-text-grey-700 frm-m-0"><?php esc_html_e( 'View Basic Profile Info', 'formidable' ); ?></h4>
-					<span class="frm-text-xs frm-text-grey-500 frm-m-0">Your WordPress user’s: first & last name and email address</span>
+					<h4 class="frm-flex frm-gap-xs frm-items-center frm-text-sm frm-font-medium frm-text-grey-700 frm-m-0">
+						<?php
+						esc_html_e( 'View Basic Profile Info', 'formidable' );
+
+						FrmAppHelper::tooltip_icon(
+							__( 'Never miss important updates, get security warnings before they become public knowledge, and receive notifications about special offers and awesome new features.', 'formidable' ),
+							array(
+								'class' => 'frm-inline-flex',
+							)
+						);
+						?>
+					</h4>
+					<span class="frm-text-xs frm-text-grey-500 frm-m-0"><?php esc_html_e( 'Your WordPress user’s: first & last name and email address', 'formidable' ); ?></span>
+				</div>
+			</div>
+
+			<div class="frm-flex frm-gap-xs frm-items-center frm-py-sm">
+				<span><?php FrmAppHelper::icon_by_class( 'frmfont frm_sample_form_icon frm_svg15', array( 'aria-hidden' => 'true' ) ); ?></span>
+
+				<div class="frm-flex-col frm-gap-2xs frm-ml-2xs">
+					<h4 class="frm-flex frm-gap-xs frm-items-center frm-text-sm frm-font-medium frm-text-grey-700 frm-m-0">
+						<?php
+						esc_html_e( 'View Basic Website Info', 'formidable' );
+
+						FrmAppHelper::tooltip_icon(
+							__( 'To provide additional functionality that’s relevant to your website, avoid WordPress or PHP incompatibilities that can break your website, and recognize which languages & regions the plugin should be translated and tailored to.', 'formidable' ),
+							array(
+								'class' => 'frm-inline-flex',
+							)
+						);
+						?>
+					</h4>
+					<span class="frm-text-xs frm-text-grey-500 frm-m-0"><?php esc_html_e( 'Homepage URL & title, WP & PHP versions, site language', 'formidable' ); ?></span>
+				</div>
+			</div>
+
+			<div class="frm-flex frm-gap-xs frm-items-center frm-py-sm">
+				<span><?php FrmAppHelper::icon_by_class( 'frmfont frm_puzzle_icon_thin frm_svg15', array( 'aria-hidden' => 'true' ) ); ?></span>
+
+				<div class="frm-flex-col frm-gap-2xs frm-ml-2xs">
+					<h4 class="frm-flex frm-gap-xs frm-items-center frm-text-sm frm-font-medium frm-text-grey-700 frm-m-0">
+						<?php esc_html_e( 'View Basic Plugin Info', 'formidable' ); ?>
+					</h4>
+					<span class="frm-text-xs frm-text-grey-500 frm-m-0"><?php esc_html_e( 'Current plugin & SDK versions, and if active or uninstalled', 'formidable' ); ?></span>
+				</div>
+			</div>
+
+			<div class="frm-flex frm-gap-xs frm-items-center frm-py-sm">
+				<span><?php FrmAppHelper::icon_by_class( 'frmfont frm-field-colors-style frm_svg20', array( 'aria-hidden' => 'true' ) ); ?></span>
+
+				<div class="frm-flex-col frm-gap-2xs frm-ml-2xs">
+					<h4 class="frm-flex frm-gap-xs frm-items-center frm-text-sm frm-font-medium frm-text-grey-700 frm-m-0">
+						<?php
+						esc_html_e( 'View Plugins & Themes List', 'formidable' );
+
+						FrmAppHelper::tooltip_icon(
+							__( 'To ensure compatibility and avoid conflicts with your installed plugins and themes.', 'formidable' ),
+							array(
+								'class' => 'frm-inline-flex',
+							)
+						);
+						?>
+					</h4>
+					<span class="frm-text-xs frm-text-grey-500 frm-m-0"><?php esc_html_e( 'Names, slugs, versions, and if active or not', 'formidable' ); ?></span>
 				</div>
 			</div>
 		</div>
