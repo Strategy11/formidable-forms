840e60bdf927cf16d2e21ce81635aa0c4f2bfb50
Mike Letellier <mike@crabcyb.org>
2024-10-18 09:36:53 -0300

    Drop IP warning from settings

--- classes/controllers/FrmAppController.php
+++ classes/controllers/FrmAppController.php
@@ -649,8 +649,6 @@ class FrmAppController {
 
 			FrmInbox::maybe_disable_screen_options();
 		}
-
-		self::maybe_add_ip_warning();
 	}
 
 	/**
@@ -674,40 +672,6 @@ class FrmAppController {
 		if ( class_exists( $class ) && method_exists( $class, 'load_page' ) ) {
 			call_user_func( array( $class, 'load_page' ) );
 		}
-	}
-
-	/**
-	 * Show a warning for the IP address setting if it hasn't been set.
-	 *
-	 * @since 6.1
-	 *
-	 * @return void
-	 */
-	private static function maybe_add_ip_warning() {
-		$settings = FrmAppHelper::get_settings();
-		if ( false !== $settings->custom_header_ip ) {
-			// The setting has been changed from the false default (to either 1 or 0), so stop showing the message.
-			return;
-		}
-
-		if ( ! self::is_behind_proxy() ) {
-			// This message is only applicable when using a reverse proxy.
-			return;
-		}
-
-		if ( FrmAppHelper::get_post_param( 'frm_action', '', 'sanitize_text_field' ) ) {
-			// Avoid the message on a POST action. We don't want to show the message if we're saving global settings.
-			return;
-		}
-
-		$global_settings_link = admin_url( 'admin.php?page=formidable-settings' ) . '#frm_custom_header_ip';
-		$message              = sprintf(
-			// Translators: 1: Global Settings Link
-			__( 'IP addresses in form submissions may no longer be accurate! If you are experiencing issues, we recommend going to %1$s and enabling the "Use custom headers when retrieving IPs with form submissions." setting.', 'formidable' ),
-			'<a href="' . esc_url( $global_settings_link ) . '">' . __( 'Global Settings', 'formidable' ) . '</a>'
-		);
-		$option_name = 'frm_dismiss_ip_address_notice';
-		FrmAppHelper::add_dismissable_warning_message( $message, $option_name );
 	}
 
 	/**
