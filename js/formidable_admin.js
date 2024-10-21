3609412277b4ea9376cc7bdda6ade0d566927a3d
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-10-21 21:32:04 +0300

    Move a variable assignment to a lower scope

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -6132,7 +6132,6 @@ function frmAdminBuildJS() {
 			let input = false;
 			let showSelect = ( valueFieldType === 'select' || valueFieldType === 'checkbox' || valueFieldType === 'radio' );
 			const showText = ( valueFieldType === 'text' || valueFieldType === 'email' || valueFieldType === 'phone' || valueFieldType === 'url' || valueFieldType === 'number' );
-			const comparison = document.querySelector( `#frm_logic_${fieldID}_${metaKey} [name="field_options[hide_field_cond_${fieldID}][]"]` ).value;
 
 			if ( showSelect ) {
 				isTaxonomy = document.getElementById( 'frm_has_hidden_options_' + val );
@@ -6143,6 +6142,7 @@ function frmAdminBuildJS() {
 			}
 
 			if ( showSelect || showText ) {
+				const comparison = document.querySelector( `#frm_logic_${fieldID}_${metaKey} [name="field_options[hide_field_cond_${fieldID}][]"]` ).value;
 				fill.innerHTML = '';
 				const creatingValuesDropdown = showSelect && ! [ 'LIKE', 'not LIKE', 'LIKE%', '%LIKE' ].includes( comparison );
 				if ( creatingValuesDropdown ) {
