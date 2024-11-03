3530ac070fcb8f32b470f7c24c10e3c68ec63f58
Truong Giang <truongwp@gmail.com>
2024-11-03 22:49:25 +0700

    Improve JS code

--- js/admin/usage-tracking.js
+++ js/admin/usage-tracking.js
@@ -3,25 +3,18 @@
 
 	// Track edit form clicked link.
 	frmDom.util.documentOn( 'click', '.toplevel_page_formidable #the-list .column-primary .row-actions a', function( event ) {
-		sendData({
-			key: 'edit_form_clicked_link',
-			value: event.target.parentNode.className
-		});
+		sendData( 'edit_form_clicked_link', event.target.parentNode.className );
 	});
 
 	// Track form templates.
 	frmDom.util.documentOn( 'click', '.frm-form-templates-use-template-button', function( event ) {
-		sendData({
-			key: 'form_templates',
-			value: event.target.closest( 'li' ).dataset.slug
-		});
+		sendData( 'form_templates', event.target.closest( 'li' ).dataset.slug );
 	});
 
-	const sendData = ( data ) => {
+	const sendData = ( key, value ) => {
 		const formData = new FormData();
-		Object.keys( data ).forEach( key => {
-			formData.append( key, data[ key ] );
-		});
+		formData.append( 'key', key );
+		formData.append( 'value', value );
 		frmDom.ajax.doJsonPost( 'track_flows', formData );
 	};
 }() );
