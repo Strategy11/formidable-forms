60836f659b3fc33ad42adf46095db8316581ede0
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2024-12-05 15:44:03 +0300

    Undo changes from other branch

--- classes/views/frm-forms/add_field.php
+++ classes/views/frm-forms/add_field.php
@@ -4,9 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <li id="frm_field_id_<?php echo esc_attr( $field['id'] ); ?>" class="<?php echo esc_attr( $li_classes ); ?>" data-fid="<?php echo esc_attr( $field['id'] ); ?>" data-formid="<?php echo esc_attr( 'divider' === $field['type'] ? $field['form_select'] : $field['form_id'] ); ?>" data-ftype="<?php echo esc_attr( $display['type'] ); ?>" data-type="<?php echo esc_attr( $field['type'] ); ?>">
-<?php
-if ( $field['type'] === 'divider' ) {
-	?>
+<?php if ( $field['type'] === 'divider' ) { ?>
 <div class="divider_section_only">
 <?php } ?>
 
