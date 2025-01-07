212146e8ee0899b3fba219d401b33fb316546ec7
Mike Letellier <mike@crabcyb.org>
2025-01-07 16:58:57 -0400

    Re-use code in inbox code

---
+++ classes/helpers/FrmApiHelper.php
@@ -0,0 +1,118 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+/**
+ * @since x.x
+ */
+class FrmApiHelper {
+
+	/**
+	 * @since x.x
+	 *
+	 * @param array $sale
+	 * @return bool
+	 */
+	public static function is_for_user( $sale ) {
+		if ( ! isset( $sale['who'] ) || $sale['who'] === 'all' ) {
+			return true;
+		}
+		$who = (array) $sale['who'];
+		if ( self::is_for_everyone( $who ) ) {
+			return true;
+		}
+		if ( self::is_user_type( $who ) ) {
+			return true;
+		}
+		if ( in_array( 'free_first_30', $who, true ) && self::is_free_first_30() ) {
+			return true;
+		}
+		if ( in_array( 'free_not_first_30', $who, true ) && self::is_free_not_first_30() ) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @param array $who
+	 * @return bool
+	 */
+	private static function is_for_everyone( $who ) {
+		return in_array( 'all', $who, true );
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @param array $who
+	 * @return bool
+	 */
+	private static function is_user_type( $who ) {
+		return in_array( self::get_user_type(), $who, true );
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @return string
+	 */
+	private static function get_user_type() {
+		if ( ! FrmAppHelper::pro_is_installed() ) {
+			return 'free';
+		}
+
+		return FrmAddonsController::license_type();
+	}
+
+	/**
+	 * Check if user is still using the Lite version only, and within
+	 * the first 30 days of activation.
+	 *
+	 * @since x.x
+	 *
+	 * @return bool
+	 */
+	private static function is_free_first_30() {
+		return self::is_free() && self::is_first_30();
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @return bool
+	 */
+	private static function is_first_30() {
+		$activation_timestamp = get_option( 'frm_first_activation' );
+		if ( false === $activation_timestamp ) {
+			// If the option does not exist, assume that it is
+			// because the user was active before this option was introduced.
+			return false;
+		}
+		$cutoff = strtotime( '-30 days' );
+		return $activation_timestamp > $cutoff;
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @return bool
+	 */
+	private static function is_free_not_first_30() {
+		return self::is_free() && ! self::is_first_30();
+	}
+
+	/**
+	 * Check if the Pro plugin is active. If not, consider the user to be on the free version.
+	 *
+	 * @since x.x
+	 *
+	 * @return bool
+	 */
+	private static function is_free() {
+		return ! FrmAppHelper::pro_is_included();
+	}
+
+}
--- classes/models/FrmSalesAPI.php
+++ classes/models/FrmSalesAPI.php
@@ -140,108 +140,6 @@ class FrmSalesApi extends FrmFormApi {
 	}
 
 	/**
-	 * Show different sales for different accounts.
-	 *
-	 * @param array $sale
-	 * @return bool
-	 */
-	private function is_for_user( $sale ) {
-		if ( ! isset( $sale['who'] ) || $sale['who'] === 'all' ) {
-			return true;
-		}
-		$who = (array) $sale['who'];
-		if ( $this->is_for_everyone( $who ) ) {
-			return true;
-		}
-		if ( $this->is_user_type( $who ) ) {
-			return true;
-		}
-		if ( in_array( 'free_first_30', $who, true ) && $this->is_free_first_30() ) {
-			return true;
-		}
-		if ( in_array( 'free_not_first_30', $who, true ) && $this->is_free_not_first_30() ) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * @since x.x
-	 *
-	 * @param array $who
-	 * @return bool
-	 */
-	private function is_for_everyone( $who ) {
-		return in_array( 'all', $who, true );
-	}
-
-	/**
-	 * @since x.x
-	 *
-	 * @param array $who
-	 * @return bool
-	 */
-	private function is_user_type( $who ) {
-		return in_array( $this->get_user_type(), $who, true );
-	}
-
-	private function get_user_type() {
-		if ( ! FrmAppHelper::pro_is_installed() ) {
-			return 'free';
-		}
-
-		return FrmAddonsController::license_type();
-	}
-
-	/**
-	 * Check if user is still using the Lite version only, and within
-	 * the first 30 days of activation.
-	 *
-	 * @since x.x
-	 *
-	 * @return bool
-	 */
-	private function is_free_first_30() {
-		return $this->is_free() && $this->is_first_30();
-	}
-
-	/**
-	 * @since x.x
-	 *
-	 * @return bool
-	 */
-	private function is_first_30() {
-		$activation_timestamp = get_option( 'frm_first_activation' );
-		if ( false === $activation_timestamp ) {
-			// If the option does not exist, assume that it is
-			// because the user was active before this option was introduced.
-			return false;
-		}
-		$cutoff = strtotime( '-30 days' );
-		return $activation_timestamp > $cutoff;
-	}
-
-	/**
-	 * @since x.x
-	 *
-	 * @return bool
-	 */
-	private function is_free_not_first_30() {
-		return $this->is_free() && ! $this->is_first_30();
-	}
-
-	/**
-	 * Check if the Pro plugin is active. If not, consider the user to be on the free version.
-	 *
-	 * @since x.x
-	 *
-	 * @return bool
-	 */
-	private function is_free() {
-		return ! FrmAppHelper::pro_is_included();
-	}
-
-	/**
 	 * @since x.x
 	 *
 	 * @return array|false
@@ -257,7 +155,7 @@ class FrmSalesApi extends FrmFormApi {
 
 		$best_sale = false;
 		foreach ( self::$sales as $sale ) {
-			if ( ! $this->is_for_user( $sale ) ) {
+			if ( ! FrmApiHelper::is_for_user( $sale ) ) {
 				continue;
 			}
 
