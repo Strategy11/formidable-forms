f2ee8d890da95877bb9604058a10c4626fce0b32
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-01-09 17:32:51 +0300

    Avoid selecting text when multiselecting fields

--- js/formidable_admin.js
+++ js/formidable_admin.js
@@ -4411,9 +4411,6 @@ function frmAdminBuildJS() {
 						}
 					}
 				);
-
-				// when holding shift and clicking, text gets selected. unselect it.
-				document.getSelection().removeAllRanges();
 			}
 		} else {
 			// not multi-selecting
@@ -10706,6 +10703,9 @@ function frmAdminBuildJS() {
 			handleShowPasswordLiveUpdate();
 			document.addEventListener( 'scroll', updateShortcodesPopupPosition, true );
 			document.addEventListener( 'change', handleBuilderChangeEvent );
+			document.onselectstart = function( e ) {
+				return e.target.closest( '.frm_form_builder' );
+			};
 		},
 
 		settingsInit: function() {
