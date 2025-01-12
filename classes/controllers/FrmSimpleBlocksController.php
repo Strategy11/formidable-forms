c4cf8b923ec1d4e5918f13cb44ee255050bb1180
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-01-12 21:41:43 +0300

    Escape custom css class rendered

--- classes/controllers/FrmSimpleBlocksController.php
+++ classes/controllers/FrmSimpleBlocksController.php
@@ -207,7 +207,7 @@ class FrmSimpleBlocksController {
 
 		$form .= FrmFormsController::get_form_shortcode( $params );
 		if ( ! empty( $attributes['className'] ) ) {
-			$form = preg_replace( '/\bfrm_forms\b/', 'frm_forms ' . $attributes['className'], $form, 1 );
+			$form = preg_replace( '/\bfrm_forms\b/', 'frm_forms ' . esc_attr( $attributes['className'] ), $form, 1 );
 		}
 		return self::maybe_remove_fade_on_load_for_block_preview( $form );
 	}
