0addb4cf318c9242067e133612e01413bd5ee007
Sherv <sherv.elmi@gmail.com>
2025-02-03 19:28:32 +0300

    Refactor format-dropdown view and add options view

---
+++ classes/views/frm-fields/back-end/format-dropdown-options.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package Formidable
+ * @since x.x
+ *
+ * @var array        $field Field data.
+ * @var array        $args  Includes 'field', 'display', and 'values' settings.
+ * @var FrmFieldType $this  Field type object.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+$field_type = $field['type'];
+$format     = FrmField::get_option( $field, 'format' );
+
+FrmHtmlHelper::echo_dropdown_option( __( 'None', 'formidable' ), '' === $format, array( 'value' => 'none' ) );
+
+FrmHtmlHelper::echo_dropdown_option(
+	in_array( $field_type, array( 'number', 'range' ) ) ? __( 'Custom', 'formidable' ) : __( 'Number', 'formidable' ),
+	false,
+	array(
+		'value'        => '',
+		'class'        => 'frm_show_upgrade frm_noallow',
+		'data-upgrade' => __( 'Format number field', 'formidable' ),
+		'data-medium'  => 'format-number-field',
+	)
+);
+
+if ( 'text' === $field_type ) {
+	FrmHtmlHelper::echo_dropdown_option(
+		__( 'Custom', 'formidable' ),
+		! empty( $format ) && 'currency' !== $format,
+		array(
+			'value'           => 'custom',
+			'data-dependency' => '#frm-field-format-custom-' . $field_id,
+		)
+	);
+}
--- classes/views/frm-fields/back-end/format-dropdown.php
+++ classes/views/frm-fields/back-end/format-dropdown.php
@@ -12,9 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You are not allowed to call this page directly.' );
 }
 
-$field_id   = $field['id'];
-$field_type = $field['type'];
-$format     = FrmField::get_option( $field, 'format' );
+$field_id = $field['id'];
 ?>
 <p class="frm6 frm_form_field frm-format-dropdown">
 	<label for="format_dropdown_<?php echo esc_attr( $field_id ); ?>">
@@ -27,44 +25,18 @@ $format     = FrmField::get_option( $field, 'format' );
 		class="frm_format_dropdown frm_select_with_upgrade frm_select_with_dependency"
 		data-field-id="<?php echo intval( $field_id ); ?>"
 	>
-		<option value="none" <?php selected( $format, '' ); ?>>
-			<?php esc_html_e( 'None', 'formidable' ); ?>
-		</option>
-
 		<?php
-		$number_option_text = in_array( $field_type, array( 'number', 'range' ) ) ? esc_html__( 'Custom', 'formidable' ) : esc_html__( 'Number', 'formidable' );
-		$number_option      = '
-			<option value="" class="frm_show_upgrade frm_noallow" data-upgrade="' . esc_attr__( 'Format number field', 'formidable' ) . '" data-medium="format-number-field">
-				' . $number_option_text . '
-			</option>';
+		$options_view_path = FrmAppHelper::plugin_path() . '/classes/views/frm-fields/back-end/format-dropdown-options.php';
 
 		/**
-		 * Filter the number option HTML for the format dropdown.
+		 * Includes the formatted options view file.
 		 *
 		 * @since x.x
 		 *
-		 * @param string $number_option       The HTML for the number option.
-		 * @param string $number_option_text The text for the number option.
-		 * @param array  $field              The field array.
-		 * @return string The filtered HTML for the number option.
+		 * @param string $options_view_path The path to the options view file.
 		 */
-		$number_option = apply_filters( 'frm_print_format_number_option', $number_option, $number_option_text, $field );
-
-		$add_allowed_html = function ( $allowed_html ) {
-			$allowed_html['option']['data-dependency'] = true;
-			return $allowed_html;
-		};
-
-		add_filter( 'frm_striphtml_allowed_tags', $add_allowed_html );
-		FrmAppHelper::kses_echo( $number_option, array( 'option' ) );
-		remove_filter( 'frm_striphtml_allowed_tags', $add_allowed_html );
+		require apply_filters( 'frm_format_options_view_path', $options_view_path );
 		?>
-
-		<?php if ( 'text' === $field_type ) { ?>
-			<option value="custom" data-dependency="#frm-field-format-custom-<?php echo esc_attr( $field_id ); ?>" <?php selected( ! empty( $format ) && 'currency' !== $format, true ); ?>>
-				<?php esc_html_e( 'Custom', 'formidable' ); ?>
-			</option>
-		<?php } ?>
 	</select>
 </p>
 
