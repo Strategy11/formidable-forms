58cc6f3deef84181bad78386c1874538b1c8384d
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-02-05 14:23:27 +0300

    Include device too small message for every admin page

--- classes/controllers/FrmFormsController.php
+++ classes/controllers/FrmFormsController.php
@@ -1866,11 +1866,36 @@ class FrmFormsController {
 		return $errors;
 	}
 
+	/**
+	 * Includes html that shows a message when the device is too small to use Formidable Forms admin pages.
+	 *
+	 * @since x.x
+	 * @return void
+	 */
+	private static function include_device_too_small_message() {
+		?>
+		<div class="frm_small_device_message_container">
+			<div class="frm_small_device_message">
+				<svg width="32" height="49" viewBox="0 0 32 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect x="1" y="1" width="30" height="47" rx="3" fill="#F5FAFF" stroke="#4199FD" stroke-width="2"/>
+					<rect x="12" y="5" width="8" height="2" rx="1" fill="#C0DDFE"/>
+					<path d="M23 33C23 29.6863 20.0899 27 16.5 27C12.9101 27 10 29.6863 10 33" stroke="#80BBFE" stroke-width="1.5"/>
+					<circle cx="10.5" cy="20.5" r="1" fill="#80BBFE" stroke="#80BBFE"/>
+					<circle cx="21.5" cy="20.5" r="1" fill="#80BBFE" stroke="#80BBFE"/>
+				</svg>
+				<b><?php esc_html_e( 'More on bigger devices', 'formidable' ); ?></b>
+				<p><?php esc_html_e( 'For the best experience, we recommend using Formidable Forms on larger devices such as a desktop or tablet.', 'formidable' ); ?></p>
+				<a href="javascript:history.back()" class="frm-button-primary"><?php esc_html_e( 'Go back', 'formidable' ); ?></a>
+			</div>
+		</div>
+		<?php
+	}
+
 	public static function route() {
 		$action = isset( $_REQUEST['frm_action'] ) ? 'frm_action' : 'action';
 		$vars   = array();
 		FrmAppHelper::include_svg();
-
+		self::include_device_too_small_message();
 		if ( isset( $_POST['frm_compact_fields'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
 			FrmAppHelper::permission_check( 'frm_edit_forms' );
 
--- classes/views/frm-forms/edit.php
+++ classes/views/frm-forms/edit.php
@@ -5,20 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div id="frm_builder_page" class="frm_wrap">
 	<div class="frm_page_container">
-	<div class="frm_small_device_message_container">
-		<div class="frm_small_device_message">
-			<svg width="32" height="49" viewBox="0 0 32 49" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<rect x="1" y="1" width="30" height="47" rx="3" fill="#F5FAFF" stroke="#4199FD" stroke-width="2"/>
-				<rect x="12" y="5" width="8" height="2" rx="1" fill="#C0DDFE"/>
-				<path d="M23 33C23 29.6863 20.0899 27 16.5 27C12.9101 27 10 29.6863 10 33" stroke="#80BBFE" stroke-width="1.5"/>
-				<circle cx="10.5" cy="20.5" r="1" fill="#80BBFE" stroke="#80BBFE"/>
-				<circle cx="21.5" cy="20.5" r="1" fill="#80BBFE" stroke="#80BBFE"/>
-			</svg>
-			<b><?php esc_html_e( 'More on bigger devices', 'formidable' ); ?></b>
-			<p><?php esc_html_e( 'For the best experience, we recommend using Formidable Forms on larger devices such as a desktop or tablet.', 'formidable' ); ?></p>
-			<a href="javascript:history.back()" class="frm-button-primary"><?php esc_html_e( 'Go back', 'formidable' ); ?></a>
-		</div>
-	</div>
 	<?php
 	require FrmAppHelper::plugin_path() . '/classes/views/frm-forms/mb_insert_fields.php';
 	FrmAppHelper::get_admin_header(
