50c6a0f3e8658167bf9ba18abef9464ce712f659
Sherv <sherv.elmi@gmail.com>
2025-02-17 22:47:54 +0300

    Apply saved sort preferences to Forms and History lists

--- classes/helpers/FrmFormsListHelper.php
+++ classes/helpers/FrmFormsListHelper.php
@@ -47,19 +47,7 @@ class FrmFormsListHelper extends FrmListHelper {
 			)
 		);
 
-		$screen = get_current_screen();
-		if ( $screen ) {
-			$user_id             = get_current_user_id();
-			$preferred_list_sort = get_user_meta( $user_id, 'frm_preferred_list_sort_' . $screen->id, true );
-
-			if ( is_array( $preferred_list_sort ) && ! empty( $preferred_list_sort['orderby'] ) ) {
-				$orderby = $preferred_list_sort['orderby'];
-
-				if ( ! empty( $preferred_list_sort['order'] ) ) {
-					$order = $preferred_list_sort['order'];
-				}
-			}
-		}
+		FrmAppController::apply_saved_sort_preference( $orderby, $order );
 
 		$start   = self::get_param(
 			array(
