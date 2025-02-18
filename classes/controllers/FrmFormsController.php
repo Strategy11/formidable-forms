8b6d30fd7188ab94d336809ab2874929e31a9314
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-02-18 10:42:54 +0300

    Incorporate some review feedbacks

--- classes/controllers/FrmFormsController.php
+++ classes/controllers/FrmFormsController.php
@@ -2533,7 +2533,7 @@ class FrmFormsController {
 	 */
 	public static function dismiss_default_email_message() {
 		check_ajax_referer( 'frm_ajax', 'nonce' );
-		update_user_meta( wp_get_current_user()->ID, 'frm_dismiss_default_email_message', 1 );
+		update_user_meta( get_current_user_id(), 'frm_dismiss_default_email_message', 1 );
 	}
 
 	/**
