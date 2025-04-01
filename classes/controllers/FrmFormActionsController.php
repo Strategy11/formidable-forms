7d0c38df4117efa65c775112fddff35c8b5c11a3
Mike Letellier <mike@crabcyb.org>
2025-04-01 12:20:47 -0300

    Drop redundant braces

--- classes/controllers/FrmFormActionsController.php
+++ classes/controllers/FrmFormActionsController.php
@@ -583,7 +583,7 @@ class FrmFormActionsController {
 				continue;
 			}
 
-			$child_entry = ( ( is_numeric( $form->parent_form_id ) && $form->parent_form_id ) || ( $entry && ( $entry->form_id != $form->id || $entry->parent_item_id ) ) || ! empty( $args['is_child'] ) );
+			$child_entry = ( is_numeric( $form->parent_form_id ) && $form->parent_form_id ) || ( $entry && ( $entry->form_id != $form->id || $entry->parent_item_id ) ) || ! empty( $args['is_child'] );
 
 			if ( $child_entry ) {
 				// maybe trigger actions for sub forms
