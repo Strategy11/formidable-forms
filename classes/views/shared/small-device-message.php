3087da31e240d53b5d5a9afa53f4853f426cac81
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-04-02 10:32:47 +0300

    Add a check for ABSPATH constant

--- classes/views/shared/small-device-message.php
+++ classes/views/shared/small-device-message.php
@@ -6,6 +6,9 @@
  * @package Formidable
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
 ?>
 <div id="frm_small_device_message_container">
 	<div id="frm_small_device_message">
