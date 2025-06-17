f0111e44460453d2c26ec952be10547bcd58224f
Sherv <sherv.elmi@gmail.com>
2025-06-17 11:10:34 +0300

    Enable text-toggle component to use options array pattern
    - Replace left/right value approach with options array for better flexibility
    - Restore dynamic ID generation with field_name and index
    - Maintain backward compatibility with existing components
    - Improve default value handling through is_default_checked

--- classes/views/styles/components/templates/text-toggle.php
+++ classes/views/styles/components/templates/text-toggle.php
@@ -3,32 +3,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You are not allowed to call this page directly.' );
 }
 
-$left_value  = $component['left_value'] ?? '';
-$right_value = $component['right_value'] ?? '';
+$options = $component['options'] ?? array();
 ?>
-<div class="<?php echo esc_attr( $component_class ); ?> frm-text-toggle-component frm-radio-component">
+<div class="frm-text-toggle-component frm-radio-component <?php echo esc_attr( $component_class ); ?>">
 	<div class="frm-radio-container frm-flex-justify">
-		<input
-			type="radio"
-			<?php echo esc_attr( $field_name ); ?>
-			id="frm-text-toggle-left-<?php echo esc_attr( $field_name ); ?>"
-			value="<?php echo esc_attr( $left_value ); ?>"
-			<?php checked( empty( $field_value ) || $left_value === $field_value, true ); ?>
-		/>
-		<label class="frm-flex-center" for="frm-text-toggle-left-<?php echo esc_attr( $field_name ); ?>" tabindex="0">
-			<span class="frm-toggle-label"><?php echo esc_html( $component['left_label'] ?? '' ); ?></span>
-		</label>
+		<?php
+		$is_default_checked = empty( $field_value );
 
-		<input
-			type="radio"
-			<?php echo esc_attr( $field_name ); ?>
-			id="frm-text-toggle-right-<?php echo esc_attr( $field_name ); ?>"
-			value="<?php echo esc_attr( $right_value ); ?>"
-			<?php checked( $right_value === $field_value, true ); ?>
-		/>
-		<label class="frm-flex-center" for="frm-text-toggle-right-<?php echo esc_attr( $field_name ); ?>" tabindex="0">
-			<span class="frm-toggle-label"><?php echo esc_html( $component['right_label'] ?? '' ); ?></span>
-		</label>
+		foreach ( $options as $index => $option ) {
+			$input_id     = 'frm-text-toggle-' . $field_name . '-' . $index;
+			$option_value = $option['value'] ?? '';
+			$is_checked   = ( $is_default_checked && 0 === $index ) || $option_value === $field_value;
+			?>
+			<input
+				type="radio"
+				<?php echo esc_attr( $field_name ); ?>
+				id="<?php echo esc_attr( $input_id ); ?>"
+				value="<?php echo esc_attr( $option_value ); ?>"
+				<?php checked( $is_checked, true ); ?>
+			/>
+			<label class="frm-flex-center" for="<?php echo esc_attr( $input_id ); ?>" tabindex="0">
+				<span class="frm-toggle-label"><?php echo esc_html( $option['label'] ?? '' ); ?></span>
+			</label>
+		<?php } ?>
 
 		<span class="frm-radio-active-tracker"></span>
 	</div>
