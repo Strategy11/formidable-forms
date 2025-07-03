6b294a8c772198184ff31f032c2370f7e30ce715
Mike Letellier <mike@crabcyb.org>
2025-07-03 13:26:30 -0300

    Typo fix

--- classes/models/FrmAddon.php
+++ classes/models/FrmAddon.php
@@ -215,9 +215,9 @@ class FrmAddon {
 	 * @since 2.04
 	 */
 	public function get_defined_license() {
-		$consant_name = 'FRM_' . strtoupper( $this->plugin_slug ) . '_LICENSE';
+		$constant_name = 'FRM_' . strtoupper( $this->plugin_slug ) . '_LICENSE';
 
-		return defined( $consant_name ) ? constant( $consant_name ) : false;
+		return defined( $constant_name ) ? constant( $constant_name ) : false;
 	}
 
 	public function set_license( $license ) {
