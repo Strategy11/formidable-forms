2df081efa9df147880b0c05515297f08f2d88868
Mike Letellier <mike@crabcyb.org>
2025-07-16 13:16:51 -0300

    Update failing unit test

--- tests/phpunit/forms/test_FrmFormsController.php
+++ tests/phpunit/forms/test_FrmFormsController.php
@@ -346,7 +346,7 @@ class test_FrmFormsController extends FrmUnitTest {
 		$this->assertNotEmpty( $created_entry, 'No entry found with key ' . $entry_key );
 
 		$response = FrmFormsController::show_form( $form->id ); // this is where the message is returned
-		$this->assertNotFalse( strpos( $response, '<div class="frm_message" role="status"><p>Done!</p>' ) );
+		$this->assertNotFalse( strpos( $response, '<div class="frm_message" role="status">Done!</div>' ) );
 		$this->assertNotFalse( strpos( $response, 'frmFrontForm.scrollMsg(' . $form->id . ')' ) );
 
 		if ( $show_form ) {
