613cbcae5eb3fe92972455045b3f70740acdee3f
Mike Letellier <mike@crabcyb.org>
2025-07-23 12:13:20 -0300

    Maybe fix unit test issue

--- classes/controllers/FrmFormTemplatesController.php
+++ classes/controllers/FrmFormTemplatesController.php
@@ -434,7 +434,9 @@ class FrmFormTemplatesController {
 	 * @return void
 	 */
 	private static function retrieve_and_set_templates() {
-		self::$templates = self::$form_template_api->get_api_info();
+		if ( ! self::$templates ) {
+			self::$templates = self::$form_template_api->get_api_info();
+		}
 
 		self::$is_expired   = FrmAddonsController::is_license_expired();
 		self::$license_type = FrmAddonsController::license_type();
