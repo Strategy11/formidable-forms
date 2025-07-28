9b2c88187d4af8818eb52e457d1cba37f3485e91
Mike Letellier <mike@crabcyb.org>
2025-07-28 09:18:58 -0300

    Remove extra spaces (unit tests)

--- classes/models/FrmTableHTMLGenerator.php
+++ classes/models/FrmTableHTMLGenerator.php
@@ -287,8 +287,8 @@ class FrmTableHTMLGenerator {
 		$row .= $this->add_row_class( $value === '' );
 		$row .= '>';
 
-		$label = '<th scope="row" ' . $this->td_style . '>' . wp_kses_post( $label ) . '</th>';
-		$value = '<td ' . $this->td_style . '>' . wp_kses_post( $value ) . '</td>';
+		$label = '<th scope="row"' . $this->td_style . '>' . wp_kses_post( $label ) . '</th>';
+		$value = '<td' . $this->td_style . '>' . wp_kses_post( $value ) . '</td>';
 
 		if ( 'rtl' == $this->direction ) {
 			$row .= $value;
