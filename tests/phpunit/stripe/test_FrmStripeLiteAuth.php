74dc35621c3860eb0712436b90277521fed15116
Mike Letellier <mike@crabcyb.org>
2025-08-07 10:46:50 -0300

    Add another test and a breaking test to confirm

--- tests/phpunit/stripe/test_FrmStripeLiteAuth.php
+++ tests/phpunit/stripe/test_FrmStripeLiteAuth.php
@@ -25,4 +25,22 @@ class test_FrmStrpLiteAuth extends FrmUnitTest {
 	private function get_statement_descriptor() {
 		return $this->run_private_method( array( 'FrmStrpLiteAuth', 'get_statement_descriptor' ) );
 	}
+
+	/**
+	 * @covers FrmStrpLiteAuth::maybe_add_statement_descriptor
+	 */
+	public function test_maybe_add_statement_descriptor() {
+		$this->assertEquals(
+			array(
+				'statement_descriptor' => get_bloginfo( 'name' ),
+			),
+			$this->maybe_add_statement_descriptor( array() )
+		);
+
+		$this->assertEquals( true, false );
+	}
+
+	private function maybe_add_statement_descriptor( $intent_data ) {
+		return $this->run_private_method( array( 'FrmStrpLiteAuth', 'maybe_add_statement_descriptor' ), array( $intent_data ) );
+	}
 }
