88c94349489b17978ec8791d138d4bac83caf610
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-08-08 14:40:53 +0300

    Move logic that creates field error messages with links to a new function

--- classes/helpers/FrmFormsHelper.php
+++ classes/helpers/FrmFormsHelper.php
@@ -263,13 +263,54 @@ class FrmFormsHelper {
 
 		$frm_settings = FrmAppHelper::get_settings( $settings_args );
 		$invalid_msg  = do_shortcode( $frm_settings->invalid_msg );
-		$field_error_messages = '';
-		foreach ( $args['errors'] as $field_id => $error ) {
-			$field = (array) FrmField::getOne( str_replace( 'field', '', $field_id ) );
-			$field_error_messages .= '<li><a href="#' . FrmFieldsHelper::get_html_id( $field ) . '">' . $error . '</a></li>';
-		}
+		$field_error_messages = self::get_field_error_messages( $args );
 		$invalid_msg .= $field_error_messages ? '<ul>' . $field_error_messages . '</ul>' : '';
 		return apply_filters( 'frm_invalid_error_message', $invalid_msg, $args );
+	}
+
+	/**
+	 * @since x.x
+	 *
+	 * @param array $args
+	 * @return string
+	 */
+	private static function get_field_error_messages( $args ) {
+		$field_error_messages = '';
+		if ( empty( $args['errors'] ) ) {
+			return $field_error_messages;
+		}
+		$field_ids  = array_map(
+			function ( $field_plus_id ) {
+				$field_id = str_replace( 'field', '', $field_plus_id );
+				if ( strpos( $field_id, '-' ) !== false ) {
+					$field_id = explode( '-', $field_id )[0];
+				}
+				return $field_id;
+			},
+			array_keys( $args['errors'] )
+		);
+		$field_keys = FrmDb::get_results( 'frm_fields', array( 'id' => $field_ids ), 'id,field_key,type' );
+		foreach ( $args['errors'] as $field_plus_id => $error ) {
+			$field_id = str_replace( 'field', '', $field_plus_id );
+			$row      = '';
+			if ( strpos( $field_id, '-' ) !== false ) {
+				$field_id_parts = explode( '-', $field_id );
+				if ( count( $field_id_parts ) === 3 ) {
+					$field_id = $field_id_parts[0];
+					$row      = '-' . $field_id_parts[2];
+				}
+			}
+			$index = array_search( $field_id, array_column( $field_keys, 'id' ), true );
+			if ( false === $index ) {
+				continue;
+			}
+			$html_id = 'field_' . $field_keys[ $index ]->field_key . $row;
+			if ( in_array( $field_keys[ $index ]->type, array( 'checkbox', 'radio' ), true ) ) {
+				$html_id .= '-0'; // Focus on the first option.
+			}
+			$field_error_messages .= '<li><a href="#' . $html_id . '">' . $error . '</a></li>';
+		}
+		return $field_error_messages;
 	}
 
 	/**
