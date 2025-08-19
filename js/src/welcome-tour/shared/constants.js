d3bd0f693ef41f8276895bd24285cb4ab9bbb243
Sherv <sherv.elmi@gmail.com>
2025-08-19 19:42:33 +0300

    Add welcome tour shared constants module
    - Create constants.js with INITIAL_STEP, IS_WELCOME_TOUR_SEEN, PREFIX, CHECKLIST_STEPS, and MODAL_SIZE
    - Add shared/index.js to export constants
    - Centralize welcome tour configuration values

---
+++ js/src/welcome-tour/shared/constants.js
@@ -0,0 +1,13 @@
+export const { INITIAL_STEP, IS_WELCOME_TOUR_SEEN } = window.frmWelcomeTourVars;
+
+
+export const PREFIX = 'frm-welcome-tour';
+
+export const CHECKLIST_STEPS = {
+	PICK_TEMPLATE: 'pick-template',
+	ADD_FIELDS: 'add-fields',
+	STYLE_FORM: 'style-form',
+	EMBED_PAGE: 'embed-page',
+};
+
+export const MODAL_SIZE = '440px';
