6fff4bcf29b9158d447bcc5f480100114979c89d
Sherv <sherv.elmi@gmail.com>
2025-08-19 19:41:29 +0300

    Consolidate global DOM utilities into single module
    - Remove event.js (single export file)
    - Add globalModules.js with consolidated frmDom exports
    - Update index.js to import from globalModules instead of event

---
+++ js/src/core/utils/globalModules.js
@@ -0,0 +1,6 @@
+const { div, span, tag, a, img, svg } = frmDom;
+const { maybeCreateModal, footerButton } = frmDom.modal;
+const { onClickPreventDefault, documentOn } = frmDom.util;
+const { doJsonPost } = frmDom.ajax;
+
+export {div, span, tag, a, img, svg, maybeCreateModal, footerButton, onClickPreventDefault, documentOn, doJsonPost};
