aa20610a4aea48cd44e80671d7cf7b5940f5a4f6
Sherv <sherv.elmi@gmail.com>
2025-09-29 18:26:31 +0300

    Add pulse animation to spotlight pointer
    - Add frm-pulse class to spotlight pointer element
    - Create pulse animation CSS with scale and opacity transitions
    - Implement 2s infinite animation with 650ms delay
    - Use relative positioning and ::after pseudo-element for animation layer
    - Scale from 1x to 2.2x with fade-out effect for attention-grabbing UX

--- classes/views/welcome-tour/spotlight.php
+++ classes/views/welcome-tour/spotlight.php
@@ -14,7 +14,7 @@ if ( empty( $spotlight['target'] ) ) {
 }
 ?>
 <div class="frm-spotlight frm-h-stack-xs frm-force-hidden" data-target="<?php echo esc_attr( $spotlight['target'] ); ?>" data-position="<?php echo esc_attr( $spotlight['position'] ?? 'end' ); ?>">
-	<div class="frm-spotlight__pointer frm-flex-center frm-mr-2xs frm-fadein-up-back"></div>
+	<div class="frm-spotlight__pointer frm-flex-center frm-mr-2xs frm-fadein-up-back frm-pulse"></div>
 
 	<div class="frm-spotlight__content frm-flex-col frm-justify-center frm-gap-xs frmcenter frm-bg-white frm-p-sm frm-rounded-md frm-box-shadow-xxl frm-fadein-up-back">
 		<?php if ( ! empty( $spotlight['title'] ) ) { ?>
