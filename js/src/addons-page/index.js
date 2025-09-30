b8432e18c4893a8b9c737398782da21ec244c859
Sherv <sherv.elmi@gmail.com>
2025-09-30 19:53:07 +0300

    Remove debug console.log from addons page initialization

--- js/src/addons-page/index.js
+++ js/src/addons-page/index.js
@@ -10,10 +10,8 @@ import { setupInitialView } from './ui';
 import { addEventListeners } from './events';
 import { buildCategorizedAddons } from './addons';
 
-domReady(() => {
+domReady( () => {
 	setupInitialView();
 	buildCategorizedAddons();
 	addEventListeners();
-
-	console.log( 'A test change to see if GitHub Actions caching is working' );
 });
