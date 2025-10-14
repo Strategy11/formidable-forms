0f7286970ba9c34492d6d625f9ab06948f80bf50
Sherv <sherv.elmi@gmail.com>
2025-10-14 18:31:54 +0300

    feat: add clickable links to style and embed form steps in welcome tour

--- classes/views/welcome-tour/steps/list.php
+++ classes/views/welcome-tour/steps/list.php
@@ -27,13 +27,23 @@ foreach ( $steps as $key => $step ) {
 	if ( $step['completed'] ) {
 		$step_attrs['class'] .= ' frm-checklist__step--completed';
 	}
+
+	$active_step_url = $is_active && in_array( $active_step, array( 'style-form', 'embed-form' ), true )
+		? FrmStylesHelper::get_list_url( $current_form_id )
+		: '';
 	?>
 	<div <?php FrmAppHelper::array_to_html_params( $step_attrs, true ); ?>>
 		<div class="frm-checklist__step-title frm-h-stack-xs frm-p-sm">
-			<span class="frm-checklist__step-status frm-flex-center">
-				<?php FrmAppHelper::icon_by_class( 'frmfont frm_checkmark_icon frm_svg7 frm-text-white', array( 'aria-hidden' => 'true' ) ); ?>
-			</span>
-			<span class=" frm-leading-none"><?php echo esc_html( $step['title'] ); ?></span>
+			<?php if ( $active_step_url ) { ?>
+				<a class="frm-h-stack-xs" href="<?php echo esc_url( $active_step_url ); ?>">
+			<?php } ?>
+				<span class="frm-checklist__step-status frm-flex-center">
+					<?php FrmAppHelper::icon_by_class( 'frmfont frm_checkmark_icon frm_svg7 frm-text-white', array( 'aria-hidden' => 'true' ) ); ?>
+				</span>
+				<span class=" frm-leading-none"><?php echo esc_html( $step['title'] ); ?></span>
+			<?php if ( $active_step_url ) { ?>
+				</a>
+			<?php } ?>
 		</div>
 
 		<div class="frm-checklist__step-description frm-flex frm-p-xs frm-mx-xs -frm-mt-2xs">
