2467a67177d9d2f16597358790066ad4144412d7
Sherv <sherv.elmi@gmail.com>
2025-10-14 16:22:04 +0300

    feat: initialize welcome tour elements with checklist, dismiss, and spotlight selectors

--- js/src/welcome-tour/elements/elements.js
+++ js/src/welcome-tour/elements/elements.js
@@ -3,9 +3,16 @@
  */
 import { createPageElements } from 'core/factory';
 
-/**
- * Internal dependencies
- */
-import { PREFIX } from '../shared';
+let elements;
 
-export const { getElements, addElements } = createPageElements( {} );
+const welcomeTour = document.getElementById( 'frm-welcome-tour' );
+if ( welcomeTour ) {
+	elements = {
+		welcomeTour,
+		checklist: welcomeTour.querySelector( '.frm-checklist' ),
+		dismiss: welcomeTour.querySelector( '.frm-checklist__dismiss' ),
+		spotlight: document.querySelector( '.frm-welcome-tour-spotlight' ),
+	};
+}
+
+export const { getElements, addElements } = createPageElements( elements );
