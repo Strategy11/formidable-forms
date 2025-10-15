03f578caacd6b432e9810db40a2aac43fab9bb50
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-10-15 14:47:39 +0300

    Fix single row style setting not making any effect

--- classes/models/fields/FrmFieldType.php
+++ classes/models/fields/FrmFieldType.php
@@ -982,7 +982,11 @@ DEFAULT_HTML;
 		if ( 'inline' === $align ) {
 			$align = 'horizontal_radio';
 		} elseif ( 'block' === $align ) {
-			$align = 'vertical_radio';
+			$check_align = FrmStylesController::get_style_val( 'check_align', $this->field['form_id'] );
+			// Check needed to fix issue #6035
+			if ( 'block' === $check_align ) {
+				$align = 'vertical_radio';
+			}
 		}
 	}
 
