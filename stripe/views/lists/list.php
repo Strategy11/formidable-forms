9ddabbdefc100a2cb29c4d958b5e9aca09607e3f
Mike Letellier <mike@crabcyb.org>
2025-10-28 12:50:35 -0300

    Add the bottom border below the tabs

--- stripe/views/lists/list.php
+++ stripe/views/lists/list.php
@@ -22,41 +22,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	?>
 
 	<div class="wrap">
-
-		<style>
-			.frm-payments-tabs {
-				margin-bottom: var(--gap-md);
-			}
-
-			.frm-payments-tab {
-				font-weight: 600;
-				font-size: 14px;
-				color: var(--grey-900);
-				display: inline-block;
-				border-width: 1px 1px 0px 1px;
-				border-style: solid;
-				border-color: rgba(234, 236, 240, 1);
-				padding: 8px 12px;
-				border-radius: 6px 6px 0 0;
-				background-color: rgba(249, 250, 251, 1);
-			}
-
-			.frm-payments-tab.frm-active {
-				background-color: white;
-			}
-
-			.frm-payments-tab a {
-				text-decoration: none;
-				color: var(--grey-900);				
-			}
-		</style>
 		<div class="frm-payments-tabs">
+			<div class="frm-payments-tab-filler"></div>	
 			<div class="frm-payments-tab frm-active">
 				<?php echo esc_html__( 'Payments', 'formidable' ); ?>
 			</div>
+			<div class="frm-payments-tab-filler"></div>	
 			<div class="frm-payments-tab">
 				<a href="<?php echo esc_url( admin_url( 'admin.php?page=formidable-coupons' ) ); ?>"><?php echo esc_html__( 'Coupons', 'formidable' ); ?></a>
 			</div>
+			<div class="frm-payments-tab-filler"></div>
 		</div>
 
 		<?php
