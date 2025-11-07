01ba2cc458741f8ccc3e051fdbfa8b0cd462a749
Mike Letellier <mike@crabcyb.org>
2025-11-07 11:45:27 -0400

    Clean up more

--- classes/models/FrmField.php
+++ classes/models/FrmField.php
@@ -243,7 +243,6 @@ class FrmField {
 				'message'      => __( 'Now you can effortlessly gather insights, preferences, and opinions by allowing users to rank options.', 'formidable' ),
 				'upsell_image' => esc_url( $images_url ) . 'ranking-field.svg',
 				'addon'        => 'surveys',
-				'is_new'       => self::field_is_new( 'ranking' ),
 			),
 		);
 
@@ -257,59 +256,6 @@ class FrmField {
 	}
 
 	/**
-	 * @since 6.8.3
-	 *
-	 * @param string $plugin
-	 * @param string $expected_version
-	 * @return bool
-	 */
-	private static function installed_plugin_meets_version( $plugin, $expected_version ) {
-		$installed_version = self::get_installed_version( $plugin );
-		return $installed_version && version_compare( $installed_version, $expected_version, '>=' );
-	}
-
-	/**
-	 * @since 6.8.3
-	 *
-	 * @param string $plugin
-	 * @return false|string String version. False if the plugin is not installed.
-	 */
-	private static function get_installed_version( $plugin ) {
-		if ( ! function_exists( 'get_plugins' ) ) {
-			require_once ABSPATH . 'wp-admin/includes/plugin.php';
-		}
-		$plugins = get_plugins();
-		if ( isset( $plugins[ $plugin ] ) && ! empty( $plugins[ $plugin ]['Version'] ) ) {
-			return $plugins[ $plugin ]['Version'];
-		}
-		return false;
-	}
-
-	/**
-	 * @since 6.8.3
-	 *
-	 * @param string $plugin
-	 * @param string $expected_version
-	 * @return bool
-	 */
-	private static function api_meets_version( $plugin, $expected_version ) {
-		$api     = new FrmFormApi();
-		$addons  = $api->get_api_info();
-		$matches = wp_list_filter( $addons, array( 'plugin' => $plugin ) );
-		if ( ! $matches ) {
-			return false;
-		}
-
-		$match = reset( $matches );
-		if ( empty( $match['new_version'] ) ) {
-			return false;
-		}
-
-		$api_version = $match['new_version'];
-		return version_compare( $api_version, $expected_version, '>=' );
-	}
-
-	/**
 	 * Consider a field new for 90 days after the release date.
 	 *
 	 * @since 6.8.3
@@ -318,12 +264,16 @@ class FrmField {
 	 * @return bool
 	 */
 	private static function field_is_new( $type ) {
-		if ( 'ranking' === $type ) {
-			$ranking_release_date       = '2024-03-12';
-			$three_months_after_release = gmdate( 'Y-m-d', strtotime( $ranking_release_date . ' + 90 days' ) );
-			return gmdate( 'Y-m-d' ) < $three_months_after_release;
+		$release_dates = array();
+
+		if ( ! isset( $release_dates[ $type ] ) ) {
+			return false;
 		}
-		return false;
+
+		$release_date = $release_dates[ $type ];
+
+		$three_months_after_release = gmdate( 'Y-m-d', strtotime( $release_date . ' + 90 days' ) );
+		return gmdate( 'Y-m-d' ) < $three_months_after_release;
 	}
 
 	/**
