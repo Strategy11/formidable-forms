68ad201655c1d53625d2b9b2eaa13157081baed9
Mike Letellier <mike@crabcyb.org>
2025-11-13 14:11:53 -0400

    Add the conditional action button to the upsell page

--- stripe/views/lists/coupons-upsell.php
+++ stripe/views/lists/coupons-upsell.php
@@ -2,6 +2,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You are not allowed to call this page directly.' );
 }
+
+$images_folder_url = FrmAppHelper::plugin_url() . '/images/coupons/';
 ?>
 <div class="frm-coupons-upsell-wrapper">
 	<h3><?php esc_html_e( 'Coupons', 'formidable' ); ?></h3>
@@ -10,19 +12,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php esc_html_e( 'Offer discounts with custom coupon codes. Add a Coupon field to your payment form and set percentage or fixed discounts, usage limits, and availability. Requires a Pro license or higher.', 'formidable' ); ?>
 	</p>
 
+	<?php
+	FrmAddonsController::conditional_action_button(
+		'coupons',
+		array(
+			'campaign' => 'coupons-upsell',
+			'content'  => 'coupons-upsell-button',
+		)
+	);
+	?>
+
 	<div class="frm-coupons-upsell frm-main-coupons-upsell">
-		<?php // TODO: Set a good alt text. ?>
-		<img src="<?php echo esc_url( FrmAppHelper::plugin_url() . '/images/coupons/main-upsell.png' ); ?>" alt="Coupons" />
+		<img src="<?php echo esc_url( $images_folder_url . 'main-upsell.png' ); ?>" alt="<?php esc_attr_e( 'New Coupon Settings', 'formidable' ); ?>" />
 	</div>
 
 	<div class="frm_grid_container frm-secondary-coupons-upsells">
 		<div class="frm6 frm-coupons-upsell frm-left-coupons-upsell">
-			<?php // TODO: Set a good alt text. ?>
-			<img src="<?php echo esc_url( FrmAppHelper::plugin_url() . '/images/coupons/left-upsell.png' ); ?>" alt="Coupons" />
+			<img src="<?php echo esc_url( $images_folder_url . 'left-upsell.png' ); ?>" alt="<?php esc_attr_e( 'Coupon List', 'formidable' ); ?>" />
 		</div>
 		<div class="frm6 frm-coupons-upsell frm-right-coupons-upsell">
-			<?php // TODO: Set a good alt text. ?>
-			<img src="<?php echo esc_url( FrmAppHelper::plugin_url() . '/images/coupons/right-upsell.png' ); ?>" alt="Coupons" />
+			<img src="<?php echo esc_url( $images_folder_url . 'right-upsell.png' ); ?>" alt="<?php esc_attr_e( 'Coupon Field Settings', 'formidable' ); ?>" />
 		</div>
 	</div>
 
