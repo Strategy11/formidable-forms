2c4de92ebc1d7377f01b668ae17b551af6a1402b
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-12-11 21:59:31 +0300

    Make Create with AI upsell button style consistent with the real one

--- classes/views/form-templates/ai-upsell-button.php
+++ classes/views/form-templates/ai-upsell-button.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $atts = array(
 	'show_pill'    => true,
 	'button_text'  => __( 'Create with AI', 'formidable' ),
-	'class'        => 'frm-form-templates-create-button frm_show_upgrade',
+	'class'        => 'frm-form-templates-create-button frm-flex-box frm-items-center frm_show_upgrade',
 	'upgrade_text' => __( 'Create with AI', 'formidable' ),
 );
 FrmFieldsHelper::render_ai_generate_options_button( $atts );
