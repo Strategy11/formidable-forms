03ae96b9562665ca8e3a6809321f6957514aa395
Sherv <sherv.elmi@gmail.com>
2025-12-16 15:27:34 +0300

    Refactor getFieldId utility to accept singleSettings parameter and add getFieldType utility function for extracting field type from class name

--- js/src/admin/settings/utils.js
+++ js/src/admin/settings/utils.js
@@ -1,12 +1,29 @@
 /**
- * Gets field ID from element's closest settings container.
+ * Gets the field ID from the single settings element or the closest single settings element to the field.
  *
  * @since x.x
  *
- * @param {HTMLElement} element The element to get field ID from.
+ * @param {HTMLElement} singleSettings The single settings element.
+ * @param {HTMLElement} field          The field element to get field ID from.
  *
  * @return {string|undefined} The field ID or undefined if not found.
  */
-export function getFieldId( element ) {
-	return element.closest( '.frm-single-settings' )?.dataset.fid;
-}
+export const getFieldId = ( singleSettings = null, field = null ) =>
+	singleSettings ? singleSettings.dataset.fid : field?.closest( '.frm-single-settings' )?.dataset.fid;
+
+/**
+ * Gets the field type from the single settings element.
+ *
+ * @since x.x
+ *
+ * @param {HTMLElement} singleSettings The single settings element.
+ * @param {HTMLElement} field          The field element.
+ *
+ * @return {string|undefined} The field type or undefined if not found.
+ */
+export const getFieldType = ( singleSettings = null, field = null ) => {
+	if ( ! singleSettings ) {
+		singleSettings = field?.closest( '.frm-single-settings' );
+	}
+	return singleSettings?.className.match( /frm-type-(\w+)/ )?.[ 1 ];
+};
