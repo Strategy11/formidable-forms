ba0d151635ab80f463d49ccbf1badd98d2251cd5
Mike Letellier <mike@crabcyb.org>
2025-12-17 15:21:54 -0400

    Fix spacing change

--- square/helpers/FrmSquareLiteConnectHelper.php
+++ square/helpers/FrmSquareLiteConnectHelper.php
@@ -55,7 +55,7 @@ class FrmSquareLiteConnectHelper {
 					<?php esc_html_e( 'Your site is not using SSL. Before using Square to collect payments, you will need to install an SSL certificate on your site.', 'formidable' ); // phpcs:ignore SlevomatCodingStandard.Files.LineLength.LineTooLong ?>
 				</em>
 			</div>
-<?php } ?>
+		<?php } ?>
 		<?php
 	}
 
