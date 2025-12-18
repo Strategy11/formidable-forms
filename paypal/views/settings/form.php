94a3c68ef90c10b7171a7bb0c163a3cfa507f91e
Mike Letellier <mike@crabcyb.org>
2025-12-18 12:33:22 -0400

    Add the section title for the legacy paypal settings

--- paypal/views/settings/form.php
+++ paypal/views/settings/form.php
@@ -5,9 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 FrmPayPalLiteConnectHelper::render_settings_container();
 
+// When the PayPal add-on is active, we want to show the add-on settings here.
 if ( is_callable( array( 'FrmPaymentSettingsController', 'route' ) ) ) {
 	?>
 	<hr style="margin: var(--gap-lg) 0;">
+	<h3><?php _e( 'PayPal Standard (Legacy) Settings', 'formidable' ); ?></h3>
 	<div>
 		<?php FrmPaymentSettingsController::route(); ?>
 	</div>
