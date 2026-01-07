376e04bb040b22eebff3c23d93ce0293fa8d0e4a
Mike Letellier <mike@crabcyb.org>
2026-01-07 15:00:27 -0400

    Use updated key names

--- square/helpers/FrmSquareLiteAppHelper.php
+++ square/helpers/FrmSquareLiteAppHelper.php
@@ -54,12 +54,12 @@ class FrmSquareLiteAppHelper {
 	/**
 	 * Add education about Stripe fees.
 	 *
-	 * @param string             $medium  Medium identifier for the tip (for example 'tip').
+	 * @param string             $content UTM Content for the admin upgrade link.
 	 * @param array|false|string $gateway Gateway or list of gateways this applies to.
 	 *
 	 * @return void
 	 */
-	public static function fee_education( $medium = 'tip', $gateway = false ) {
+	public static function fee_education( $content = 'tip', $gateway = false ) {
 		$license_type = FrmAddonsController::license_type();
 
 		if ( in_array( $license_type, array( 'elite', 'business' ), true ) ) {
@@ -75,8 +75,8 @@ class FrmSquareLiteAppHelper {
 		FrmTipsHelper::show_tip(
 			array(
 				'link'  => array(
-					'content' => 'square-fee',
-					'medium'  => $medium,
+					'campaign' => 'square-fee',
+					'content'  => $content,
 				),
 				'tip'   => 'Pay as you go pricing: 3% fee per-transaction + Square fees.',
 				'call'  => __( 'Upgrade to save on fees.', 'formidable' ),
