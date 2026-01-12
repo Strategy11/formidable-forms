572580d9ee7fc3b8fb8c35935bfdc83a17039291
Mike Letellier <mike@crabcyb.org>
2026-01-12 11:32:56 -0400

    Tweak some comments

--- classes/helpers/FrmAppHelper.php
+++ classes/helpers/FrmAppHelper.php
@@ -327,11 +327,12 @@ class FrmAppHelper {
 		);
 		$atts     = array_merge( $defaults, $atts );
 
-		// phpcs:ignore SlevomatCodingStandard.Files.LineLength.LineTooLong
+		// phpcs:disable SlevomatCodingStandard.Files.LineLength.LineTooLong
 		return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 599.68 601.37" width="' . esc_attr( $atts['width'] ) . '" height="' . esc_attr( $atts['height'] ) . '">
 			<path fill="' . esc_attr( $atts['orange'] ) . '" d="M289.6 384h140v76h-140z"/>
 			<path fill="' . esc_attr( $atts['fill'] ) . '" d="M400.2 147h-200c-17 0-30.6 12.2-30.6 29.3V218h260v-71zM397.9 264H169.6v196h75V340H398a32.2 32.2 0 0 0 30.1-21.4 24.3 24.3 0 0 0 1.7-8.7V264zM299.8 601.4A300.3 300.3 0 0 1 0 300.7a299.8 299.8 0 1 1 511.9 212.6 297.4 297.4 0 0 1-212 88zm0-563A262 262 0 0 0 38.3 300.7a261.6 261.6 0 1 0 446.5-185.5 259.5 259.5 0 0 0-185-76.8z"/>
 		</svg>';
+		// phpcs:enable SlevomatCodingStandard.Files.LineLength.LineTooLong
 	}
 
 	/**
