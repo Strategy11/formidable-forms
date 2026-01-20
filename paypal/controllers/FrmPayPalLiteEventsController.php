f579d3b1048e53174acf48ab991af0fdeedf5424
Mike Letellier <mike@crabcyb.org>
2026-01-20 09:46:34 -0400

    Apply new sniffs

--- paypal/controllers/FrmPayPalLiteEventsController.php
+++ paypal/controllers/FrmPayPalLiteEventsController.php
@@ -76,13 +76,14 @@ class FrmPayPalLiteEventsController {
 
 			$this->event = FrmPayPalLiteConnectHelper::get_event( $event_id );
 
-			if ( is_object( $this->event ) ) {
-				$this->handle_event();
-				$this->track_handled_event( $event_id );
-				FrmPayPalLiteConnectHelper::process_event( $event_id );
-			} else {
+			if ( ! is_object( $this->event ) ) {
 				$this->count_failed_event( $event_id );
+				continue;
 			}
+
+			$this->handle_event();
+			$this->track_handled_event( $event_id );
+			FrmPayPalLiteConnectHelper::process_event( $event_id );
 		}
 	}
 
