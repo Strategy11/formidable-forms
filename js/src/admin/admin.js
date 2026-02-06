15ecf1531babf0ce6e888fce8fb172b7a866018a
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2026-02-06 18:22:40 +0300

    Rebuild files

--- js/src/admin/admin.js
+++ js/src/admin/admin.js
@@ -5857,20 +5857,21 @@ window.frmAdminBuildJS = function() {
 		}
 
 		if ( e?.metaKey || e?.ctrlKey ) {
-			const groupIsActive = this.closest( 'ul' ).classList.contains( 'frm-selected-field-group' );
+			const target = this.closest( 'ul' );
+			const groupIsActive = target.classList.contains( 'frm-selected-field-group' );
 			const $selectedFieldGroups = getSelectedFieldGroups();
 
 			let numberOfSelectedGroups = $selectedFieldGroups.length;
 			if ( groupIsActive ) {
 				// Unselect if holding ctrl or cmd and the group was already active.
 				--numberOfSelectedGroups;
-				this.closest( 'ul' ).classList.remove( 'frm-selected-field-group' );
+				target.classList.remove( 'frm-selected-field-group' );
 				syncAfterMultiSelect( numberOfSelectedGroups );
 				return;
 			}
 
 			// Add the target to list of selected fields.
-			this.closest( 'ul' ).classList.add( 'frm-selected-field-group' );
+			target.classList.add( 'frm-selected-field-group' );
 			++numberOfSelectedGroups;
 			syncAfterMultiSelect( numberOfSelectedGroups );
 
