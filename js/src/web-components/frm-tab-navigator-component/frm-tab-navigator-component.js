9a4e251534444f816bf4aeb5ede85584e44a0a6a
Sherv <sherv.elmi@gmail.com>
2026-03-16 19:51:11 +0300

    Update import paths for TabsNavigator to reflect new tabs subdirectory structure

--- js/src/web-components/frm-tab-navigator-component/frm-tab-navigator-component.js
+++ js/src/web-components/frm-tab-navigator-component/frm-tab-navigator-component.js
@@ -1,4 +1,4 @@
-import { frmTabsNavigator } from '../../components/class-tabs-navigator';
+import { frmTabsNavigator } from '../../components/tabs/class-tabs-navigator';
 import { frmWebComponent } from '../frm-web-component';
 import style from './frm-tab-navigator-component.css';
 
