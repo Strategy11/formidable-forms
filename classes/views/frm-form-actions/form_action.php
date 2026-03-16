1bab89b43bcbda773711184172cabf0390511cc2
Sherv <sherv.elmi@gmail.com>
2026-03-16 21:03:45 +0300

    Reorganize form action widget header layout by moving drag handle before title and reordering action icons
    Move drag handle icon to start of widget-top, reposition title section before action icons, and swap widget-title-action toggle button to end of header. Remove extra space from delete icon class and frm-outer-circle class from action icon wrapper.

--- classes/views/frm-form-actions/form_action.php
+++ classes/views/frm-form-actions/form_action.php
@@ -21,19 +21,25 @@ if ( FrmOnSubmitAction::$slug === $form_action->post_excerpt ) {
 	<?php FrmAppHelper::array_to_html_params( $data_attrs, true ); ?>
 >
 	<div class="widget-top">
-		<div class="widget-title-action">
-			<button type="button" class="widget-action hide-if-no-js" aria-expanded="false">
-				<?php FrmAppHelper::icon_by_class( 'frmfont frm_arrow_right_icon' ); ?>
-			</button>
+		<span class="frm-action-drag-handle">
+			<?php FrmAppHelper::icon_by_class( 'frmfont frm_drag_icon' ); ?>
+		</span>
+		<div class="widget-title">
+			<h4>
+				<span class="frm_form_action_icon <?php echo esc_attr( str_contains( $action_control->action_options['classes'], 'frm-inverse' ) ? 'frm-inverse' : '' ); ?>">
+					<?php FrmAppHelper::icon_by_class( $action_control->action_options['classes'] ); ?>
+				</span>
+				<?php echo esc_html( $form_action->post_title ); ?>
+			</h4>
 		</div>
-		<span class="frm_email_icons alignright">
+		<span class="frm_email_icons">
 			<?php if ( $action_control->action_options['limit'] > 2 ) { ?>
 				<a href="javascript:void(0)" class="frm_duplicate_form_action" title="<?php esc_attr_e( 'Duplicate', 'formidable' ); ?>">
 					<?php FrmAppHelper::icon_by_class( 'frmfont frm_clone_icon' ); ?>
 				</a>
 			<?php } ?>
 			<a href="javascript:void(0)" data-removeid="frm_form_action_<?php echo esc_attr( $action_key ); ?>" class="frm_remove_form_action" data-frmverify="<?php esc_attr_e( 'Delete this form action?', 'formidable' ); ?>" data-frmverify-btn="frm-button-red" title="<?php esc_attr_e( 'Delete', 'formidable' ); ?>">
-				<?php FrmAppHelper::icon_by_class( 'frmfont frm_delete_icon ' ); ?>
+				<?php FrmAppHelper::icon_by_class( 'frmfont frm_delete_icon' ); ?>
 			</a>
 
 			<?php
@@ -50,13 +56,10 @@ if ( FrmOnSubmitAction::$slug === $form_action->post_excerpt ) {
 			);
 			?>
 		</span>
-		<div class="widget-title">
-			<h4>
-				<span class="frm_form_action_icon frm-outer-circle <?php echo esc_attr( str_contains( $action_control->action_options['classes'], 'frm-inverse' ) ? ' frm-inverse' : '' ); ?>">
-					<?php FrmAppHelper::icon_by_class( $action_control->action_options['classes'] ); ?>
-				</span>
-				<?php echo esc_html( $form_action->post_title ); ?>
-			</h4>
+		<div class="widget-title-action">
+			<button type="button" class="widget-action hide-if-no-js" aria-expanded="false">
+				<?php FrmAppHelper::icon_by_class( 'frmfont frm_arrow_right_icon' ); ?>
+			</button>
 		</div>
 	</div>
 	<div class="widget-inside">
