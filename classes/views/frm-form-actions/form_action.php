bed5dc7a95e42fbc746d9e2542fab96d60bd9a28
Sherv <sherv.elmi@gmail.com>
2026-03-23 23:23:50 +0300

    refactor(form-action): improve layout and styling of action card components for better user experience

--- classes/views/frm-form-actions/form_action.php
+++ classes/views/frm-form-actions/form_action.php
@@ -20,53 +20,56 @@ if ( FrmOnSubmitAction::$slug === $form_action->post_excerpt ) {
 	class="widget frm_form_action_settings frm_single_<?php echo esc_attr( $form_action->post_excerpt ); ?>_settings <?php echo esc_attr( $form_action->post_status === 'publish' ? '' : 'frm_disabled_action' ); ?>"
 	<?php FrmAppHelper::array_to_html_params( $data_attrs, true ); ?>
 >
-	<div class="widget-top">
-		<div class="widget-title">
-			<h4>
-				<span class="frm_form_action_icon">
-					<?php FrmAppHelper::icon_by_class( $action_control->action_options['classes'] ); ?>
-				</span>
-				<?php echo esc_html( $form_action->post_title ); ?>
+	<div class="widget-top frm-h-stack-xs">
+		<div class="widget-title frm-flex-full">
+			<h4 class="frm-h-stack-xs">
+				<?php FrmAppHelper::icon_by_class( $action_control->action_options['classes'] ); ?>
+				<span><?php echo esc_html( $form_action->post_title ); ?></span>
 			</h4>
 		</div>
-		<span class="frm_email_icons">
-			<?php if ( $action_control->action_options['limit'] > 2 ) { ?>
-				<a href="javascript:void(0)" class="frm_duplicate_form_action" title="<?php esc_attr_e( 'Duplicate', 'formidable' ); ?>">
-					<?php FrmAppHelper::icon_by_class( 'frmfont frm_clone_icon' ); ?>
-				</a>
-			<?php } ?>
-			<a href="javascript:void(0)" data-removeid="frm_form_action_<?php echo esc_attr( $action_key ); ?>" class="frm_remove_form_action" data-frmverify="<?php esc_attr_e( 'Delete this form action?', 'formidable' ); ?>" data-frmverify-btn="frm-button-red" title="<?php esc_attr_e( 'Delete', 'formidable' ); ?>">
-				<?php FrmAppHelper::icon_by_class( 'frmfont frm_delete_icon' ); ?>
-			</a>
 
-			<?php
-			FrmHtmlHelper::toggle(
-				$action_control->get_field_id( 'post_status', '' ),
-				$action_control->get_field_name( 'post_status', '' ),
-				array(
-					'checked'     => $form_action->post_status === 'publish',
-					'on_label'    => 'publish',
-					'off_label'   => 'OFF',
-					'show_labels' => false,
-					'echo'        => true,
-				)
-			);
-			?>
-		</span>
-		<div class="widget-title-action">
-			<button type="button" class="widget-action hide-if-no-js" aria-expanded="false">
-				<?php FrmAppHelper::icon_by_class( 'frmfont frm_arrow_right_icon' ); ?>
-			</button>
+		<div class="frm-ml-auto">
+			<div class="widget-title-action">
+				<button type="button" class="widget-action hide-if-no-js" aria-expanded="false">
+					<?php FrmAppHelper::icon_by_class( 'frmfont frm_arrowdown8_icon' ); ?>
+				</button>
+			</div>
+
+			<span class="frm_email_icons">
+				<?php if ( $action_control->action_options['limit'] > 2 ) { ?>
+					<a href="javascript:void(0)" class="frm_duplicate_form_action" title="<?php esc_attr_e( 'Duplicate', 'formidable' ); ?>">
+						<?php FrmAppHelper::icon_by_class( 'frmfont frm_clone_icon' ); ?>
+					</a>
+				<?php } ?>
+
+				<a href="javascript:void(0)" data-removeid="frm_form_action_<?php echo esc_attr( $action_key ); ?>" class="frm_remove_form_action" data-frmverify="<?php esc_attr_e( 'Delete this form action?', 'formidable' ); ?>" data-frmverify-btn="frm-button-red" title="<?php esc_attr_e( 'Delete', 'formidable' ); ?>">
+					<?php FrmAppHelper::icon_by_class( 'frmfont frm_delete_icon' ); ?>
+				</a>
+
+				<?php
+				FrmHtmlHelper::toggle(
+					$action_control->get_field_id( 'post_status', '' ),
+					$action_control->get_field_name( 'post_status', '' ),
+					array(
+						'checked'     => $form_action->post_status === 'publish',
+						'on_label'    => 'publish',
+						'off_label'   => 'OFF',
+						'show_labels' => false,
+						'echo'        => true,
+					)
+				);
+				?>
+			</span>
 		</div>
 	</div>
+
 	<div class="widget-inside">
 		<?php
+		// Load settings only if just added or open, otherwise include hidden settings to prevent losing the action on update.
 		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			// Only load settings if they are just added or are open
 			include __DIR__ . '/_action_inside.php';
 		} else {
-			// Include hidden settings so action won't get lost on update
-			?>
+		?>
 		<input type="hidden" name="<?php echo esc_attr( $action_control->get_field_name( 'post_excerpt', '' ) ); ?>" class="frm_action_name" value="<?php echo esc_attr( $form_action->post_excerpt ); ?>" />
 		<input type="hidden" name="<?php echo esc_attr( $action_control->get_field_name( 'ID', '' ) ); ?>" value="<?php echo esc_attr( $form_action->ID ); ?>" />
 		<?php } ?>
