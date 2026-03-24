a4512a32cd1d017a08076ed81fefcf74e92ab662
Mike Letellier <mike@crabcyb.org>
2026-03-24 09:23:26 -0300

    Fix pro issue 6081 (Square fatal error when refunding with payments submodule active)

---
+++ square/helpers/FrmSquareLiteApiHelper.php
@@ -0,0 +1,35 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+/**
+ * This file exists for backward compatibility with the Payments Submodule.
+ * Without these functions the Authorize.Net add on will trigger fatal errors.
+ */
+class FrmSquareLiteApiHelper {
+
+	/**
+	 * The payments submodule calls this function.
+	 * This function exists so payments can be refunded when Authorize.Net is active.
+	 *
+	 * @param int $payment_id
+	 *
+	 * @return bool
+	 */
+	public static function refund_payment( $payment_id ) {
+		return FrmSquareLiteConnectHelper::refund_payment( $payment_id );
+	}
+
+	/**
+	 * The payments submodule calls this function.
+	 * This function exists so subscriptions can be cancelled when Authorize.Net is active.
+	 *
+	 * @param string $sub_id
+	 *
+	 * @return bool
+	 */
+	public static function cancel_subscription( $sub_id ) {
+		return FrmSquareLiteConnectHelper::cancel_subscription( $sub_id );
+	}
+}
