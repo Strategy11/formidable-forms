e32179a2b67678fc435821d4c89d7ba46dd3d1a0
Sherv <sherv.elmi@gmail.com>
2026-03-30 16:54:17 +0300

    style(form-actions): update action icon classes for improved styling and consistency

--- classes/views/frm-form-actions/_action_icon.php
+++ classes/views/frm-form-actions/_action_icon.php
@@ -7,25 +7,25 @@ $single_action_attrs = array_merge(
 	$data,
 	array(
 		'href'            => 'javascript:void(0)',
-		'class'           => $classes . ' button frm-button-secondary frm-button-sm frm-with-icon frm-ml-auto-force',
+		'class'           => $classes . ' button frm-button-secondary frm-button-sm frm-with-icon frm-ml-auto-force frm-fadein-down-short',
 		'data-limit'      => $action_control->action_options['limit'],
 		'data-actiontype' => $action_control->id_base,
 	)
 );
 ?>
 <li class="frm-card-item frm-card-item--outlined frm-action<?php echo esc_attr( $group_class . ( isset( $data['data-upgrade'] ) ? ' frm-not-installed' : '' ) ); ?>">
-	<div class="frm-h-stack-xs frm-w-full frm-mb-2xs">
+	<div class="frm-h-stack-xs frm-w-full">
 		<span class="frm-border-icon">
 			<?php FrmAppHelper::icon_by_class( $action_control->action_options['classes'], FrmFormActionsController::get_action_icon_atts( $action_control ) ); ?>
 		</span>
 
 		<div class="frm-flex-col">
-			<h3 class="frm-text-md frm-capitalize">
-				<?php if ( isset( $data['data-upgrade'] ) && ! isset( $data['data-oneclick'] ) ) { ?>
-					<span class="frm-action-lock-icon">
-						<?php FrmAppHelper::icon_by_class( 'frmfont frm_lock_icon', array( 'aria-label' => __( 'Lock icon', 'formidable' ) ) ); ?>
-					</span>
-				<?php } ?>
+			<h3 class="frm-h-stack-xs frm-text-md frm-capitalize">
+				<?php
+				if ( isset( $data['data-upgrade'] ) && ! isset( $data['data-oneclick'] ) ) {
+					FrmAppHelper::icon_by_class( 'frmfont frm_lock_icon frm_svg15', array( 'aria-label' => __( 'Lock icon', 'formidable' ) ) );
+				}
+				?>
 				<span class="frm-font-medium frm-truncate"><?php echo esc_html( str_replace( 'Add to ', '', $action_control->name ) ); ?></span>
 				<?php if ( ! empty( $action_control->action_options['is_new'] ) ) { ?>
 					<?php FrmAppHelper::show_pill_text(); ?>
@@ -37,7 +37,7 @@ $single_action_attrs = array_merge(
 		</div>
 
 		<a <?php FrmAppHelper::array_to_html_params( $single_action_attrs, true ); ?>>
-			<?php FrmAppHelper::icon_by_class( 'frm_icon_font frm_plus_icon' ); ?>
+			<?php FrmAppHelper::icon_by_class( 'frmfont frm_plus_icon' ); ?>
 			<span><?php echo esc_html_x( 'Add', 'form action', 'formidable' ); ?></span>
 		</a>
 
