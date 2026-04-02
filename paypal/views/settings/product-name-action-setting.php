d4970c885dc9a207ef383f9d3cd2b408b67ce76b
Mike Letellier <mike@crabcyb.org>
2026-04-02 17:00:01 -0300

    Use $action_control instead of $this, add nullable check for product name setting

--- paypal/views/settings/product-name-action-setting.php
+++ paypal/views/settings/product-name-action-setting.php
@@ -8,5 +8,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<label for="<?php echo esc_attr( $action_control->get_field_id( 'product_name' ) ); ?>">
 		<?php  esc_html_e( 'Product Name', 'formidable' ); ?>
 	</label>
-	<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'product_name' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'product_name' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['product_name'] ); ?>" class="frm_not_email_subject large-text" />
+	<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'product_name' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'product_name' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['product_name'] ?? '' ); ?>" class="frm_not_email_subject large-text" />
 </p>
--- stripe/views/action-settings/payments-options.php
+++ stripe/views/action-settings/payments-options.php
@@ -24,7 +24,7 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 }
 ?>
 
-<?php FrmTransLiteAppHelper::show_gateway_buttons( $gateways, $form_action, $this ); ?>
+<?php FrmTransLiteAppHelper::show_gateway_buttons( $gateways, $form_action, $action_control ); ?>
 
 <div class="frm_grid_container">
 	<?php
@@ -48,30 +48,30 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 		<label for="<?php echo esc_attr( $action_control->get_field_id( 'description' ) ); ?>">
 			<?php esc_html_e( 'Description', 'formidable' ); ?>
 		</label>
-		<input type="text" name="<?php echo esc_attr( $this->get_field_name( 'description' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'description' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['description'] ); ?>" class="frm_not_email_subject large-text" />
+		<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'description' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'description' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['description'] ); ?>" class="frm_not_email_subject large-text" />
 	</p>
 
 	<p class="frm6">
 		<label for="<?php echo esc_attr( $action_control->get_field_id( 'amount' ) ); ?>">
 			<?php esc_html_e( 'Amount', 'formidable' ); ?>
 		</label>
-		<input type="text" value="<?php echo esc_attr( $form_action->post_content['amount'] ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'amount' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'amount' ) ); ?>" class="frm_not_email_subject large-text" />
+		<input type="text" value="<?php echo esc_attr( $form_action->post_content['amount'] ); ?>" name="<?php echo esc_attr( $action_control->get_field_name( 'amount' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'amount' ) ); ?>" class="frm_not_email_subject large-text" />
 	</p>
 
-	<?php $cc_field_id = $this->get_credit_card_field_id( $field_dropdown_atts ); ?>
-	<input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'credit_card' ) ); ?>" value="<?php echo esc_attr( $cc_field_id ); ?>" />
+	<?php $cc_field_id = $action_control->get_credit_card_field_id( $field_dropdown_atts ); ?>
+	<input type="hidden" name="<?php echo esc_attr( $action_control->get_field_name( 'credit_card' ) ); ?>" value="<?php echo esc_attr( $cc_field_id ); ?>" />
 
 	<p class="frm6">
 		<label>
 			<?php esc_html_e( 'Payment Type', 'formidable' ); ?>
 		</label>
-		<select name="<?php echo esc_attr( $this->get_field_name( 'type' ) ); ?>" class="frm_trans_type">
+		<select name="<?php echo esc_attr( $action_control->get_field_name( 'type' ) ); ?>" class="frm_trans_type">
 			<option value="single" <?php selected( $form_action->post_content['type'], 'one_time' ); ?>><?php esc_html_e( 'One-time Payment', 'formidable' ); ?></option>
 			<option value="recurring" <?php selected( $form_action->post_content['type'], 'recurring' ); ?>><?php esc_html_e( 'Recurring', 'formidable' ); ?></option>
 		</select>
 	</p>
 
-	<?php $this->echo_capture_payment_upsell( $form_action->post_content['gateway'] ); ?>
+	<?php $action_control->echo_capture_payment_upsell( $form_action->post_content['gateway'] ); ?>
 
 	<p class="frm6 frm_trans_sub_opts <?php echo $form_action->post_content['type'] === 'recurring' ? '' : 'frm_hidden'; ?>">
 		<label>
@@ -79,13 +79,13 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 		</label>
 		<span>
 			<span class="frm_grid_container">
-				<input class="frm6" type="number" name="<?php echo esc_attr( $this->get_field_name( 'interval_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['interval_count'] ); ?>" max="90" min="1" step="1" />
-				<select class="frm6" name="<?php echo esc_attr( $this->get_field_name( 'interval' ) ); ?>" class="auto_width">
+				<input class="frm6" type="number" name="<?php echo esc_attr( $action_control->get_field_name( 'interval_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['interval_count'] ); ?>" max="90" min="1" step="1" />
+				<select class="frm6" name="<?php echo esc_attr( $action_control->get_field_name( 'interval' ) ); ?>" class="auto_width">
 					<?php foreach ( $repeat_times as $k => $v ) { ?>
 						<option value="<?php echo esc_attr( $k ); ?>" <?php selected( $form_action->post_content['interval'], $k ); ?>><?php echo esc_html( $v ); ?></option>
 					<?php } ?>
 				</select>
-				<input type="hidden" name="<?php echo esc_attr( $this->get_field_name( 'payment_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['payment_count'] ); ?>" />
+				<input type="hidden" name="<?php echo esc_attr( $action_control->get_field_name( 'payment_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['payment_count'] ); ?>" />
 			</span>
 		</span>
 	</p>
@@ -102,36 +102,36 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 		<label>
 			<?php esc_html_e( 'Recurring Payment Limit', 'formidable' ); ?>
 		</label>
-		<input type="text" name="<?php echo esc_attr( $this->get_field_name( 'payment_limit' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['payment_limit'] ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'payment_limit' ) ); ?>" class="frm_not_email_subject" />
+		<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'payment_limit' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['payment_limit'] ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'payment_limit' ) ); ?>" class="frm_not_email_subject" />
 	</p>
 
 	<p class="frm_trans_sub_opts frm6 <?php echo $form_action->post_content['type'] === 'recurring' ? '' : 'frm_hidden'; ?>">
 		<label>
 			<?php esc_html_e( 'Trial Period', 'formidable' ); ?>
 		</label>
-		<input type="text" name="<?php echo esc_attr( $this->get_field_name( 'trial_interval_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['trial_interval_count'] ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'trial_interval_count' ) ); ?>" class="frm_not_email_subject" />
+		<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'trial_interval_count' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['trial_interval_count'] ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'trial_interval_count' ) ); ?>" class="frm_not_email_subject" />
 		<?php esc_html_e( 'day(s)', 'formidable' ); ?>
 	</p>
 
 	<p class="frm6">
-		<label for="<?php echo esc_attr( $this->get_field_id( 'currency' ) ); ?>">
+		<label for="<?php echo esc_attr( $action_control->get_field_id( 'currency' ) ); ?>">
 			<?php esc_html_e( 'Currency', 'formidable' ); ?>
 		</label>
-		<?php FrmTransLiteAppHelper::show_currency_dropdown( $this->get_field_id( 'currency' ), $this->get_field_name( 'currency' ), $form_action->post_content ); ?>
+		<?php FrmTransLiteAppHelper::show_currency_dropdown( $action_control->get_field_id( 'currency' ), $action_control->get_field_name( 'currency' ), $form_action->post_content ); ?>
 	</p>
 
 	<?php
 	FrmStrpLiteActionsController::add_action_options(
 		array(
 			'form_action'    => $form_action,
-			'action_control' => $this,
+			'action_control' => $action_control,
 		)
 	);
 
 	FrmPayPalLiteActionsController::add_action_options(
 		array(
 			'form_action'    => $form_action,
-			'action_control' => $this,
+			'action_control' => $action_control,
 		)
 	);
 	?>
@@ -146,7 +146,7 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 		<label for="<?php echo esc_attr( $action_control->get_field_id( 'email' ) ); ?>">
 			<?php esc_html_e( 'Email', 'formidable' ); ?>
 		</label>
-		<input type="text" name="<?php echo esc_attr( $this->get_field_name( 'email' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'email' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['email'] ); ?>" class="frm_not_email_to large-text" />
+		<input type="text" name="<?php echo esc_attr( $action_control->get_field_name( 'email' ) ); ?>" id="<?php echo esc_attr( $action_control->get_field_id( 'email' ) ); ?>" value="<?php echo esc_attr( $form_action->post_content['email'] ); ?>" class="frm_not_email_to large-text" />
 	</p>
 
 	<?php
@@ -168,15 +168,15 @@ if ( ! $stripe_connected && ! $square_connected && ! $paypal_connected ) {
 	?>
 
 	<p class="frm6">
-		<label for="<?php echo esc_attr( $this->get_field_id( 'billing_first_name' ) ); ?>">
+		<label for="<?php echo esc_attr( $action_control->get_field_id( 'billing_first_name' ) ); ?>">
 			<?php esc_html_e( 'First Name', 'formidable' ); ?>
 		</label>
-		<?php $this->show_fields_dropdown( $field_dropdown_atts, array( 'name' => 'billing_first_name' ) ); ?>
+		<?php $action_control->show_fields_dropdown( $field_dropdown_atts, array( 'name' => 'billing_first_name' ) ); ?>
 	</p>
 	<p class="frm6">
-		<label for="<?php echo esc_attr( $this->get_field_id( 'billing_last_name' ) ); ?>">
+		<label for="<?php echo esc_attr( $action_control->get_field_id( 'billing_last_name' ) ); ?>">
 			<?php esc_html_e( 'Last Name', 'formidable' ); ?>
 		</label>
-		<?php $this->show_fields_dropdown( $field_dropdown_atts, array( 'name' => 'billing_last_name' ) ); ?>
+		<?php $action_control->show_fields_dropdown( $field_dropdown_atts, array( 'name' => 'billing_last_name' ) ); ?>
 	</p>
 </div>
