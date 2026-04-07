0c995cba9d456bc5577f9ad42d7180b3f5881bbe
Truong Giang <truongwp@gmail.com>
2026-04-08 00:38:50 +0700

    Skip DeepSource PHP error

--- classes/helpers/FrmFormsListHelper.php
+++ classes/helpers/FrmFormsListHelper.php
@@ -244,8 +244,7 @@ class FrmFormsListHelper extends FrmListHelper {
 	 *
 	 * @return string
 	 */
-	// phpcs:ignore SlevomatCodingStandard.Complexity.Cognitive.ComplexityTooHigh
-	// skipcq: PHP-R1006
+	// phpcs:ignore SlevomatCodingStandard.Complexity.Cognitive.ComplexityTooHigh skipcq: PHP-R1006
 	public function single_row( $item, $style = '' ) {
 		global $mode;
 
