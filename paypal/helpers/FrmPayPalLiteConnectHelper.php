1736f06d2788ec4cb0efb7fa5f7961970c79b120
Mike Letellier <mike@crabcyb.org>
2026-06-12 10:21:10 -0300

    Pass action descriptions to paypal

--- paypal/helpers/FrmPayPalLiteConnectHelper.php
+++ paypal/helpers/FrmPayPalLiteConnectHelper.php
@@ -1008,14 +1008,15 @@ class FrmPayPalLiteConnectHelper {
 	 * @param string $shipping_preference
 	 * @param array  $pricing_data Optional. Array of products with prices and quantities.
 	 * @param array  $shipping     Optional. Shipping name and address data.
+	 * @param string $description  Optional. Description for the order.
 	 *
 	 * @return false|object
 	 */
-	public static function create_order( $amount, $currency, $payment_source, $payer, $shipping_preference, $pricing_data = array(), $shipping = array() ) {
+	public static function create_order( $amount, $currency, $payment_source, $payer, $shipping_preference, $pricing_data = array(), $shipping = array(), $description = '' ) {
 		$brand_name = self::get_brand_name();
 
 		// phpcs:ignore SlevomatCodingStandard.Files.LineLength.LineTooLong
-		return self::post_with_authenticated_body( 'create_order', compact( 'amount', 'currency', 'payment_source', 'brand_name', 'payer', 'shipping_preference', 'pricing_data', 'shipping' ) );
+		return self::post_with_authenticated_body( 'create_order', compact( 'amount', 'currency', 'payment_source', 'brand_name', 'payer', 'shipping_preference', 'pricing_data', 'shipping', 'description' ) );
 	}
 
 	/**
