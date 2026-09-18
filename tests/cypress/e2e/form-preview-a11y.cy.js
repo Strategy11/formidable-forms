02715abc8964be1d09e7d8933fce4f44cd0f680f
Mike Letellier <mike@crabcyb.org>
2026-09-18 14:07:40 -0300

    Use the other form key

--- tests/cypress/e2e/form-preview-a11y.cy.js
+++ tests/cypress/e2e/form-preview-a11y.cy.js
@@ -15,7 +15,7 @@ describe( 'Run some accessibility tests', function() {
 	it( 'Check the form list has valid HTML', () => {
 		cy.login();
 		cy.ensureContactUsFormExists();
-		cy.visit( '/wp-admin/admin-ajax.php?action=frm_forms_preview&form=contact-us' );
+		cy.visit( '/wp-admin/admin-ajax.php?action=frm_forms_preview&form=contact-form' );
 		cy.injectAxe();
 		configureAxeWithBaselineIgnoredRuleset();
 		cy.checkA11y();
--- tests/cypress/e2e/form-preview-html-validation.cy.js
+++ tests/cypress/e2e/form-preview-html-validation.cy.js
@@ -2,7 +2,7 @@ describe( 'Run some HTML validation', function() {
 	it( 'Check the form list has valid HTML', () => {
 		cy.login();
 		cy.ensureContactUsFormExists();
-		cy.visit( '/wp-admin/admin-ajax.php?action=frm_forms_preview&form=contact-us' );
+		cy.visit( '/wp-admin/admin-ajax.php?action=frm_forms_preview&form=contact-form' );
 		cy.get( '#form_contact-us' ).htmlvalidate();
 	} );
 } );
